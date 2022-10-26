5820a67eb5a0fc0521f2f0ccf8731ea9fd2a94bf
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-26 13:32:29 +0800

    Create serviceTipCalculatorFunction.js

---
+++ JavaScript/post-ECMA2015/ES6/serviceTipCalculatorFunction.js
@@ -0,0 +1,13 @@
+const tipCalculator = (quality, total) => {
+  if (quality === 'bad') {
+    return total*0.05
+  } else if (quality === 'ok') {
+    return total*0.15
+  } else if (quality === 'good') {
+    return total*0.20
+  } else if (quality === 'excellent') {
+    return total*0.30 
+  } else {
+    return total*0.18
+  }
+}
