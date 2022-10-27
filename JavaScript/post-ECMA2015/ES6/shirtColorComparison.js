bc3efbfacf486fc3764b3b180174b9c658aaaca5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-27 08:30:49 +0800

    Create shirtColorComparison.js

---
+++ JavaScript/post-ECMA2015/ES6/shirtColorComparison.js
@@ -0,0 +1,10 @@
+// Create function below
+const colorMessage = (favoriteColor, shirtColor) => {
+  if (favoriteColor === shirtColor) {
+    return 'The shirt is your favorite color!'
+  } else {
+    return 'That is a nice color.'
+  }
+}
+
+console.log(colorMessage('red', 'red'));
