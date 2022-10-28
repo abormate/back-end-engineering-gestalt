d2d680032b3c8109e8e6822ae28097a86b66cfca
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-28 17:50:00 +0800

    Update sentenceAboutProgramming.js

--- JavaScript/post-ECMA2015/ES6/sentenceAboutProgramming.js
+++ JavaScript/post-ECMA2015/ES6/sentenceAboutProgramming.js
@@ -2,3 +2,5 @@ const sillySentence = (adjective, verb, noun) => {
   
   return `I am so ${adjective} because I ${verb} coding! Time to write some more awesome ${noun}!`
 }
+
+// Outputs a sentence string based on input strings that is joined via literal concatenation
