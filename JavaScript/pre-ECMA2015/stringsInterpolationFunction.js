99053cace6d3b4f1dd9a46239f31dbdcdb1b390c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-10-29 15:16:09 +0800

    Update stringsInterpolationFunction.js

--- JavaScript/pre-ECMA2015/stringsInterpolationFunction.js
+++ JavaScript/pre-ECMA2015/stringsInterpolationFunction.js
@@ -1,3 +1,5 @@
 function reportingForDuty(rank, lastName) {
   return `${rank} ${lastName} reporting for duty!`
 };
+
+// Outputs string with interpolated string values displaying the rank and last name of the person
