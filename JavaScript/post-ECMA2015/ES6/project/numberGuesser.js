c54fb6f196cbfd77c4ed2b4ddebd8015d3278777
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-01 12:19:09 +0800

    Update numberGuesser.js

--- JavaScript/post-ECMA2015/ES6/project/numberGuesser.js
+++ JavaScript/post-ECMA2015/ES6/project/numberGuesser.js
@@ -6,7 +6,6 @@ let currentRoundNumber = 1;
 const generateTarget = () => {
   return Math.floor(Math.random() * 9);
 };
-
 const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
   diffHuman = Math.abs(secretTarget - humanGuess);
   diffComputer = Math.abs(secretTarget - computerGuess);
@@ -18,3 +17,18 @@ const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
     return true
   }
 };
+
+const updateScore = (stringWinner) => {
+  if (stringWinner === 'human') {
+    humanScore +=1;
+  } else {
+    computerScore +=1;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber +=1;
+};
+
+
+
