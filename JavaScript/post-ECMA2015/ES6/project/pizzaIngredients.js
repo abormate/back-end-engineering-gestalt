f0e3f987268d0a469078b17dd89f133b333fdebc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-08 07:27:20 +0800

    Create pizzaIngredients.js

---
+++ JavaScript/post-ECMA2015/ES6/project/pizzaIngredients.js
@@ -0,0 +1,5 @@
+const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+const politelyDecline = (veg) => {
+      console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+}
