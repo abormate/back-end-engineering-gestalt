4cbbbee2d96457b6a4d8004c20e3e5dd1df5ae84
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-08 06:34:43 +0800

    Update babyAnimals.js

--- JavaScript/post-ECMA2015/ES6/project/babyAnimals.js
+++ JavaScript/post-ECMA2015/ES6/project/babyAnimals.js
@@ -1,4 +1,13 @@
-const animals = ['panda', 'turtle', 'giraffe', 'tiger', 'sloth', 'human'];
- 
-convertToBaby(animals); 
-// Should return ['baby panda', 'baby turtle', 'baby giraffe', 'baby hippo', 'baby sloth', 'baby human'];
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+result = [];
+const convertToBaby = array => {
+  for (let i=0; i<array.length; i++) {
+    result.push('baby ' + array[i]);
+  }
+  return result;
+}
+// When you're ready to test your code, uncomment the below and run:
+
+
+console.log(convertToBaby(animals)) 
