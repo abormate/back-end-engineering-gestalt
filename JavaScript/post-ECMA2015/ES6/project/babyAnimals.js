e83d5257d1ebdbd40c127d44fb1a44654329adba
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-08 06:22:06 +0800

    Create babyAnimals.js

---
+++ JavaScript/post-ECMA2015/ES6/project/babyAnimals.js
@@ -0,0 +1,4 @@
+const animals = ['panda', 'turtle', 'giraffe', 'tiger', 'sloth', 'human'];
+ 
+convertToBaby(animals); 
+// Should return ['baby panda', 'baby turtle', 'baby giraffe', 'baby hippo', 'baby sloth', 'baby human'];
