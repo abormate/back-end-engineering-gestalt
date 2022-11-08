8d0e274aa431fdfb8ea75ffc72d99aac40685433
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-08 08:12:59 +0800

    Update pizzaIngredients.js

--- JavaScript/post-ECMA2015/ES6/project/pizzaIngredients.js
+++ JavaScript/post-ECMA2015/ES6/project/pizzaIngredients.js
@@ -8,3 +8,7 @@ const politelyDecline = (veg) => {
 const declineEverything = array => {
   array.forEach(politelyDecline);
 };
+
+const acceptEverything = array => {
+  array.forEach(element => console.log('Ok, I guess I will eat some ' + element + '.'));
+}
