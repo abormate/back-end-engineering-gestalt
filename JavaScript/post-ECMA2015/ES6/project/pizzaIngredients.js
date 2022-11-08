a152225db408cb7ed0f019c976172602bc797288
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-08 08:08:23 +0800

    Update pizzaIngredients.js

--- JavaScript/post-ECMA2015/ES6/project/pizzaIngredients.js
+++ JavaScript/post-ECMA2015/ES6/project/pizzaIngredients.js
@@ -3,3 +3,8 @@ const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
 const politelyDecline = (veg) => {
       console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
 }
+
+// Write your code here:
+const declineEverything = array => {
+  array.forEach(politelyDecline);
+};
