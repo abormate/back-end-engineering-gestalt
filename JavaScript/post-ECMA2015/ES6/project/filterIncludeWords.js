dbbf38d5413ecb554f51fc3976aee6c84e87488b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-09 13:22:59 +0800

    Update filterIncludeWords.js

--- JavaScript/post-ECMA2015/ES6/project/filterIncludeWords.js
+++ JavaScript/post-ECMA2015/ES6/project/filterIncludeWords.js
@@ -1,3 +1,9 @@
 const justCoolStuff = (array1, array2) => {
   return array1.filter(word => array2.includes(word));
 }
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
