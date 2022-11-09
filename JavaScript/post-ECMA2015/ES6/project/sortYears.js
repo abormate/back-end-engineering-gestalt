8b2e4f77473878ad42119dc40793f4679c7fa001
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-09 11:59:00 +0800

    Update sortYears.js

--- JavaScript/post-ECMA2015/ES6/project/sortYears.js
+++ JavaScript/post-ECMA2015/ES6/project/sortYears.js
@@ -3,3 +3,5 @@ const sortYears = array => array.sort((x,y) => y - x);
 const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
 
 console.log(sortYears(years))
+
+// prints out a list of numbers/years arranged in the descending order. By default sort() arranges items alphabetically in ascending order.  
