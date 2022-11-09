59a8ec0e72848b9089867e76f1cacf0040c9db22
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-09 10:23:55 +0800

    Update shoutGreetings.js

--- JavaScript/post-ECMA2015/ES6/project/shoutGreetings.js
+++ JavaScript/post-ECMA2015/ES6/project/shoutGreetings.js
@@ -1,3 +1,7 @@
 const shoutGreetings = array => {
   array.map(word => word.toUpperCase() + "!");
 }
+
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+console.log(shoutGreetings(greetings))
