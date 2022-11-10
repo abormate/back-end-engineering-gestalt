46f212b94859eac42d449b9472981f3189d630a8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-10 10:00:42 +0800

    Create findKeysIndex.js

---
+++ JavaScript/post-ECMA2015/projects/findKeysIndex.js
@@ -0,0 +1,14 @@
+const findMyKeys = array => array.indexOf('keys');
+
+
+
+
+
+
+
+
+// Feel free to comment out the code below to test your function
+
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'spoon'];
+
+console.log(findMyKeys(randomStuff))
