5ee339d1c964b5285097f853100fe708ddfcd9b0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-14 09:01:33 +0800

    Update groceries.js

--- JavaScript/post-ECMA2015/projects/groceries.js
+++ JavaScript/post-ECMA2015/projects/groceries.js
@@ -13,3 +13,18 @@ groceries( [{item: 'Cheese Balls'}] );
 // returns 'Cheese Balls'
 
 /*
+
+const groceries = list => {
+  let listString = ''
+
+  for (let i=0; i<list.length; i++) {
+    listString += list[i].item;
+    if (i < list.length - 2) {
+      listString += ', ';
+    } else if (i == list.length - 2){
+      listString += ' and ';
+    }
+  }
+  
+  return listString;
+}
