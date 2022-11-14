9ae7d5b6ee559a497d11e5ff36a8ad150adc91da
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-14 09:05:13 +0800

    Update factorialsNum.js

--- JavaScript/post-ECMA2015/projects/factorialsNum.js
+++ JavaScript/post-ECMA2015/projects/factorialsNum.js
@@ -1,3 +1,12 @@
+/*
+Write a function factorial() that takes a number as an argument and returns the factorial of the number.
+
+Example:
+
+factorial(6); 
+// returns `720` because 6 * 5 * 4 * 3 * 2 * 1 = 720 
+*/
+
 const factorial = num => {
   let result = 1;
   
