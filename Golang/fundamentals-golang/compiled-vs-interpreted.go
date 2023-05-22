ae46df8adbaa8060edf9bf4912e5f7ee30ab4ca3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-23 05:25:40 +0800

    Golang Tutorials

---
+++ Golang/fundamentals-golang/compiled-vs-interpreted.go
@@ -0,0 +1,28 @@
+// -------------------------------
+//
+// Compiled versus Interpreted?
+//
+// -------------------------------
+
+/*
+Compiled programs can be run without access to the original source code, and without access to a compiler. For 
+example, when your browser executes the code you write in this course, it doesn't use the original code, just 
+the compiled result.
+
+Note how this is different than interpreted languages like Python and JavaScript. With Python and JavaScript 
+the code is interpreted at runtime by a separate program known as the "interpreter". Distributing code for 
+users to run can be a pain because they need to have an interpreter installed, and they need access to the 
+original source code.
+*/
+
+// Examples of Compiled languages
+// -- Go
+// -- C
+// -- C++
+// -- Rust
+
+// Examples of Interpreted languages
+// -- JavaScript
+// -- Python
+// -- Ruby
+
