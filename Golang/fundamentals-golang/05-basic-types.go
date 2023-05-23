c1064bebac6c11e468f6e8dbce03938ca84da180
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-24 04:58:40 +0800

    Golang Tutorials

--- Golang/fundamentals-golang/05-basic-types.go
+++ Golang/fundamentals-golang/05-basic-types.go
@@ -22,3 +22,13 @@ float32 float64
 
 complex64 complex128
 
+/*
+We talked about strings and ints previously, and those two types should be fairly self-explanatory. A bool is 
+a boolean variable, meaning it has a value of true or false. The floating point types (float32 and float64) 
+are used for numbers that are not integers -- that is, they have digits to the right of the decimal place, 
+such as 3.14159. The float32 type uses 32 bits of precision, while the float64 type uses 64 bits to be able 
+to more precisely store more digits. Don't worry too much about the intricacies of the other types for now. 
+
+We will cover some of them in more detail as the course progresses.
+
+*/
