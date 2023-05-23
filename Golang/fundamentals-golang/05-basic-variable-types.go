3b9662e9d50e03bb1b69bec90a8025c035be0e42
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-24 05:16:45 +0800

    Golang Tutorials

--- Golang/fundamentals-golang/05-basic-variable-types.go
+++ Golang/fundamentals-golang/05-basic-variable-types.go
@@ -72,6 +72,10 @@ import "fmt"
 
 func main() {
 	// initialize variables here
+	var smsSendingLimit int = 0
+	var costPerSMS float64 = 0.0
+	var hasPermission bool = false
+	var username string = ""
 
 	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
