50e2665e38307922283bfadc6b045132e3cfc62f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-25 12:27:08 +0800

    Golang Code Writing

--- Golang/fundamentals-golang/07-type-inference-variable.go
+++ Golang/fundamentals-golang/07-type-inference-variable.go
@@ -16,6 +16,7 @@ var i int
 j := i // j is also an int
 
 /*
-
+However, when the right hand side is a literal value (an untyped numeric constant like 42 or 3.14), the new 
+variable will be an int, float64, or complex128 depending on its precision:
 
 */
