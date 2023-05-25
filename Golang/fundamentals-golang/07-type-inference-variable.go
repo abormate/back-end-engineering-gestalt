108342be9127a0a2f85278ca1fa9013572e2bc3b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-25 12:24:52 +0800

    Golang Programming

---
+++ Golang/fundamentals-golang/07-type-inference-variable.go
@@ -0,0 +1,14 @@
+// ---------------------------------
+//
+// Variable Type inference
+//
+// ---------------------------------
+
+/*
+To declare a variable without specifying an explicit type (either by using the := syntax or var = expression 
+	syntax), the variable's type is inferred from the value on the right hand side.
+
+When the right hand side of the declaration is typed, the new variable is of that same type:
+
+*/
+
