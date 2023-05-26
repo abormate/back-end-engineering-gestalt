cc5ecbc9191de2cc1680a1ccd2e9a8c61b185454
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-26 19:10:37 +0800

    Golang Programming Tutorials

---
+++ Golang/fundamentals-golang/15-functions.go
@@ -0,0 +1,26 @@
+// -------------------------- //
+//
+// Functions -- Golang
+//
+// -------------------------- //
+
+/*
+Functions in Go can take zero or more arguments.
+
+To make Go code easier to read, the variable type comes after the variable name.
+
+For example, the following function:
+
+*/
+
+func sub(x int, y int) int {
+	return x-y
+  }
+
+/*
+Takes in two integer parameters and returns another integer.
+
+Here, func sub(x int, y int) int is known as the "function signature".
+
+*/
+
