1e9106a00fd89ea5298d3653dd446422dfab2cbc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-26 18:51:51 +0800

    Golang Programming Tutorials

--- Golang/fundamentals-golang/13-conditionals.go
+++ Golang/fundamentals-golang/13-conditionals.go
@@ -41,3 +41,20 @@ Here are some of the comparison operators in Go:
 
 */
 
+package main
+
+import "fmt"
+
+func main() {
+	messageLen := 10
+	maxMessageLen := 20
+	fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
+
+	// don't touch above this line
+
+	if messageLen <= maxMessageLen {
+		fmt.Println("Message sent")
+	} else {
+		fmt.Println("Message not sent")
+	}
+}
