b6535ddc802833398117929d5983499cab4b8cdc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-27 07:14:32 +0800

    Golang Programming Tutorials

---
+++ Golang/fundamentals-golang/16-multiple-parameters.go
@@ -0,0 +1,11 @@
+// ---------------------------- //
+//
+// Multiple Parameters
+//
+// ---------------------------- //
+
+/*
+When multiple arguments are of the same type, the type only needs to be declared after the last one, 
+assuming they are in order.
+*/
+
