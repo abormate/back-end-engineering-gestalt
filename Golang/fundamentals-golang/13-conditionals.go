ea192dd57f84be8178353358f16ca6070edf94c9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-26 18:49:10 +0800

    Golang Programming

--- Golang/fundamentals-golang/13-conditionals.go
+++ Golang/fundamentals-golang/13-conditionals.go
@@ -10,3 +10,13 @@ if height > 4 {
     fmt.Println("You are tall enough!")
 }
 
+// else if and else are supported
+
+if height > 6 {
+    fmt.Println("You are super tall!")
+} else if height > 4 {
+    fmt.Println("You are tall enough!")
+} else {
+    fmt.Println("You are not tall enough!")
+}
+
