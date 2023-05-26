6a0bb0c32b8a813cc6639de6d2e1920206d7b96e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-26 18:46:51 +0800

    Golang Programming Tutorials

---
+++ Golang/fundamentals-golang/13-conditionals.go
@@ -0,0 +1,12 @@
+// ------------------------------- //
+//
+// Golang Conditionals
+//
+// ------------------------------- //
+
+// if statements in Go don't use parentheses around the condition:
+
+if height > 4 {
+    fmt.Println("You are tall enough!")
+}
+
