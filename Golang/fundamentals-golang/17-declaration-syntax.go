a0af0c3e8b9392b04f61d187bacac6abcba69343
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-27 07:31:46 +0800

    Golang Programming Tutorials

--- Golang/fundamentals-golang/17-declaration-syntax.go
+++ Golang/fundamentals-golang/17-declaration-syntax.go
@@ -30,3 +30,27 @@ signatures gets confusing really fast - take a look at this nightmare.
 
 int (*fp)(int (*ff)(int x, int y), int b)
 
+// ---------------------------- //
+//  Go-style syntax
+// ---------------------------- //
+
+// GO's declaration are clear. You need to read them left to right, just like you would English.
+
+x int
+p *int
+a [3]int
+
+// It's nice for more complex signatures, it makes them easier to read.
+
+f func(func(int,int) int, int) int
+
+// -------------------------- //
+//  Assignment -- Practice
+// -------------------------- //
+
+/*
+What are we talking about when we discuss declaration syntax?
+*/
+
+// The style of language used to create new variables, types, functions, et ceterra...
+
