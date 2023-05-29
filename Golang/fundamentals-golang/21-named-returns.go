ce370e1dac6ff533ab049206f22e557925639e32
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-30 06:02:56 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/21-named-returns.go
+++ Golang/fundamentals-golang/21-named-returns.go
@@ -10,4 +10,14 @@ from its signature, no need for a comment.
 
 Named return parameters are particularly important in longer functions with many return values.
 
+*/
+
+func calculator(a, b int) (mul, div int, err error) {
+    if b == 0 {
+      return 0, 0, errors.New("Can't divide by zero")
+    }
+    mul = a * b
+    div = a / b
+    return mul, div, nil
+}
 
