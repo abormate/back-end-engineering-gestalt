6fcf2953ecab04fae91042d744ba729e2c0728e3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-29 10:04:14 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/18-pass-values-variables.go
+++ Golang/fundamentals-golang/18-pass-values-variables.go
@@ -38,3 +38,17 @@ Fix main() to capture the return value from incrementSends() and overwrite the p
 
 */
 
+package main
+
+import "fmt"
+
+func main() {
+	sendsSoFar := 430
+	const sendsToAdd = 25
+	sendsSoFar = incrementSends(sendsSoFar, sendsToAdd)
+	fmt.Println("you've sent", sendsSoFar, "messages")
+}
+
+func incrementSends(sendsSoFar, sendsToAdd int) int {
+	return sendsSoFar + sendsToAdd
+}
