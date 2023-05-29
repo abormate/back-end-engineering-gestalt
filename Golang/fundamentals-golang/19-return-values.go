891a141a8290ae01de06089374122492ad593c6f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-29 10:06:16 +0800

    Go Programming Tutorials

---
+++ Golang/fundamentals-golang/19-return-values.go
@@ -0,0 +1,14 @@
+// -------------------------------- //
+//
+// Return Values
+//
+// -------------------------------- //
+
+/*
+A function can return a value that the caller doesn't care about. We can explicitly ignore variables 
+by using an underscore: _
+
+For example:
+
+*/
+
