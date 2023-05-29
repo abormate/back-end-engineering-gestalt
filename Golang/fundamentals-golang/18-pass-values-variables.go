0dea60b5433ef1a41aefb04ee5c74fcd46ce4b09
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-29 09:55:47 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/18-pass-values-variables.go
+++ Golang/fundamentals-golang/18-pass-values-variables.go
@@ -24,3 +24,17 @@ func main(){
 func increment(x int){
     x++
 }
+
+// ------------------------ //
+//  Assignment -- Practice
+// ------------------------ //
+
+/*
+It's critical in Textio that we keep track of how many SMS messages we send on behalf of our clients. 
+Fix the bug to accurately track the number of SMS messages sent.
+
+Modify the incrementSends function so that it returns the result after incrementing the sendsSoFar.
+Fix main() to capture the return value from incrementSends() and overwrite the previous sendsSoFar value.
+
+*/
+
