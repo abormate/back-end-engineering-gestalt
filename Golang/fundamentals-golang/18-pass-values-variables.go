957ba079d4bb46d1eb75002c342602a9400d6269
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-29 09:52:09 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/18-pass-values-variables.go
+++ Golang/fundamentals-golang/18-pass-values-variables.go
@@ -4,3 +4,10 @@
 //
 // ------------------------------ //
 
+/*
+Variables in Go are passed by value (except for a few data types we haven't covered yet). "Pass by value" means 
+that when a variable is passed into a function, that function receives a copy of the variable. The function is 
+unable to mutate the caller's original data
+
+*/
+
