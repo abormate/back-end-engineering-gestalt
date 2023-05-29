2168f8f376b42dbf259c5505d8cebac055b2de1e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-30 06:01:47 +0800

    Go Programming Tutorials

---
+++ Golang/fundamentals-golang/21-named-returns.go
@@ -0,0 +1,13 @@
+// --------------------------------- //
+//
+// Named Returns
+//
+// --------------------------------- //
+
+/*
+Named return parameters are great for documenting a function. We know what the function is returning directly 
+from its signature, no need for a comment.
+
+Named return parameters are particularly important in longer functions with many return values.
+
+
