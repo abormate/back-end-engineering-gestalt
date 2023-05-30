cd477ec35d95ed97f645b707fcd7211e330ba656
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-31 03:33:01 +0800

    Golang Programming Tutorials

---
+++ Golang/fundamentals-golang/23-structs.go
@@ -0,0 +1,19 @@
+// ------------------------------------ //
+//
+// Structs 
+//
+// ------------------------------------ //
+
+/*
+We use structs in Go to represent structured data. It's often convenient to group different types of variables 
+together. For example, if we want to represent a car we could do the following:
+
+*/
+
+type car struct {
+	Make string
+	Model string
+	Height int
+	Width int
+  }
+
