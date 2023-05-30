ea89911477e1cfdf5905f00697552e20889a8ffe
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-05-31 02:43:11 +0800

    Golang Programming Tutorials

--- Golang/fundamentals-golang/21-named-returns.go
+++ Golang/fundamentals-golang/21-named-returns.go
@@ -21,3 +21,44 @@ func calculator(a, b int) (mul, div int, err error) {
     return mul, div, nil
 }
 
+// Which is easier to understand than...
+
+func calculator(a, b int) (int, int, error) {
+    if b == 0 {
+      return 0, 0, errors.New("Can't divide by zero")
+    }
+    mul := a * b
+    div := a / b
+    return mul, div, nil
+}
+
+/*
+We know the meaning of each return value just by looking at the function signature: func calculator(a, b int) 
+(mul, div int, err error)
+
+*/
+
+// --------------------------- //
+//  Less Code
+// --------------------------- //
+
+/*
+If there are multiple return statements in a function, you don’t need to write all the return values each time, 
+though you probably should.
+
+When you choose to omit return values, it's called a naked return. Naked returns should only be used in short 
+and simple functions.
+
+*/
+
+// ------------------------- //
+//  Practice -- Question: When should naked returns be used?
+// ------------------------- //
+
+// For small functions
+
+// --------------------------- //
+//  Question 2: When should named returns be used?
+// --------------------------- //
+
+// When there are many values being returned
