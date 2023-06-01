e5c83b3bdb3faab0b7a8b831099662ddcd8866f4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-01 13:03:13 +0800

    Golang Programming Tutorials

--- Golang/fundamentals-golang/26-embedded-structs.go
+++ Golang/fundamentals-golang/26-embedded-structs.go
@@ -11,3 +11,15 @@ and share fields between struct definitions.
 
 */
 
+type car struct {
+	make string
+	model string
+  }
+  
+  type truck struct {
+	// "car" is embedded, so the definition of a
+	// "truck" now also additionally contains all
+	// of the fields of the car struct
+	car
+	bedSize int
+  }
