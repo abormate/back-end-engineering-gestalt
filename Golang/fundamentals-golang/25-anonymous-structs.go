020f1b19291b35c91c0a54675073ede4942411a8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-01 12:50:28 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/25-anonymous-structs.go
+++ Golang/fundamentals-golang/25-anonymous-structs.go
@@ -4,3 +4,8 @@
 //
 // ---------------------------------- //
 
+/*
+An anonymous struct is just like a normal struct, but it is defined without a name and therefore cannot be referenced 
+elsewhere in the code.
+
+*/
