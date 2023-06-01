5dfc8cb8187d34f8f8e3afd2061c3e9538d9ba85
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-01 12:52:02 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/25-anonymous-structs.go
+++ Golang/fundamentals-golang/25-anonymous-structs.go
@@ -14,3 +14,11 @@ elsewhere in the code.
 To create an anonymous struct, just instantiate the instance immediately using a second pair of brackets after declaring the type:
 
 */
+
+myCar := struct {
+	Make string
+	Model string
+  } {
+	Make: "tesla",
+	Model: "model 3"
+  }
