7f208731c384bcddc07cdcdd5c7951180917d69d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-01 13:02:14 +0800

    Go Programming Tutorials

---
+++ Golang/fundamentals-golang/26-embedded-structs.go
@@ -0,0 +1,13 @@
+// -------------------------------- //
+//
+// Embedded Structs
+//
+// -------------------------------- //
+
+/*
+Go is not an object-oriented language. However, embedded structs provide a kind of data-only inheritance that can be useful at 
+times. Keep in mind, Go doesn't support classes or inheritance in the complete sense, embedded structs are just a way to elevate 
+and share fields between struct definitions.
+
+*/
+
