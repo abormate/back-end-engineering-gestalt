7c8ce14875786cfba63c01f44a8da88be3055af5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-05 17:56:10 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/27-struct-methods.go
+++ Golang/fundamentals-golang/27-struct-methods.go
@@ -5,3 +5,7 @@
 // -------------------------------- //
 
 /*
+While Go is not object-oriented, it does support methods that can be defined on structs. Methods are just functions that have a 
+receiver. A receiver is a special parameter that syntactically goes before the name of the function.
+
+*/
