0ec52f32756ae1e4ed6e1d2fba09c00e71a58ef5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-05 17:56:37 +0800

    Golang Programming Tutorials

--- Golang/fundamentals-golang/27-struct-methods.go
+++ Golang/fundamentals-golang/27-struct-methods.go
@@ -9,3 +9,21 @@ While Go is not object-oriented, it does support methods that can be defined on 
 receiver. A receiver is a special parameter that syntactically goes before the name of the function.
 
 */
+
+type rect struct {
+	width int
+	height int
+  }
+  
+  // area has a receiver of (r rect)
+  func (r rect) area() int {
+	return r.width * r.height
+  }
+  
+  r := rect{
+	width: 5,
+	height: 10,
+  }
+  
+  fmt.Println(r.area())
+  // prints 50
