f92e513c0c07d66edd429d5677618a12ea954556
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-07 07:46:18 +0800

    Golang Programming Tutorials

--- Golang/fundamentals-golang/29-interfaces-implementation.go
+++ Golang/fundamentals-golang/29-interfaces-implementation.go
@@ -25,3 +25,63 @@ Add the missing getSalary method to the contractor type so that it fulfills the 
 A contractor's salary is their hourly pay multiplied by how many hours they work per year.
 
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+type employee interface {
+	getName() string
+	getSalary() int
+}
+
+type contractor struct {
+	name         string
+	hourlyPay    int
+	hoursPerYear int
+}
+
+func (c contractor) getName() string {
+	return c.name
+}
+
+// ?
+
+// don't touch below this line
+
+type fullTime struct {
+	name   string
+	salary int
+}
+
+func (ft fullTime) getSalary() int {
+	return ft.salary
+}
+
+func (ft fullTime) getName() string {
+	return ft.name
+}
+
+func test(e employee) {
+	fmt.Println(e.getName(), e.getSalary())
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(fullTime{
+		name:   "Jack",
+		salary: 50000,
+	})
+	test(contractor{
+		name:         "Bob",
+		hourlyPay:    100,
+		hoursPerYear: 73,
+	})
+	test(contractor{
+		name:         "Jill",
+		hourlyPay:    872,
+		hoursPerYear: 982,
+	})
+}
