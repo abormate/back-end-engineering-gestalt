9fb67f218f44b4881e39b4e14acefed6c44aa001
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-06 10:36:08 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/28-interfaces-with-golang.go
+++ Golang/fundamentals-golang/28-interfaces-with-golang.go
@@ -4,3 +4,10 @@
 //
 // ---------------------------------- //
 
+/*
+Interfaces are collections of method signatures. A type "implements" an interface if it has all of the methods of the given 
+interface defined on it.
+
+In the following example, a "shape" must be able to return its area and perimeter. Both rect and circle fulfill the interface.
+
+*/
