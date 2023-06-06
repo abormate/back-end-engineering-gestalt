5eddbd3d7f74260035186d4a6756717ee14bd521
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-06 10:18:39 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/27-struct-methods.go
+++ Golang/fundamentals-golang/27-struct-methods.go
@@ -27,3 +27,58 @@ type rect struct {
   
   fmt.Println(r.area())
   // prints 50
+
+// ------------------- //
+//  Assignment -- Practice
+// ------------------- //
+
+/*
+Let's clean up Textio's authentication logic. We store our user's authentication data inside an authenticationInfo struct. 
+We need a method that can take that data and return a basic authorization string.
+
+The format of the string should be:
+
+Authorization: Basic USERNAME:PASSWORD
+
+
+Create a method on the authenticationInfo struct called getBasicAuth that returns the formatted string.
+
+*/
+
+package main
+
+import "fmt"
+
+type authenticationInfo struct {
+	username string
+	password string
+}
+
+// ?
+
+func (authInfo authenticationInfo) getBasicAuth() string {
+	return "Authorization: Basic"  + authInfo.username + ":" + authInfo.password
+}
+
+
+// don't touch below this line
+
+func test(authInfo authenticationInfo) {
+	fmt.Println(authInfo.getBasicAuth())
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(authenticationInfo{
+		username: "Google",
+		password: "12345",
+	})
+	test(authenticationInfo{
+		username: "Bing",
+		password: "98765",
+	})
+	test(authenticationInfo{
+		username: "DDG",
+		password: "76921",
+	})
+}
