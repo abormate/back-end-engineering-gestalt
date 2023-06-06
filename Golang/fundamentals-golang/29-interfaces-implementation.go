857a3a91ff9e736885fc7ca347d160edf919495f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-07 07:45:50 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/29-interfaces-implementation.go
+++ Golang/fundamentals-golang/29-interfaces-implementation.go
@@ -11,3 +11,17 @@ A type never declares that it implements a given interface. If an interface exis
 then the type automatically fulfills that interface.
 
 */
+
+// ------------------------- //
+//  Assignment -- Practice
+// ------------------------- //
+
+/*
+At Textio we have full-time employees and contract employees. We have been tasked with making a more general employee interface 
+so that dealing with different employee types is simpler.
+
+Add the missing getSalary method to the contractor type so that it fulfills the employee interface.
+
+A contractor's salary is their hourly pay multiplied by how many hours they work per year.
+
+*/
