022459a50861a8412476cf1ccbb0bcdc85ad8a73
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-06 10:36:47 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/28-interfaces-with-golang.go
+++ Golang/fundamentals-golang/28-interfaces-with-golang.go
@@ -11,3 +11,28 @@ interface defined on it.
 In the following example, a "shape" must be able to return its area and perimeter. Both rect and circle fulfill the interface.
 
 */
+
+type shape interface {
+	area() float64
+	perimeter() float64
+  }
+  
+  type rect struct {
+	  width, height float64
+  }
+  func (r rect) area() float64 {
+	  return r.width * r.height
+  }
+  func (r rect) perimeter() float64 {
+	  return 2*r.width + 2*r.height
+  }
+  
+  type circle struct {
+	  radius float64
+  }
+  func (c circle) area() float64 {
+	  return math.Pi * c.radius * c.radius
+  }
+  func (c circle) perimeter() float64 {
+	  return 2 * math.Pi * c.radius
+  }
