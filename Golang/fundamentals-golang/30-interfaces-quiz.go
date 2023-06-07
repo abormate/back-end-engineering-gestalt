98193821fd56e079e0baed6888c75be2b4c41469
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-07 08:18:19 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/30-interfaces-quiz.go
+++ Golang/fundamentals-golang/30-interfaces-quiz.go
@@ -14,3 +14,27 @@ type shape interface {
 	area() float64
   }
 
+/*
+If a type in your code implements an area method, with the same signature (e.g. accepts nothing and returns a float64), then that 
+object is said to implement the shape interface.
+
+*/
+
+type circle struct{
+	radius int
+}
+
+func (c *circle) area() float64 {
+  return 3.14 * c.radius * c.radius
+}
+
+/*
+This is different from most other languages, where you have to explicitly assign an interface type to an object, like with Java:
+
+*/
+
+class Circle implements Shape
+
+// ------------------------- //
+//
+// ------------------------- //
