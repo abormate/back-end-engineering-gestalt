eab794bbea6ffbb445ce1820c1570a9fdc5a9799
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-08 12:44:31 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/31-multiple-interfaces.go
+++ Golang/fundamentals-golang/31-multiple-interfaces.go
@@ -4,3 +4,9 @@
 //
 // ------------------------------------ //
 
+/*
+A type can implement any number of interfaces in Go. For example, the empty interface, interface{}, is always implemented by every 
+type because it has no requirements.
+
+*/
+
