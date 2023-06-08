0ff78124efd53c2fbaec5d423f058c64daa481d8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-08 12:50:02 +0800

    Golang Programming Tutorials

--- Golang/fundamentals-golang/31-multiple-interfaces.go
+++ Golang/fundamentals-golang/31-multiple-interfaces.go
@@ -10,3 +10,13 @@ type because it has no requirements.
 
 */
 
+// -------------------------- //
+//  Assignment -- Practice
+// -------------------------- //
+
+/*
+Add the required methods so that the email type implements both the expense and printer interfaces.
+
+*/
+
+
