72b66818cf6e3f0bdfc0480666fd939732cd79c1
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-09 15:26:12 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/32-interface-arguments.go
+++ Golang/fundamentals-golang/32-interface-arguments.go
@@ -12,3 +12,10 @@ type Copier interface {
 
 // Based on the code alone, can you deduce what kinds of strings you should pass into the Copy function?
 
+/*
+We know the function signature expects 2 string types, but what are they? Filenames? URLs? Raw string data? For that matter, 
+what the heck is that int that's being returned?
+
+Let's add some named arguments and return data to make it more clear.
+
+*/
