1e5d2a8d01ce8d248e1c11dc838ba2bafa21ab23
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-10 10:25:51 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/34-type-switches.go
+++ Golang/fundamentals-golang/34-type-switches.go
@@ -4,3 +4,9 @@
 //
 // ------------------------------------ //
 
+/*
+A type switch makes it easy to do several type assertions in a series.
+
+A type switch is similar to a regular switch statement, but the cases specify types instead of values.
+
+*/
