264f84bea68f6a29dd2553ead578c38dd0a1f1af
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-10 10:11:32 +0800

    Go Programming Tutorials

--- Golang/fundamentals-golang/33-type-assertions.go
+++ Golang/fundamentals-golang/33-type-assertions.go
@@ -4,3 +4,9 @@
 //
 // ----------------------------------- //
 
+/*
+When working with interfaces in Go, every once-in-awhile you'll need access to the underlying type of an interface value. 
+You can cast an interface to its underlying type using a type assertion.
+
+*/
+
