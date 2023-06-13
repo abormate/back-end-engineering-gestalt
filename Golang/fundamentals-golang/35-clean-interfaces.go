5413e7936ee00cf0920a831b60346accf073cdaa
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-13 08:39:43 +0800

    Golang Tutorials - Keep Interfaces Clean

--- Golang/fundamentals-golang/35-clean-interfaces.go
+++ Golang/fundamentals-golang/35-clean-interfaces.go
@@ -4,3 +4,9 @@
 // 
 // ----------------------------------- //
 
+/*
+Writing clean interfaces is hard. Frankly, anytime you’re dealing with abstractions in code, the simple can become complex 
+very quickly if you’re not careful. Let’s go over some rules of thumb for keeping interfaces clean.
+
+*/
+
