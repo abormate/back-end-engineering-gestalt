ea9f5f67afade61276d3b741bf013f463224a254
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-14 18:42:27 +0800

    Golang Programming -- Error Interface

--- Golang/fundamentals-golang/36-error-interface.go
+++ Golang/fundamentals-golang/36-error-interface.go
@@ -18,3 +18,16 @@ When something can go wrong in a function, that function should return an error 
 Any code that calls a function that can return an error should handle errors by testing whether the error is nil.
 
 */
+
+// Atoi converts a stringified number to an integer
+i, err := strconv.Atoi("42b")
+if err != nil {
+    fmt.Println("couldn't convert:", err)
+    // because "42b" isn't a valid integer, we print:
+    // couldn't convert: strconv.Atoi: parsing "42b": invalid syntax
+    // Note:
+    // 'parsing "42b": invalid syntax' is returned by the .Error() method
+    return
+}
+// if we get here, then
+// i was converted successfully
