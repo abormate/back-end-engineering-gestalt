0b31792b02f6cbabfafa29eeb3b24e31301ec3d1
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-15 10:08:59 +0800

    Golang Programming -- Strings Formatting

--- Golang/fundamentals-golang/37-formatting-strings.go
+++ Golang/fundamentals-golang/37-formatting-strings.go
@@ -54,3 +54,28 @@ Complete the getSMSErrorString() function. It should return a string with this f
 Be sure to include the $ symbol and the single quotes
 
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+func getSMSErrorString(cost float64, recipient string) string {
+	// ?
+}
+
+// don't edit below this line
+
+func test(cost float64, recipient string) {
+	s := getSMSErrorString(cost, recipient)
+	fmt.Println(s)
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(1.4, "+1 (435) 555 0923")
+	test(2.1, "+2 (702) 555 3452")
+	test(32.1, "+1 (801) 555 7456")
+	test(14.4, "+1 (234) 555 6545")
+}
