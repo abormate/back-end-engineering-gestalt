f8f046a419b13d986d720edc3d8a243f040f2350
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-15 10:04:33 +0800

    Golang Programming -- Strings Format

--- Golang/fundamentals-golang/37-formatting-strings.go
+++ Golang/fundamentals-golang/37-formatting-strings.go
@@ -3,3 +3,11 @@
 // Formatting Strings
 //
 // ------------------------------------ //
+
+/*
+A convenient way to format strings in Go is by using the standard library's fmt.Sprintf() function. It's a string interpolation 
+function, similar to JavaScript's built-in template literals. The %v substring uses the type's default formatting, which is often 
+what you want.
+
+*/
+
