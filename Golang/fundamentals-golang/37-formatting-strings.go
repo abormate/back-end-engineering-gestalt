c19c6ee63e46d0e1ae9d1b30f320ec10a0c281b6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-15 10:08:36 +0800

    Golang Programming -- Strings Formatting

--- Golang/fundamentals-golang/37-formatting-strings.go
+++ Golang/fundamentals-golang/37-formatting-strings.go
@@ -12,21 +12,45 @@ what you want.
 */
 
 // Default Values
+// -----------------
 const name = "Kim"
 const age = 22
 s := fmt.Sprintf("%v is %v years old.", name, age)
 // s = "Kim is 22 years old."
 
 // Equivalent in JavaScript code:
+// ---------------------------------
 const name = 'Kim'
 const age = 22
 s = `${name} is ${age} years old.`
 // s = "Kim is 22 years old."
 
+// ----------------------
 // Rounding Float values
+// ----------------------
+
 fmt.Printf("I am %f years old", 10.523)
 // I am 10.523000 years old
 
 // The ".2" rounds the number to 2 decimal places
 fmt.Printf("I am %.2f years old", 10.523)
 // I am 10.53 years old
+
+// --------------------------- //
+//  Assignment -- Practice
+// --------------------------- //
+
+/*
+We need better error logs for our backend developers to help them debug their code.
+
+Complete the getSMSErrorString() function. It should return a string with this format:
+
+// -->> SMS that costs $COST to be sent to 'RECIPIENT' can not be sent
+
+-- COST is the cost of the SMS, always showing the price formatted to 2 decimal places.
+-- RECIPIENT is the stringified representation of the recipient's phone number
+
+
+Be sure to include the $ symbol and the single quotes
+
+*/
