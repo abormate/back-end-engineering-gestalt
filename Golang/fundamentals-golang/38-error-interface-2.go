98617c1a2dc5c6e704a33d04578033967e774962
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-16 17:28:25 +0800

    Go Programming -- Error Interface

--- Golang/fundamentals-golang/38-error-interface-2.go
+++ Golang/fundamentals-golang/38-error-interface-2.go
@@ -10,3 +10,10 @@ a userError struct that implements the error interface:
 
 */
 
+type userError struct {
+    name string
+}
+
+func (e userError) Error() string {
+    return fmt.Sprintf("%v has a problem with their account", e.name)
+}
