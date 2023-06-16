eb10a3b4a6f446fb22851ec029bb0b0516fd3793
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-16 17:27:29 +0800

    Go Programming -- Error Interface

--- Golang/fundamentals-golang/38-error-interface-2.go
+++ Golang/fundamentals-golang/38-error-interface-2.go
@@ -3,3 +3,10 @@
 // Error Interface -- Golang -- Part 2
 //
 // ------------------------------------ //
+
+/*
+Because errors are just interfaces, you can build your own custom types that implement the error interface. Here's an example of 
+a userError struct that implements the error interface:
+
+*/
+
