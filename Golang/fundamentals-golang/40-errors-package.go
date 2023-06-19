3e994e25d4e371f6bd8f84a759a7223815fe5cdf
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-19 13:37:57 +0800

    Golang Programming -- Errors Package

--- Golang/fundamentals-golang/40-errors-package.go
+++ Golang/fundamentals-golang/40-errors-package.go
@@ -7,4 +7,8 @@
 /*
 The Go standard library provides an "errors" package that makes it easy to deal with errors.
 
+Read the godoc for the errors.New() function, but here's a simple example:
+
 */
+
+var err error = errors.New("something went wrong")
