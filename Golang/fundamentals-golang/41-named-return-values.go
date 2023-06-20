1e625b3dc14354d4a4290bd13330f8279124f5f9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-20 12:45:32 +0800

    Golang Programming -- Named Return Values

--- Golang/fundamentals-golang/41-named-return-values.go
+++ Golang/fundamentals-golang/41-named-return-values.go
@@ -30,3 +30,61 @@ func getCoords() (int, int){
 	var y int
 	return x, y
 }
+
+/*
+In the first example, x and y are the return values. At the end of the function, we could simply write return to return the 
+values of those two variables, rather that writing return x,y.
+
+*/
+
+// -------------------- //
+// ASSIGNMENT -- Practice
+// -------------------- //
+
+/*
+
+One of our clients likes us to send text messages reminding users of life events coming up.
+
+Fix the bug by using named return values in the function signature so the code will compile and run as intended.
+
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func yearsUntilEvents(age int) (int, int, int) {
+	yearsUntilAdult = 18 - age
+	if yearsUntilAdult < 0 {
+		yearsUntilAdult = 0
+	}
+	yearsUntilDrinking = 21 - age
+	if yearsUntilDrinking < 0 {
+		yearsUntilDrinking = 0
+	}
+	yearsUntilCarRental = 25 - age
+	if yearsUntilCarRental < 0 {
+		yearsUntilCarRental = 0
+	}
+	return
+}
+
+// don't edit below this line
+
+func test(age int) {
+	fmt.Println("Age:", age)
+	yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental := yearsUntilEvents(age)
+	fmt.Println("You are an adult in", yearsUntilAdult, "years")
+	fmt.Println("You can drink in", yearsUntilDrinking, "years")
+	fmt.Println("You can rent a car in", yearsUntilCarRental, "years")
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(4)
+	test(10)
+	test(22)
+	test(35)
+}
