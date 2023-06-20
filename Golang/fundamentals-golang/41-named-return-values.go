ef206d3b62b7aecd1a96964cf854f4b9b85fdf30
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-20 12:39:50 +0800

    Golang Programming -- Named Return Values

--- Golang/fundamentals-golang/41-named-return-values.go
+++ Golang/fundamentals-golang/41-named-return-values.go
@@ -4,3 +4,12 @@
 //
 // ------------------------------------- //
 
+/*
+Return values may be given names, and if they are, then they are treated the same as if they were new variables defined at the 
+top of the function.
+
+Named return values are best thought of as a way to document the purpose of the returned values.
+
+According to the tour of go:
+
+*/
