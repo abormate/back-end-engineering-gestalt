b189c2f21ac5a6895dfe7385533a181bb9e9437d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-21 16:30:08 +0800

    Go Programming -- Implicit Named Return Values

--- Golang/fundamentals-golang/42-named-return-values-2.go
+++ Golang/fundamentals-golang/42-named-return-values-2.go
@@ -6,6 +6,14 @@
 
 // Even though a function has named return values, we can still explicitly return values if we want to.
 
+<<<<<<< HEAD
 func getCoords() (x, y int){
 	return x, y // this is explicit
   }
+
+// 
+=======
+func getCoords() (x, y int){
+	return x, y // this is explicit
+  }
+>>>>>>> bc37ddbe24ec4a86decd771ac3af8a2b3dc9dd8a
