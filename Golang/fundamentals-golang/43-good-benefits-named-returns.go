1cea3651058baf78bfbfb55ad5298bb1c274c38d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-22 08:51:06 +0800

    Go Programming -- Named Return Values -- Good Stuff to Implement

--- Golang/fundamentals-golang/43-good-benefits-named-returns.go
+++ Golang/fundamentals-golang/43-good-benefits-named-returns.go
@@ -27,3 +27,11 @@ func calculator(a, b int) (mul, div int, err error) {
 
 // Which is easier to understand than:
 
+func calculator(a, b int) (int, int, error) {
+  if b == 0 {
+    return 0, 0, errors.New("Can't divide by zero")
+  }
+  mul := a * b
+  div := a / b
+  return mul, div, nil
+}
