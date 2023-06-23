5f4638eadc0aabecff47605f71f474f6486c0c5e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-23 08:45:13 +0800

    Go Programming -- Early Returns

--- Golang/fundamentals-golang/44-early-return-values.go
+++ Golang/fundamentals-golang/44-early-return-values.go
@@ -8,4 +8,7 @@
 Go supports the ability to return early from a function. This is a powerful feature that can clean up code, especially when 
 used as guard clauses.
 
+Guard Clauses leverage the ability to return early from a function (or continue through a loop) to make nested conditionals 
+one-dimensional. Instead of using if/else chains, we just return early from the function at the end of each conditional block.
+
 */
