4c5de10a95af9ba4ed9246a995f75fa6ee6df95c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-23 08:50:57 +0800

    Go Programming -- Early Return Values

--- Golang/fundamentals-golang/44-early-return-values.go
+++ Golang/fundamentals-golang/44-early-return-values.go
@@ -28,3 +28,23 @@ Lets take a look at an exaggerated example of nested conditional logic:
 
 */
 
+func getInsuranceAmount(status insuranceStatus) int {
+	amount := 0
+	if !status.hasInsurance(){
+	  amount = 1
+	} else {
+	  if status.isTotaled(){
+		amount = 10000
+	  } else {
+		if status.isDented(){
+		  amount = 160
+		  if status.isBigDent(){
+			amount = 270
+		  }
+		} else {
+		  amount = 0
+		}
+	  }
+	}
+	return amount
+  }
