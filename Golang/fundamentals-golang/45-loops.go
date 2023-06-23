07804728ed550f650ca3ff323952e1206b52752b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-23 09:21:39 +0800

    Go Programming -- Simple Loops

--- Golang/fundamentals-golang/45-loops.go
+++ Golang/fundamentals-golang/45-loops.go
@@ -27,3 +27,24 @@ for i := 0; i < 10; i++ {
 	fmt.Println(i)
   }
   // Prints 0 through 9
+
+
+// ----------------------- //
+// Assignment -- Practice
+// ----------------------- //
+
+/*
+At Textio we have a dynamic formula for determining how much a batch of bulk messages costs to send.
+
+COMPLETE THE BULKSEND() FUNCTION
+This function should return the total cost (as a float64) to send a batch of numMessages messages. Each message costs 1.0, plus 
+an additional fee. The fee structure is:
+
+1st message: 1.0 + 0.00
+2nd message: 1.0 + 0.01
+3rd message: 1.0 + 0.02
+4th message: 1.0 + 0.03
+...
+Use a loop to calculate the total cost and return it.
+
+*/
