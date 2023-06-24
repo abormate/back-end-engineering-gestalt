f1e0b8289b32fbc7719f2f9331d0517ccf876922
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-24 08:23:34 +0800

    Golang Programming -- For Loop Conditions Omit

--- Golang/fundamentals-golang/46-conditions-omit-loop.go
+++ Golang/fundamentals-golang/46-conditions-omit-loop.go
@@ -4,3 +4,9 @@
 //
 // -------------------------------------- //
 
+/*
+Loops in Go can omit sections of a for loop. For example, the CONDITION (middle part) can be omitted 
+which causes the loop to run forever.
+
+*/
+
