af3edd45edf7265adfcbc7a5f74a517c9c30401b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-24 08:34:00 +0800

    Go Programming -- Omit Conditions FOR Loop

--- Golang/fundamentals-golang/46-conditions-omit-loop.go
+++ Golang/fundamentals-golang/46-conditions-omit-loop.go
@@ -30,3 +30,29 @@ Each message costs 1.0, plus an additional fee. The fee structure is:
 
 */
 
+package main
+
+import (
+	"fmt"
+)
+
+func maxMessages(thresh float64) int {
+	// ?
+}
+
+// don't edit below this line
+
+func test(thresh float64) {
+	fmt.Printf("Threshold: %.2f\n", thresh)
+	max := maxMessages(thresh)
+	fmt.Printf("Maximum messages that can be sent: = %v\n", max)
+	fmt.Println("===============================================================")
+}
+
+func main() {
+	test(10.00)
+	test(20.00)
+	test(30.00)
+	test(40.00)
+	test(50.00)
+}
