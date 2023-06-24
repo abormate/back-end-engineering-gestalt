d9a37ebde6df823368a2ca413a639ac349d29912
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-24 09:05:36 +0800

    Go Programming -- No While Loop

--- Golang/fundamentals-golang/47-no-while-loop.go
+++ Golang/fundamentals-golang/47-no-while-loop.go
@@ -5,6 +5,12 @@
 // ------------------------------------ //
 
 /*
-
+Most programming languages have a concept of a while loop. Because Go allows for the omission of sections of a for loop, a while 
+loop is just a for loop that only has a CONDITION.
 
 */
+
+for CONDITION {
+	// do some stuff while CONDITION is true
+  }
+
