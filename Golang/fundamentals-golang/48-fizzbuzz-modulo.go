04f1a43f172429ebe1abaa6dacd8c87b045133eb
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-26 12:47:08 +0800

    Go Programming -- Modulo

--- Golang/fundamentals-golang/48-fizzbuzz-modulo.go
+++ Golang/fundamentals-golang/48-fizzbuzz-modulo.go
@@ -26,5 +26,9 @@ false || false // false
 We're hiring engineers at Textio, so time to brush up on the classic "Fizzbuzz" game, a coding exercise that has been dramatically 
 overused in coding interviews across the world.
 
+Complete the fizzbuzz function that prints the numbers 1 to 100 inclusive each on their own line, but substitutes multiples of 3 
+for the text fizz and multiples of 5 for buzz. For multiples of 3 AND 5 print instead fizzbuzz.
+
 */
+
 
