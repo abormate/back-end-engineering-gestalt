3801ff770c2821408e2c46ea0b487f8e45541db9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-29 18:58:04 +0800

    Go Programming -- Continue for Loops

--- Golang/fundamentals-golang/49-continue.go
+++ Golang/fundamentals-golang/49-continue.go
@@ -3,3 +3,10 @@
 // Continue -- Golang
 //
 // ------------------------------- //
+
+/*
+The continue keyword stops the current iteration of a loop and continues to the next iteration. continue is a powerful way 
+to use the "guard clause" pattern within loops.
+
+*/
+
