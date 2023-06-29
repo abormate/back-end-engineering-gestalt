ef28785214a6d124bfc2ee4dbf0290314db01fd4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-06-29 18:54:51 +0800

    Golang Programming -- Fizzbuzz

--- Golang/fundamentals-golang/48-fizzbuzz-modulo.go
+++ Golang/fundamentals-golang/48-fizzbuzz-modulo.go
@@ -34,7 +34,17 @@ for the text fizz and multiples of 5 for buzz. For multiples of 3 AND 5 print in
 package main
 
 func fizzbuzz() {
-	// ?
+	for i := 1; i < 101; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
 
 // don't touch below this line
