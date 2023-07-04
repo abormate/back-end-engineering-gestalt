7dafe3717df5a28f2984df17e358cc1cd66be1e0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-05 05:51:51 +0800

    Create 01-Fix-1st-Error.py
    Python Fundamentals -- 1st bug fix

---
+++ Python/Py-Fundamentals/01-Fix-1st-Error.py
@@ -0,0 +1,14 @@
+# ------------------------------------ #
+#
+# Python Programming for the back-end
+#
+# ------------------------------------ #
+
+"""
+FIX YOUR FIRST BUG
+The combat system in Fantasy Quest isn't working as intended! It appears that players are gaining health when attacked instead of losing it.
+
+ASSIGNMENT
+Fix the math bug on line 3.
+
+"""
