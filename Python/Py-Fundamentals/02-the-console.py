52a21ddbdaf9765775e17ab8759dde83b051befc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-05 06:00:51 +0800

    Python Programming -- The Console

--- Python/Py-Fundamentals/02-the-console.py
+++ Python/Py-Fundamentals/02-the-console.py
@@ -5,6 +5,11 @@
 # ------------------------------------- #
 
 """
+The "console" is a panel that displays important messages, like errors or the text output of a program. If 
+we want to see what's happening in our code, we need to print it to the console by using the print() function.
 
+We'll learn more about functions later, but for now, just know that the print() function will print anything 
+you put inside its parentheses.
 
 """
+
