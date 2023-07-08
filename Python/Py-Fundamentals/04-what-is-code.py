58b2aa6cf592b1a7a94bc6bad9c4ee3f10d21e03
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-08 19:01:33 +0800

    Python Programming -- What is Code

--- Python/Py-Fundamentals/04-what-is-code.py
+++ Python/Py-Fundamentals/04-what-is-code.py
@@ -4,3 +4,12 @@
 #
 # -------------------------------------- #
 
+"""
+Code is just a series of instructions that computers can follow. Computers obey each instruction, one after another.
+
+Programs can be comprised of many instructions. Like many. Like millions.
+
+Addition is one of the most common instructions in coding.
+
+
+"""
