c238a7d27a66a8e72cab6698a9f7c4ee81c337f8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-08 19:15:57 +0800

    Python Programming -- Multiple Instructions

--- Python/Py-Fundamentals/05-multiple-instructions.py
+++ Python/Py-Fundamentals/05-multiple-instructions.py
@@ -9,3 +9,28 @@
 print("this prints first")
 print("this prints second")
 print("this prints last")
+
+# Each print() instruction prints on a new line.
+
+# ----------------------------- #
+# Assignment -- Practice
+# ----------------------------- #
+
+"""
+The dialogue in our game needs to be printed in the correct order! In the first level, our hero encounters a talking owl 
+named Jax. Let's display their conversation to the user through text.
+
+Rearrange the code so that it prints in the following order.
+
+Jax: B-Kaw!
+Hero: ...
+Jax: Where are you off to this morning? Bkaw...
+Hero: Where did an owl learn to speak??
+
+"""
+
+print("Jax: B-Kaw!")
+print("Hero: ...")
+print("Jax: Where are you off to this morning? Bkaw...")
+print("Hero: Where did an owl learn to speak??")
+
