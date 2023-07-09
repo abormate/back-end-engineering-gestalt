493c7714158bca06268f7a3f73c117cd8b7de50e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-10 07:10:42 +0800

    SQL LearnSpace -- Requirement -- Learn SQL

--- SQL/PostgreSQL/Fundamentals/02-select-statement.sql
+++ SQL/PostgreSQL/Fundamentals/02-select-statement.sql
@@ -4,3 +4,8 @@
 --
 ---------------------------------------- //
 
+/*
+Let's write our own SQL statement from scratch! A SELECT statement is the most common operation in SQL - often called a "query". 
+SELECT retrieves data from one or more tables. Standard SELECT statements do not alter the state of the database.
+
+*/
