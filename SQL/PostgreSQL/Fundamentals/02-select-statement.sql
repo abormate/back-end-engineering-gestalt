52132b7956562b1e66d9e60c670f4c6712fafc82
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-10 07:20:28 +0800

    SQL LearnSpace -- Select Statement

--- SQL/PostgreSQL/Fundamentals/02-select-statement.sql
+++ SQL/PostgreSQL/Fundamentals/02-select-statement.sql
@@ -9,3 +9,27 @@ Let's write our own SQL statement from scratch! A SELECT statement is the most c
 SELECT retrieves data from one or more tables. Standard SELECT statements do not alter the state of the database.
 
 */
+
+SELECT id from users;
+
+/*
+Select a Single Field 
+----------------------
+
+A SELECT statement begins with the keyword Select followed by the fields that you want to retrieve.
+
+
+*/
+
+SELECT id from users;
+
+/*
+Select from multiple fields
+---------------------------
+If you want to select more than one field you can specify multiple fields separated by commas
+
+
+*/
+
+SELECT id, age from users;
+
