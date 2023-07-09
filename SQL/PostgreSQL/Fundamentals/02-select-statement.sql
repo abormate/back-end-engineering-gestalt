5e08e7daa41520663c8ddb3647a3515b05cd7b3a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-10 07:25:13 +0800

    SQL LearnSpace -- Select Statement

--- SQL/PostgreSQL/Fundamentals/02-select-statement.sql
+++ SQL/PostgreSQL/Fundamentals/02-select-statement.sql
@@ -33,3 +33,25 @@ If you want to select more than one field you can specify multiple fields separa
 
 SELECT id, age from users;
 
+/*
+Select All fields 
+-----------------
+If you want to select every field within a record you can denote this by using the shorthand * syntax
+
+*/
+
+SELECT * from users;
+
+/*
+After specifying fields, you need to indicate which table you want to pull the records from using the from statement followed by
+the name of the table. We'll talk more about tables later, but for now, you can think about them like structs or objects. 
+
+For example, the users table might have 3 fields:
+
+id
+name
+balance
+
+And finally, all statements end with a semi-colon ;
+
+*/
