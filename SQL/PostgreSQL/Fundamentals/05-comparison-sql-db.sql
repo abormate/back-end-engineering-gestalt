74658b516f5d428f1d1976ff3e192cc34aecb49a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-13 05:28:16 +0800

    SQL LearnSpace -- Comparing databases

---
+++ SQL/PostgreSQL/Fundamentals/05-comparison-sql-db.sql
@@ -0,0 +1,19 @@
+--------------------------------------- //
+--
+-- Comparing SQL databases
+--
+--------------------------------------- //
+
+/*
+Let's dive deeper and talk about some of the popular SQL Databases and what makes them different from one another. 
+Some of the most popular SQL Databases right now are:
+
+OracleDB
+MySQL
+Microsoft SQL Server
+PostgreSQL
+SQLite
+Microsoft Access
+And many others
+
+*/
