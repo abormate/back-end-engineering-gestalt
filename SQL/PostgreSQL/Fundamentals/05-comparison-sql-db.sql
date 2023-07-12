53a4fd3cb10d05d2ced61456af2e74a51e8e7686
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-13 05:37:10 +0800

    SQL LearnSpace -- Databases

--- SQL/PostgreSQL/Fundamentals/05-comparison-sql-db.sql
+++ SQL/PostgreSQL/Fundamentals/05-comparison-sql-db.sql
@@ -56,3 +56,13 @@ CREATE TABLE users (id INTEGER, name TEXT, age INTEGER);
 INSERT into users (id, name, age) values (1, 'John Doe', 21);
 INSERT into users (id, name, age) values (2, 'Montgomery Burns', 33);
 SELECT * from users;
+
+CREATE TABLE users (id INTEGER, name TEXT, age INTEGER);
+INSERT into users (id, name, age) values (1, 'John Doe', 21);
+INSERT into users (id, name, age) values (2, '1', 33);
+SELECT * from users;
+
+/*
+SQLite still runs the code with type name defined as TEXT, modified to become an integer 1
+
+*/
