cf8bfefd473ac845d1fba8f142f3b25a6774376f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-12 19:31:45 +0800

    SQL LearnSpace -- NoSQL What it is

---
+++ SQL/PostgreSQL/Fundamentals/04-nosql-vs-sql.sql
@@ -0,0 +1,21 @@
+--------------------------------------- //
+--
+-- SQL versus NoSQL
+--
+--------------------------------------- //
+
+/*
+When talking about SQL databases, we also have to mention the elephant in the room: NoSQL.
+
+To put it simply, a NoSQL database is a database that does not use SQL (Structured Query Language). Each NoSQL typically 
+has it's own way of writing and executing queries. For example, MongoDB uses MQL (MongoDB Query Language) and ElasticSearch 
+simply has a JSON API.
+
+While most relational databases are fairly similar, NoSQL databases tend to be fairly unique and are used for more niche 
+purposes. Some of the main differences between a SQL and NoSQL databases are:
+
+-- 1. NoSQL databases are usually non-relational, SQL databases are usually relational (we'll talk more about what this means later).
+-- 2. SQL databases usually have a defined schema, NoSQL databases usually have dynamic schema.
+-- 3. SQL databases are table-based, NoSQL databases have a variety of different storage methods, such as document, key-value, graph, wide-column, and more.
+
+*/
