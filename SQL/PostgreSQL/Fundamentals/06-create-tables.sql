c10377b3e0e81b50e4c77a1ef548fb8168bfbfc9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-14 14:07:01 +0800

    SQL LearnSpace -- Create Tables

--- SQL/PostgreSQL/Fundamentals/06-create-tables.sql
+++ SQL/PostgreSQL/Fundamentals/06-create-tables.sql
@@ -27,3 +27,38 @@ CREATE TABLE employees(
     is_manager BOOLEAN,
     salary INTEGER
 );
+
+--------------------------- //
+-- Assignment -- Practice --
+--------------------------- //
+
+/*
+Let's begin building a table for CashPal database! Create the people table with the following fields:
+
+id - Integer
+handle - Text
+name - Text
+age - Integer
+balance - Integer
+is_admin - boolean
+
+
+TIP
+The pragma table_info(TABLENAME); command returns information about a table and its fields. You don't need to edit this line, 
+I just thought you might be curious!
+
+*/
+
+CREATE TABLE people(
+  id INTEGER,
+  handle TEXT,
+  name TEXT,
+  age INTEGER,
+  balance integer,
+  is_admin boolean
+  
+);
+
+-- TEST SUITE, DON'T TOUCH BELOW THIS LINE --
+
+pragma table_info('people');
