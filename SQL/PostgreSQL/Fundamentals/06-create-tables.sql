fc92620caf70be63dabd1c8fc3aa121aae3e80e4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-14 13:59:04 +0800

    SQL

--- SQL/PostgreSQL/Fundamentals/06-create-tables.sql
+++ SQL/PostgreSQL/Fundamentals/06-create-tables.sql
@@ -5,3 +5,8 @@
 ---------------------------------------- //
 
 -- The CREATE TABLE statement is used to create a new table in a database.
+
+/*
+To create a table, use the CREATE TABLE statement followed by the name of the table and the fields you want in the table.
+
+*/
