fa8f192430075c62da10c06d82d3081906a56e86
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-15 09:11:43 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/08-altering-tables.sql
+++ SQL/PostgreSQL/Fundamentals/08-altering-tables.sql
@@ -8,5 +8,7 @@
 We often need to alter our database schema without deleting it and re-creating it. Imagine if Twitter deleted its database 
 each time it needed to add a feature, that would be a disaster! Your account and all your tweets would be wiped out daily.
 
+Instead, we can use use the ALTER TABLE statement to make changes in place without deleting any data.
+
 */
 
