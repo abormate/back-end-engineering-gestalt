8140ca377747921c7eb758a192f0c87d6ef53916
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-15 09:10:37 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/08-altering-tables.sql
+++ SQL/PostgreSQL/Fundamentals/08-altering-tables.sql
@@ -4,5 +4,9 @@
 --
 ---------------------------------------- //
 
+/*
+We often need to alter our database schema without deleting it and re-creating it. Imagine if Twitter deleted its database 
+each time it needed to add a feature, that would be a disaster! Your account and all your tweets would be wiped out daily.
 
+*/
 
