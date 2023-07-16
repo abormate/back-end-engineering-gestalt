505b6368eaab4bab01627dabe3b87eb5d1f53119
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-17 06:08:05 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/10-database-migration-practice.sql
+++ SQL/PostgreSQL/Fundamentals/10-database-migration-practice.sql
@@ -4,3 +4,10 @@
 --
 ---------------------------------------- //
 
+/*
+When writing migrations that are reversible, we use the terms "up" and "down" migrations. An "up" migration is simply the 
+set of changes you want to make, like altering/removing/adding/editing a table in some way. A "down" migration includes 
+the changes that would revert any of the "up" migration's changes.
+
+*/
+
