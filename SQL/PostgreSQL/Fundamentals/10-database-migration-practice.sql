ec0ad662356537510178e0a2c0357d8c58ea361b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-17 06:09:25 +0800

    SQL LearnSpace -- Database Migrations

--- SQL/PostgreSQL/Fundamentals/10-database-migration-practice.sql
+++ SQL/PostgreSQL/Fundamentals/10-database-migration-practice.sql
@@ -14,3 +14,12 @@ the changes that would revert any of the "up" migration's changes.
 ---------------------------- //
 -- Assignment -- Practice --
 ---------------------------- //
+
+/*
+We need to add an additional column to the transactions table. We want to know whether or not the transaction was successfully 
+completed between two users. We also want our database to track the type of transaction.
+
+Our transactions table looks like this at the moment:
+
+*/
+
