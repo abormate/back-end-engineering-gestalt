a653d708376c1f9dead8fdab29a3665331d26319
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-17 06:19:41 +0800

    SQL LearnSpace -- SQLite

--- SQL/PostgreSQL/Fundamentals/11-sql-data-types.sql
+++ SQL/PostgreSQL/Fundamentals/11-sql-data-types.sql
@@ -3,3 +3,12 @@
 -- SQL data types
 --
 ------------------------------------------ //
+
+/*
+SQL as a language can support many different data types. However, the datatypes that your database management system (DBMS) 
+supports will vary depending on the specific database you're using.
+
+SQLite only supports the most basic types, and we're using SQLite in this course!
+
+*/
+
