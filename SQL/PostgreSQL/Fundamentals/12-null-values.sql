74ce3a7af0d0c50042aa638d12f213388cc35c16
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-18 14:07:36 +0800

    SQL LearnSpace -- Null Values

--- SQL/PostgreSQL/Fundamentals/12-null-values.sql
+++ SQL/PostgreSQL/Fundamentals/12-null-values.sql
@@ -3,3 +3,11 @@
 -- SQL Null Values
 --
 ---------------------------------------- //
+
+/*
+In SQL a row with a NULL value represents that the value is missing. A NULL value within a row is very different than a 
+zero value. When inserting records into a table, unless a constraint has been set, a record can be added with missing (NULL) 
+fields.
+
+*/
+
