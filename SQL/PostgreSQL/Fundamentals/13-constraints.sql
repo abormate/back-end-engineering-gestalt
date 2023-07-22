82b0c97caca1cd923f415bf62d8e0f2c49140806
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-22 19:13:23 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/13-constraints.sql
+++ SQL/PostgreSQL/Fundamentals/13-constraints.sql
@@ -35,4 +35,22 @@ when we create our tables we need to make sure we specify all the constraints we
 
 */
 
+------------------------------ //
+-- Assignment -- Practice
+------------------------------ //
 
+/*
+Thankfully all the tables we have created for CashPal up to this point have been for testing purposes! Now that we have a 
+better understanding of constraints, let's rebuild our database with the proper constraints and tables!
+
+Create the USERS table with the following fields and constraints:
+
+id - INTEGER, PRIMARY KEY
+name - TEXT, NOT NULL
+age - INTEGER, NOT NULL
+country_code - TEXT, NOT NULL
+username - TEXT, UNIQUE
+password - TEXT, NOT NULL
+is_admin - BOOLEAN
+
+*/
