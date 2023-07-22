d591013c2ada64ed9e7d44ae5b729e67de3ca7ff
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-22 18:48:24 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/13-constraints.sql
+++ SQL/PostgreSQL/Fundamentals/13-constraints.sql
@@ -8,4 +8,7 @@
 A constraint is a rule we create on a database that enforces some specific behavior. For example, setting a NOT NULL 
 constraint on a column ensures that the column will not accept NULL values.
 
+If we try to insert a NULL value into a column with the NOT NULL constraint, the insert will fail with an error message. 
+Constraints are extremely useful when we need to ensure that certain kinds of data exist within our database.
+
 */
