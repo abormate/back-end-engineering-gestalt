65dfe0ed9dcbf0e3d8be0e99b0307a2a0f7450b9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-24 09:39:20 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/14-primary-keys.sql
+++ SQL/PostgreSQL/Fundamentals/14-primary-keys.sql
@@ -10,3 +10,16 @@ within a table. Each table can have one, and only one primary key.
 
 */
 
+----------------------------- //
+-- More into Primary Keys 
+----------------------------- //
+
+/*
+It's very common to have a column named id on each table in a database, and that id is the primary key for that table. No two 
+rows in that table can share an id.
+
+A PRIMARY KEY constraint can be explicitly specified on a column to ensure uniqueness, rejecting any inserts where you attempt 
+to create a duplicate ID.
+
+*/
+
