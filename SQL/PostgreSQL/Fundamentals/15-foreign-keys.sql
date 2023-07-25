ae933ea50f2f2af4783c70b172e581eb3135d28f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-25 16:39:31 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/15-foreign-keys.sql
+++ SQL/PostgreSQL/Fundamentals/15-foreign-keys.sql
@@ -59,5 +59,27 @@ violated. You'll need to reference the setup code below.
 
 */
 
+INSERT into users (
+    id,
+    name,
+    country_code
+) values (
+    0,
+    "Jerry",
+    "US"
+);
 
+INSERT into users (
+    id,
+    name,
+    country_code
+) values (
+    1,
+    "Amit",
+    "IND"
+);
+
+/* -- DON'T TOUCH THIS LINE -- */
+
+select * from users;
 
