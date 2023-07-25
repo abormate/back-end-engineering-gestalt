0a0a790773419d3c7b5a05b9f2df1349b8cb2b99
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-25 16:23:28 +0800

    SQL LearnSpace -- Foreign Keys

--- SQL/PostgreSQL/Fundamentals/15-foreign-keys.sql
+++ SQL/PostgreSQL/Fundamentals/15-foreign-keys.sql
@@ -3,3 +3,10 @@
 -- SQL LearnSpace -- Foreign Keys --
 --
 ---------------------------------------- //
+
+/*
+Foreign keys are what makes relational databases relational! Foreign keys define the relationships between tables. Simply put, 
+a FOREIGN KEY is a field in one table that references another table's PRIMARY KEY.
+
+*/
+
