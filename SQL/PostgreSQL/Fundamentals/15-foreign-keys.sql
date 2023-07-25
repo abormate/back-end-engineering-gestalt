203bd9d0f285ac7af16688be62f24c285dbe9c4c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-25 16:34:36 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/15-foreign-keys.sql
+++ SQL/PostgreSQL/Fundamentals/15-foreign-keys.sql
@@ -14,3 +14,8 @@ a FOREIGN KEY is a field in one table that references another table's PRIMARY KE
 -- Creating of Foreign Key
 ----------------------------- //
 
+/*
+Creating a FOREIGN KEY in SQLite happens at table creation! After we define the table fields and constraints we add an 
+additional CONSTRAINT where we define the FOREIGN KEY and its REFERENCES.
+
+*/
