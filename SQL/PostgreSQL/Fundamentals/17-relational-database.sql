da5c73f4ce9990f88add1f07deb6e9fe4a7b4f3b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-27 06:56:34 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/17-relational-database.sql
+++ SQL/PostgreSQL/Fundamentals/17-relational-database.sql
@@ -4,5 +4,10 @@
 --
 --------------------------------------- //
 
+/*
+We have been using the term relational quite a bit, it's time we actually go over what that means! A relational database is a 
+type of database that stores data so that it can be easily related to other data. For example, a user can have many tweets. 
+There's a relationship between a user and their tweet.
 
+*/
 
