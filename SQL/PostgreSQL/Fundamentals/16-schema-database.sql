883465edcad354c1d5979e7d680bab317408d368
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-26 16:23:09 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/16-schema-database.sql
+++ SQL/PostgreSQL/Fundamentals/16-schema-database.sql
@@ -4,3 +4,11 @@
 --
 --------------------------------------- //
 
+/*
+We've used the word schema a few times now, let's talk about what that word means. A database's schema describes how data is 
+organized within it.
+
+Data types, table names, field names, constraints, and the relationships between all of those entities are part of a database's 
+schema.
+
+*/
