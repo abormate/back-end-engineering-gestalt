f6210fe2510583eec6c6641b0bd3285052c6770c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-07-27 07:02:58 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/17-relational-database.sql
+++ SQL/PostgreSQL/Fundamentals/17-relational-database.sql
@@ -11,3 +11,12 @@ There's a relationship between a user and their tweet.
 
 */
 
+/*
+In a relational database:
+
+1. Data is typically represented in "tables".
+2. Each table has "columns" or "fields that hold attributes related to the record.
+3. Each row or entry in the table is called a record.
+4. Typically, each record has a unique Id called the primary key.
+
+*/
