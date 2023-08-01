a19a17debc12a30f828cf858607b73cc01b9928a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-01 13:15:06 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/20-insert-statement.sql
+++ SQL/PostgreSQL/Fundamentals/20-insert-statement.sql
@@ -3,3 +3,11 @@
 -- INSERT statement
 --
 ---------------------------------------- //
+
+/*
+Tables are pretty useless without data in them! In SQL we can add records to a table using an INSERT INTO statement. 
+When using an INSERT statement we must first specify the table we are inserting the record into, followed by the fields within 
+that table we want to add VALUES to.
+
+*/
+
