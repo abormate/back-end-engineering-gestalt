2e34a325d2722d004c19148ddfef7361ddfdce82
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-01 13:24:22 +0800

    SQL LearnSpace -- Insert Into

--- SQL/PostgreSQL/Fundamentals/20-insert-statement.sql
+++ SQL/PostgreSQL/Fundamentals/20-insert-statement.sql
@@ -50,3 +50,12 @@ is_admin: false
 
 
 */
+
+INSERT INTO users (id, name, age, country_code, username, password, is_admin)
+VALUES (1, 'David', 34, 'US', 'DavidDev', 'insertPractice', false),
+  (2, 'Samantha', 29, 'BR', 'Sammy93', 'addingRecords!', false);
+
+
+-- TEST SUITE, DON'T TOUCH BELOW THIS LINE --
+
+SELECT * FROM users;
