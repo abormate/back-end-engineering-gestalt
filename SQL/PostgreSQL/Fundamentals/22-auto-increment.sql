15196d486cef87e56e6d88c4bbccc13a306a7df2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-03 05:31:33 +0800

    SQL LearnSpace -- Auto-increment

--- SQL/PostgreSQL/Fundamentals/22-auto-increment.sql
+++ SQL/PostgreSQL/Fundamentals/22-auto-increment.sql
@@ -4,3 +4,12 @@
 --
 --------------------------------------- //
 
+/**************************************
+
+Many dialects of SQL support an AUTO INCREMENT feature. When inserting records into a table with AUTO INCREMENT enabled, 
+the database will assign the next value automatically. In SQLite an integer id field that has the PRIMARY KEY constraint 
+will auto increment by default!
+
+**************************************/
+
+
