8e2bef0f1c4f39960f078d836fb539c995ea70ad
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-03 05:40:10 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/22-auto-increment.sql
+++ SQL/PostgreSQL/Fundamentals/22-auto-increment.sql
@@ -14,9 +14,20 @@ will auto increment by default!
 
 /*************************************
 
+IDs
+
 Depending on how your database is set up, you may be using traditional ids or you may be using UUIDs. SQL doesn't support auto 
 incrementing a uuid so if your database is using them your server will have to handle the changing uuid's for each record.
 
 *************************************/
 
+/************************************
+
+Using Auto Increment with SQLite
+
+We are using traditional ids in our database, so we can take advantage of the auto increment feature. Different dialects of 
+SQL will implement this feature differently, but in SQLite any id field that has the PRIMARY KEY constraint will auto increment! 
+So we can omit the id field within the INSERT statement and allow the database to automatically add that field for us!
+
+************************************/
 
