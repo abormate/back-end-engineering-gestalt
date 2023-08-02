95dbc22c68fd83b7e8b5dc8a0fa792bf2339b6f0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-02 17:14:20 +0800

    SQL LearnSpace -- CRUD Lifecycle

--- SQL/PostgreSQL/Fundamentals/21-http-crud-lifecycle.sql
+++ SQL/PostgreSQL/Fundamentals/21-http-crud-lifecycle.sql
@@ -5,3 +5,5 @@
 ---------------------------------------- //
 
 -- It's important to understand how data flows through a typical web application.
+-- Image diagram jpeg --> https://prnt.sc/YOIyIJ1rkI3S
+
