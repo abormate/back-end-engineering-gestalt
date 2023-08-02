5e1d3008e746e4e60956348cbf4e75ca17c5edb9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-02 17:14:58 +0800

    SQL LearnSpace -- CRUD LifeCycle

--- SQL/PostgreSQL/Fundamentals/21-http-crud-lifecycle.sql
+++ SQL/PostgreSQL/Fundamentals/21-http-crud-lifecycle.sql
@@ -7,3 +7,12 @@
 -- It's important to understand how data flows through a typical web application.
 -- Image diagram jpeg --> https://prnt.sc/YOIyIJ1rkI3S
 
+/*
+The front-end processes some data from user input - maybe a form is submitted.
+The front-end sends that data to the server through an HTTP request - maybe a POST.
+The server makes a SQL query to it's database to create an associated record - Probably using an INSERT statement.
+Once the server has processed that the database query was successful, it responds to the front-end with a status code! 
+
+Hopefully a 200-level code (success)!
+
+*/
