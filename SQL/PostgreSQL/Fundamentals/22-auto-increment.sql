678f510d92b02130d072b124270f42a58b47b6fa
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-04 14:20:36 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/22-auto-increment.sql
+++ SQL/PostgreSQL/Fundamentals/22-auto-increment.sql
@@ -58,3 +58,47 @@ password: autoincrement
 is_admin: true
 
 */
+
+/* SETUP CODE */
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    country_code TEXT NOT NULL,
+    username TEXT UNIQUE,
+    password TEXT NOT NULL,
+    is_admin BOOLEAN
+);
+
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (1, 'David', 34, 'US', 'DavidDev', 'insertPractice', false);
+
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (2, 'Samantha', 29, 'BR', 'Sammy93', 'addingRecords!', false);
+
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (3, 'John', 39, 'CA', 'Jjdev21', 'welovebootdev', false);
+
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (4, 'Ram', 42, 'IN', 'Ram11c', 'thisSQLcourserocks', false);
+
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (5, 'Hunter', 30, 'US', 'Hdev92', 'backendDev', false);
+
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (6, 'Allan', 27, 'US', 'Alires', 'iLoveB00tdev', true);
+
+
+/* END SETUP CODE */
+
+
+/* ANSWER CODE */
+
+INSERT INTO users (name, age, country_code, username, password, is_admin)
+  VALUES ('Lance', 20, 'US', 'LanChr', 'b00tdevisbest', false), 
+  ('Tiffany', 28, 'US', 'Tifferoon', 'autoincrement', true);
+
+-- TEST SUITE, DON'T TOUCH BELOW THIS LINE --
+
+SELECT * FROM users;
