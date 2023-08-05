badaba7b083ef096212a2937c2898ba59af9d559
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-05 14:03:18 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/23-manual-entry-values.sql
+++ SQL/PostgreSQL/Fundamentals/23-manual-entry-values.sql
@@ -3,3 +3,9 @@
 -- Manual Entry of Values
 --
 ---------------------------------------- //
+
+/*
+Manually INSERTing every single record in a database would be an extremely time-consuming task! Working with 
+raw SQL as we are now is not super common when designing backend systems.
+
+*/
