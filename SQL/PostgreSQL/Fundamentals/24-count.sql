16bd50004a6d6caf5ebb04d13ab5333215831e0e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-05 14:23:23 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/24-count.sql
+++ SQL/PostgreSQL/Fundamentals/24-count.sql
@@ -11,3 +11,11 @@ need to know how many records there are, but we don't particularly care what's i
 */
 
 SELECT count(*) from employees;
+
+/*
+Mechanics -- 
+
+The * in this case refers to a column name. We don't care about the count of a specific column - we want to 
+know the number of total records so we can use the wildcard (*).
+
+*/
