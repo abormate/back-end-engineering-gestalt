320126f0dbb843c0fdc3d3d68708f41bb620589e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-05 14:21:33 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/24-count.sql
+++ SQL/PostgreSQL/Fundamentals/24-count.sql
@@ -3,3 +3,10 @@
 -- Count -- SQL LearnSpace
 --
 --------------------------------------- //
+
+/*
+We can use a SELECT statement to get a count of the records within a table. This can be very useful when we 
+need to know how many records there are, but we don't particularly care what's in them.
+
+*/
+
