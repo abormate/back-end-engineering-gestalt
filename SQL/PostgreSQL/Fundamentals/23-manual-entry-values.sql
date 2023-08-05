9bf82394f9a69fdcdcd5e4625d562e033eb036b8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-05 14:04:15 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/23-manual-entry-values.sql
+++ SQL/PostgreSQL/Fundamentals/23-manual-entry-values.sql
@@ -8,4 +8,9 @@
 Manually INSERTing every single record in a database would be an extremely time-consuming task! Working with 
 raw SQL as we are now is not super common when designing backend systems.
 
+When working with SQL within a software system, like a backend web application, you'll typically have access 
+to a programming language such as Go or Python. For example, a backend server written in Go can use string 
+concatenation to dynamically create SQL statements, and that's usually how it's done!
+
 */
+
