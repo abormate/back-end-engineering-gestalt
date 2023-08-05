b2141a9b5313514e87de3c579cf9d3001a8b9693
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-05 14:34:24 +0800

    SQL LearnSpace -- CRUD HTTP Lifecycle

---
+++ SQL/PostgreSQL/Fundamentals/25-HTTP-CRUD-lifecycle.sql
@@ -0,0 +1,13 @@
+---------------------------------------- //
+--
+-- HTTP -- CRUD -- Database Lifecycle
+--
+---------------------------------------- //
+
+/*
+We talked about how a "create" operation flows through a web application. Let's talk about a "read".
+
+*/
+
+-- visual diagram of how it works, lifecycle --> https://prnt.sc/4xO5FEFrD-d-
+
