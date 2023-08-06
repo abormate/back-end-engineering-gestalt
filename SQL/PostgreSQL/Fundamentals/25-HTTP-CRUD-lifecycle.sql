c8c66260e5968474a03374ea3c46f590b173d14f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-07 06:02:50 +0800

    SQL LearnSpace -- CRUD Lifecycle

--- SQL/PostgreSQL/Fundamentals/25-HTTP-CRUD-lifecycle.sql
+++ SQL/PostgreSQL/Fundamentals/25-HTTP-CRUD-lifecycle.sql
@@ -12,6 +12,13 @@ We talked about how a "create" operation flows through a web application. Let's 
 -- visual diagram of how it works, lifecycle --> https://prnt.sc/4xO5FEFrD-d-
 
 /*
+Let's talk through an example using the CashPal app. Our product manager wants to show profile data on a 
+user's settings page. Here's how we could engineer that feature request:
 
+First, the front-end webpage loads.
+The front-end sends an HTTP GET request to a /users endpoint on the back-end server.
+The server receives the request.
+The server uses a SELECT statement to retrieve the user's record from the users table in the database.
+The server converts the row of SQL data into a JSON object and sends it back to the front-end.
 
 */
