330389b262f7808c0cf514a9e24b415d5477eab0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-07 06:04:52 +0800

    SQL LearnSpace -- Where

--- SQL/PostgreSQL/Fundamentals/26-where-clause.sql
+++ SQL/PostgreSQL/Fundamentals/26-where-clause.sql
@@ -3,3 +3,10 @@
 -- WHERE Clause
 --
 ---------------------------------------- //
+
+/*
+In order to keep learning about CRUD operations in SQL, we need to learn how to make the instructions we 
+send to the database more specific. SQL accepts a WHERE statement within a query that allows us to be very 
+specific with our instructions.
+
+*/
