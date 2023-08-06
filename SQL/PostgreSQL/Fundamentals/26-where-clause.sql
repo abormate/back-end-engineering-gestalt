1e7c520e47bf808911a4ec06c9a9d68f2b477292
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-07 06:05:35 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/26-where-clause.sql
+++ SQL/PostgreSQL/Fundamentals/26-where-clause.sql
@@ -9,4 +9,7 @@ In order to keep learning about CRUD operations in SQL, we need to learn how to 
 send to the database more specific. SQL accepts a WHERE statement within a query that allows us to be very 
 specific with our instructions.
 
+If we were unable to specify the specific record we wanted to READ, UPDATE, or DELETE making queries to a 
+database would be very frustrating, and very inefficient.
+
 */
