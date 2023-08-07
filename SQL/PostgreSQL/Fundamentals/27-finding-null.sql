2d361aff4d6d5d8be923233c2c5fd3a052ddab63
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-08 04:43:35 +0800

    SQL LearnSpace -- Null

--- SQL/PostgreSQL/Fundamentals/27-finding-null.sql
+++ SQL/PostgreSQL/Fundamentals/27-finding-null.sql
@@ -16,3 +16,17 @@ SELECT name FROM users WHERE first_name IS NOT NULL;
 -- Assignment -- Practice
 --------------------------- //
 
+/*
+The way we store transactions at CashPal is interesting. We store a user_id field on the transactions table. 
+That user is the "owner" of the transaction, and a user_id is never null.
+
+Whenever the owner of the transaction receives money, the sender_id will not be null - it will be the user id 
+of the sender. The recipient_id will be null.
+
+Whenever the owner of the transaction sends money, the recipient_id will not be null - it will be the user id 
+of the recipient. The sender_id will be null.
+
+Select all the rows from the transactions table where the owner of the transactions is receiving money.
+
+*/
+
