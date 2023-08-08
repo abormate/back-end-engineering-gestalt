bdb650b7c2c5ba276c573cf230d2c6105789dbdc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-09 05:59:26 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/29-delete-statement.sql
+++ SQL/PostgreSQL/Fundamentals/29-delete-statement.sql
@@ -4,3 +4,9 @@
 --
 ---------------------------------------- //
 
+/*
+When a user deletes their account on Twitter, or deletes a comment on a YouTube video, that data needs to be 
+removed from its respective database.
+
+*/
+
