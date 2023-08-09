aa08ce50848634a839eb8e1e49e601b871dca090
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-10 07:12:59 +0800

    SQL LearnSpace -- Update

--- SQL/PostgreSQL/Fundamentals/31-update.sql
+++ SQL/PostgreSQL/Fundamentals/31-update.sql
@@ -3,3 +3,12 @@
 -- Update SQL
 --
 ---------------------------------------- //
+
+/*
+Whenever you update your profile picture or change your password online, you are changing the data in a field 
+on a table in a database! Imagine if every time you accidentally messed up a Tweet on Twitter you had to 
+delete the entire tweet and post a new one instead of just editing it...
+
+...Well, that's a bad example.
+
+*/
