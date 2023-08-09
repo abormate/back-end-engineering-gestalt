5061290cea10d73d5d6bd3c1d5e6e51fd34f633f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-10 07:18:10 +0800

    SQL LearnSpace -- Update Statement

--- SQL/PostgreSQL/Fundamentals/31-update.sql
+++ SQL/PostgreSQL/Fundamentals/31-update.sql
@@ -42,4 +42,17 @@ UPDATE Lane's record within the users table so that the is_admin field is set to
 
 Here is the current state of the USERS table for reference!
 
+
+id	name	age	country_code	username	password	is_admin
+1	David	34	US	DavidDev	insertPractice	0
+2	Samantha	29	BR	Sammy93	addingRecords!	0
+3	John	39	CA	Jjdev21	welovebootdev	0
+4	Ram	42	IN	Ram11c	thisSQLcourserocks	0
+5	Hunter	30	US	Hdev92	backendDev	0
+6	Allan	27	US	Alires	iLoveB00tdev	1
+7	Lance	20	US	LanChr	b00tdevisbest	0
+8	Tiffany	28	US	Tifferoon	autoincrement	1
+9	Lane	27	US	wagslane	update_me	0
+
 */
+
