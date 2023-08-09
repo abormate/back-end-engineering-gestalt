04d0085277a7ca1ce37c95fd08401e871629fa68
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-10 07:34:20 +0800

    SQL LearnSpace -- Update Statement

--- SQL/PostgreSQL/Fundamentals/32-object-relational-mapping.sql
+++ SQL/PostgreSQL/Fundamentals/32-object-relational-mapping.sql
@@ -4,3 +4,13 @@
 --
 ---------------------------------------- //
 
+/*
+An Object-Relational Mapping or an ORM for short, is a tool that allows you to perform CRUD operations on a 
+database using a traditional programming language. These typically come in the form of a library or framework 
+that you would use in your backend code.
+
+The primary benefit an ORM provides is that it maps your database records to in-memory objects. For example, 
+in Go we might have a struct that we use in our code:
+
+*/
+
