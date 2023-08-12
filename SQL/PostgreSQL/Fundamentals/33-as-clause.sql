5ad33832b09fab5c3e02018497d76453b6eb874b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-12 08:24:23 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/33-as-clause.sql
+++ SQL/PostgreSQL/Fundamentals/33-as-clause.sql
@@ -4,3 +4,9 @@
 --
 --------------------------------------- //
 
+/*
+Sometimes we need to structure the data we return from our queries in a specific way. An AS clause allows 
+us to "alias" a piece of data in our query. The alias only exists for the duration of the query.
+
+*/
+
