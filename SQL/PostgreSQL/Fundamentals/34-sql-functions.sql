2962c65390c02f5b7ef283e75edeb3f9ce0adcea
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-14 07:40:03 +0800

    SQL LearnSpace -- SQL Functions

---
+++ SQL/PostgreSQL/Fundamentals/34-sql-functions.sql
@@ -0,0 +1,12 @@
+---------------------------------------- //
+--
+-- SQL Functions -- SQL LearnSpace
+--
+---------------------------------------- //
+
+/*
+At the end of the day, SQL is a programming language, and it's one that supports functions. We can use functions and aliases to calculate new columns 
+in a query. This is similar to how you might use formulas in excel.
+
+*/
+
