5f5723fd708c7b929486fecc71c1adade400655a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-15 04:44:35 +0800

    SQL LearnSpace -- Functions

--- SQL/PostgreSQL/Fundamentals/34-sql-functions.sql
+++ SQL/PostgreSQL/Fundamentals/34-sql-functions.sql
@@ -29,3 +29,24 @@ SELECT quantity,
     IIF(quantity < 10, "Order more", "In Stock") AS directive
     from products
 
+-------------------------- //
+-- Assignment -- Practice
+-------------------------- //
+
+/*
+We need to look through CashPal's transaction data and determine whether or not any of the transactions need to be audited.
+
+Return all the data from the transactions table, and add an extra column at the end called audit.
+
+-- If a row's was_successful field is true, the audit field should say "No action required".
+-- If a row's was_successful field is false, the audit field should say "Perform an audit".
+
+
+TIP
+Some equality operators in SQL:
+
+=: Equal to
+<: Less than
+>: Greater than
+
+*/
