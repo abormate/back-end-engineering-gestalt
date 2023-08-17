080e6f482241735156e4c82fc734c35087206c89
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-17 14:25:49 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/35-between.sql
+++ SQL/PostgreSQL/Fundamentals/35-between.sql
@@ -14,3 +14,9 @@ SELECT employee_name, salary
 FROM employees
 WHERE salary BETWEEN 30000 and 60000;
 
+/*
+This query returns all the employees name and salary fields for any rows where the salary is BETWEEN 30,000 and 60,000! We can also query results that 
+are NOT BETWEEN two specified values.
+
+*/
+
