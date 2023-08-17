1a5cc8418b861c1efa224bddf5e39bfa8501130a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-17 14:24:59 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/35-between.sql
+++ SQL/PostgreSQL/Fundamentals/35-between.sql
@@ -9,3 +9,8 @@ We can check if values are between two numbers using the WHERE clause in an intu
 specify specific id's or values. We can also use it to help narrow down our result set. Here's an example:
 
 */
+
+SELECT employee_name, salary
+FROM employees
+WHERE salary BETWEEN 30000 and 60000;
+
