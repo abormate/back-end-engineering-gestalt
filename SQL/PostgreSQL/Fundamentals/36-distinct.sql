ff36c925b70c16250f75e5845b8ee901bcbe0e42
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-17 14:39:24 +0800

    SQL LearnSpace -- Distinct

--- SQL/PostgreSQL/Fundamentals/36-distinct.sql
+++ SQL/PostgreSQL/Fundamentals/36-distinct.sql
@@ -4,3 +4,11 @@
 --
 ---------------------------------------- //
 
+/*
+Sometimes we want to retrieve records from a table without getting back any duplicates.
+
+For example, we may want to know all the different companies our employees have worked at previously, but we don't want to see the same company multiple 
+times in the report.
+
+*/
+
