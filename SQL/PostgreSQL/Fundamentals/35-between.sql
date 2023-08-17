ad172d35ef276b5e836cddaeec128db62d3793f0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-17 14:24:35 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/35-between.sql
+++ SQL/PostgreSQL/Fundamentals/35-between.sql
@@ -4,3 +4,8 @@
 --
 ---------------------------------------- //
 
+/*
+We can check if values are between two numbers using the WHERE clause in an intuitive way! The WHERE clause doesn't always have to be used to 
+specify specific id's or values. We can also use it to help narrow down our result set. Here's an example:
+
+*/
