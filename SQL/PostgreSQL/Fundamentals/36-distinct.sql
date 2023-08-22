4c3e9afebc07514f8ed3b5b1903106e665e9eb89
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-22 14:54:25 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/36-distinct.sql
+++ SQL/PostgreSQL/Fundamentals/36-distinct.sql
@@ -20,3 +20,21 @@ times in the report.
 SQL offers us the DISTINCT keyword that removes duplicate records from the resulting query.
 
 */
+
+SELECT DISTINCT previous_company
+    FROM employees;
+
+-- This only returns one row for each unique previous_company value.
+
+--------------------------- //
+-- Assignment -- Practice
+--------------------------- //
+
+/*
+CashPal executives want to know how many countries we have customers in. We store country_code data as a column on the users table.
+
+Run a DISTINCT query to get all the unique country_codes from the users table.
+
+*/
+
+SELECT DISTINCT country_code FROM users;
