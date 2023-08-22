b6a334824f1e167e379394b126fdd7cce8ade186
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-22 15:17:04 +0800

    SQL LearnSpace -- AND Operators

--- SQL/PostgreSQL/Fundamentals/37-logical-operators.sql
+++ SQL/PostgreSQL/Fundamentals/37-logical-operators.sql
@@ -40,3 +40,17 @@ All of the following operators are supported in SQL. The = is the main one to wa
 >=
 
 */
+
+------------------------ //
+-- Assignment -- Practice
+------------------------ //
+
+/*
+The legal restrictions in Canada have changed! The way we have to handle Canadian minors' CashPal transactions is more strict. 
+We need to find all of those users, so we can see how many users this change affects!
+
+Write a query that retrieves all of the fields from the users table who are from Canada (CA), and are under the age of 18.
+
+*/
+
+
