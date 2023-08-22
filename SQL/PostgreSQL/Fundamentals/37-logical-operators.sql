75bae13a9cadee57103143b443c19a06c3f63cf9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-22 15:03:01 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/37-logical-operators.sql
+++ SQL/PostgreSQL/Fundamentals/37-logical-operators.sql
@@ -3,3 +3,12 @@
 -- Logical Operators -- AND
 --
 --------------------------------------- //
+
+/*
+We often need to use multiple conditions to retrieve the exact information we want. We can begin to structure much more complex queries by using 
+multiple conditions together to narrow down the search results of our query.
+
+The logical AND operator can be used to narrow down our result sets even more!
+
+*/
+
