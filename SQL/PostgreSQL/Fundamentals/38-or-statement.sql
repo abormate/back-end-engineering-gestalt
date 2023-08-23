e4367ba9a0327ab4b5fa455c834e7dd5d663df74
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-23 09:12:59 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/38-or-statement.sql
+++ SQL/PostgreSQL/Fundamentals/38-or-statement.sql
@@ -3,3 +3,9 @@
 -- OR statement
 --
 ---------------------------------------- //
+
+/*
+As you've probably guessed, if the logical AND operator is supported, the OR operator is probably supported as well.
+
+*/
+
