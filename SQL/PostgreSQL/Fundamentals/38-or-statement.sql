74f7ad32a972eb4587d618698534c1b02ebcc543
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-23 09:13:34 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/38-or-statement.sql
+++ SQL/PostgreSQL/Fundamentals/38-or-statement.sql
@@ -9,3 +9,8 @@ As you've probably guessed, if the logical AND operator is supported, the OR ope
 
 */
 
+SELECT product_name, quantity, shipment_status
+    FROM products
+    WHERE shipment_status = 'out of stock'
+    OR quantity BETWEEN 10 and 100;
+
