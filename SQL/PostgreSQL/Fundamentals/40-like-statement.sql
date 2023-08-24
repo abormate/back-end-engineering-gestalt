e77944b640f4b97716e9727dd178985724731853
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-24 18:28:36 +0800

    SQL LearnSpace -- Like Statement

---
+++ SQL/PostgreSQL/Fundamentals/40-like-statement.sql
@@ -0,0 +1,14 @@
+--------------------------------------- //
+--
+-- Like -- SQL LearnSpace
+--
+--------------------------------------- //
+
+/*
+Sometimes we don't have the luxury of knowing exactly what it is we need to query. Have you ever wanted to look up a song or a video but you only remember 
+part of the name? SQL provides us an option for when we're in situations LIKE this.
+
+The LIKE keyword allows for the use of the % and _ wildcard operators. Let's focus on % first.
+
+*/
+
