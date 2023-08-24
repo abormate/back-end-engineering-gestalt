f903a39d1c9e474ff7d6994f5833e9d565cb7995
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-25 07:43:09 +0800

    SQL LearnSpace

---
+++ SQL/PostgreSQL/Fundamentals/41-underscore-operator.sql
@@ -0,0 +1,11 @@
+--------------------------------------- //
+--
+-- Underscore Operator
+--
+--------------------------------------- //
+
+/*
+As discussed, the % wildcard operator matches zero or more characters. Meanwhile, the _ wildcard operator only matches a single character.
+
+*/
+
