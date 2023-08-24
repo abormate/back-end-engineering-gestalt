4c2df3ded187575ff8f83ef3ba029e3fbcf41571
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-24 18:20:48 +0800

    SQL LearnSpace -- In

--- SQL/PostgreSQL/Fundamentals/39-in-statement.sql
+++ SQL/PostgreSQL/Fundamentals/39-in-statement.sql
@@ -4,3 +4,11 @@
 --
 ---------------------------------------- //
 
+/*
+Another variation to the WHERE clause we can utilize is the IN operator. IN returns true or false if the first operand matches any of the values in the 
+second operand. The IN operator is a shorthand for multiple OR conditions.
+
+These two queries are equivalent:
+
+*/
+
