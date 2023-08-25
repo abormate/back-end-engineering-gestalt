f11def79764399e9ae8e9dde731e6a80c0f089ca
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-25 13:51:09 +0800

    SQL LearnSpace -- Wildcards

--- SQL/PostgreSQL/Fundamentals/42-wildcards.sql
+++ SQL/PostgreSQL/Fundamentals/42-wildcards.sql
@@ -1,1 +1,12 @@
 --------------------------------------- //
+--
+-- Wildcards
+--
+--------------------------------------- //
+
+SELECT * from users
+    WHERE name LIKE 'or_%';
+
+
+SELECT * from users
+    WHERE name LIKE '__ing';
