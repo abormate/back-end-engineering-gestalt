a27606b74db0b1b290b009cddb67e0be338b3588
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-28 17:30:55 +0800

    SQL LearnSpace -- Limit

--- SQL/PostgreSQL/Fundamentals/43-limit.sql
+++ SQL/PostgreSQL/Fundamentals/43-limit.sql
@@ -4,3 +4,9 @@
 --
 ---------------------------------------- //
 
+/*
+Sometimes we don't want to retrieve every record from a table. For example, it's common for a production database table to have millions of rows, 
+and SELECTing all of them might crash your system! The LIMIT keyword has entered the chat.
+
+*/
+
