0ebc4d3f9db25369b289f494422779ae936b0162
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-28 17:36:21 +0800

    SQL LearnSpace -- Limit records

--- SQL/PostgreSQL/Fundamentals/43-limit.sql
+++ SQL/PostgreSQL/Fundamentals/43-limit.sql
@@ -32,3 +32,14 @@ the query, the LIMIT statement will not have an effect.
 -- Assignment -- Practice
 ---------------------------- //
 
+/*
+A lot of our users have been using CashPal to pay other users for lunch. Let's take a look at a sample of that data.
+
+Write a query that returns all rows and fields from the transactions table:
+
+Any record where the note field has the word lunch in it.
+The query should return at most 5 records.
+
+*/
+
+SELECT * FROM transactions WHERE note LIKE '%lunch%' LIMIT 5; 
