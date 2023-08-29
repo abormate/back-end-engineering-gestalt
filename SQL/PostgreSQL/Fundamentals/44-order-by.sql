1af46913c55996dd5dd5185a488e0980711696ab
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-30 07:57:20 +0800

    SQL LearnSpace -- Order

--- SQL/PostgreSQL/Fundamentals/44-order-by.sql
+++ SQL/PostgreSQL/Fundamentals/44-order-by.sql
@@ -5,6 +5,7 @@
 ---------------------------------------- //
 
 /*
-
+SQL also offers us the ability to sort the results of a query using ORDER BY. By default, the ORDER BY keyword sorts records by the given field in 
+ascending order, or ASC for short. However, ORDER BY does support descending order as well with the keyword DESC.
 
 */
