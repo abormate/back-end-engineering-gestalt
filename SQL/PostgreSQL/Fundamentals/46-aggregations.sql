ad151e314f344c6edb410edf10cb5c88319ebb21
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-31 07:06:14 +0800

    SQL LearnSpace -- Aggregations

---
+++ SQL/PostgreSQL/Fundamentals/46-aggregations.sql
@@ -0,0 +1,13 @@
+---------------------------------------- //
+--
+-- Aggregations -- SQL LearnSpace
+--
+---------------------------------------- //
+
+/*
+An "aggregation" is a single value that's derived by combining several other values. We performed an aggregation earlier when we used the count statement 
+to count the number of records in a table.
+
+
+*/
+
