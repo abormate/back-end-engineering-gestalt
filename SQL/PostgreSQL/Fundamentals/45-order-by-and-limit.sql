711019efded45ad4f470d7ecfc5447125742fa95
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-08-31 07:03:38 +0800

    SQL LearnSpace -- Order and Limit

--- SQL/PostgreSQL/Fundamentals/45-order-by-and-limit.sql
+++ SQL/PostgreSQL/Fundamentals/45-order-by-and-limit.sql
@@ -10,3 +10,21 @@
 -- Assignment -- Practice
 ---------------------------- //
 
+/*
+An HR employee got into the Git repository where we store all the queries and tried to update one himself.
+
+Fix the bug in the SQL query.
+
+*/
+
+-- buggy code
+SELECT * FROM transactions
+WHERE amount BETWEEN 10 AND 80
+LIMIT 4
+ORDER BY amount DESC;
+
+-- corrected code
+SELECT * FROM transactions
+WHERE amount BETWEEN 10 AND 80
+ORDER BY amount DESC
+LIMIT 4;
