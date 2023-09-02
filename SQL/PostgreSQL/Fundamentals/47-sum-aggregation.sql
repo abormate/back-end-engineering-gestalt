cfeec0b66f971004fad6ba04696ef939c6efa4a4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-02 10:07:19 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/47-sum-aggregation.sql
+++ SQL/PostgreSQL/Fundamentals/47-sum-aggregation.sql
@@ -4,3 +4,11 @@
 --
 ---------------------------------------- //
 
+/*
+The sum aggregation function returns the sum of a set of values.
+
+For example, the query below returns a single record containing a single field. The returned value is equal to the total salary being collected by all of the 
+employees in the employees table.
+
+*/
+
