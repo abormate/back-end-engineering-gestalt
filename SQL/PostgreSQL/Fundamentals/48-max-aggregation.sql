d88fe180ba9490f72e3540f9a6bcd66b82834224
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-06 07:37:04 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/48-max-aggregation.sql
+++ SQL/PostgreSQL/Fundamentals/48-max-aggregation.sql
@@ -9,3 +9,9 @@
 SELECT max(price)
 FROM products
 
+/*
+This query looks through all of the prices in the products table and returns the price with the largest price value. Remember it only returns the price, 
+not the rest of the record! You always need to specify each field you want a query to return.
+
+*/
+
