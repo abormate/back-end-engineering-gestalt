d547a9214bcdf23aa99b0543c4b0a020bfe17676
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-07 07:44:36 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/50-group-by.sql
+++ SQL/PostgreSQL/Fundamentals/50-group-by.sql
@@ -4,3 +4,11 @@
 --
 ---------------------------------------- //
 
+/*
+There are times we need to group data based on specific values.
+
+SQL offers the GROUP BY clause which can group rows that have similar values into "summary" rows. It returns one row for each group. The interesting part is 
+that each group can have an aggregate function applied to it that operates only on the grouped data.
+
+*/
+
