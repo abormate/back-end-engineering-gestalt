a1bcdccd434c9098a29e06075823ac653fe6feda
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-07 06:35:30 +0800

    SQL LearnSpace -- Min

--- SQL/PostgreSQL/Fundamentals/49-min-aggregation.sql
+++ SQL/PostgreSQL/Fundamentals/49-min-aggregation.sql
@@ -14,3 +14,18 @@ from products;
 
 -- This query returns the product_name and the price fields of the record with the lowest price.
 
+
+-------------------------- //
+-- Assignment -- Practice 
+-------------------------- //
+
+/*
+Use a min aggregation to find the age of our youngest CashPal user in the United States in the users table.
+
+USERS TABLE
+| id | name | age | country_code | username | password | is_admin |
+
+
+The country_code of the United States is US.
+
+*/
