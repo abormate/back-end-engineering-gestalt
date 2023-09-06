046974dd27aeacfbc76c8491bb0ab4056ab76f85
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-07 06:30:19 +0800

    SQL LearnSpace -- Min

--- SQL/PostgreSQL/Fundamentals/49-min-aggregation.sql
+++ SQL/PostgreSQL/Fundamentals/49-min-aggregation.sql
@@ -3,3 +3,8 @@
 -- Min -- Aggregation -- SQL StudySpace
 --
 ---------------------------------------- //
+
+/*
+The min function works the same as the max function but finds the lowest value instead of the highest value.
+
+*/
