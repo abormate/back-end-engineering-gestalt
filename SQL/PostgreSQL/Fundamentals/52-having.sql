7513461f7bbe774872cd86e42c49d0c4fea770a1
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-11 06:47:42 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/52-having.sql
+++ SQL/PostgreSQL/Fundamentals/52-having.sql
@@ -29,4 +29,14 @@ This query returns the album_id and count of its songs, but only for albums with
 A new page in the CashPal app allows users to see how much money they've spent on a specific kind of transaction, and alerts them if that amount is fairly 
 large. Let's write a query that returns the total amount spent by each user on lunch when that balance is greater than 20
 
+Your query should:
+
+Return a sender_id (the person spending money) and a balance.
+The balance is the sum() of all amounts spent.
+Don't return any rows that have a NULL sender_id.
+Group by sender_id.
+The note must contain the word lunch to be a part of the aggregation.
+The aggregated balance must be greater than 20.
+Order the results by the balance in ascending order.
+
 */
