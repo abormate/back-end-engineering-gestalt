c93ad087bebbb1071d9bc1814addfb3c07a94427
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-11 07:05:08 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/53-having-vs-where.sql
+++ SQL/PostgreSQL/Fundamentals/53-having-vs-where.sql
@@ -12,4 +12,11 @@ The difference is fairly simple in actuality:
 A WHERE condition is applied to all the data in a query before it's grouped by a GROUP BY clause.
 A HAVING condition is only applied to the grouped rows that are returned after a GROUP BY is applied.
 
+------------------------------
+
+IMPORTANT! 
+This means that if you want to filter on the result of an aggregation, you need to use HAVING. If you want to filter on a value that's present in the raw data, 
+you should use a simple WHERE clause.
+
 */
+
