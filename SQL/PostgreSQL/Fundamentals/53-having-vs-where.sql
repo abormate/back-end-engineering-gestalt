1080275399698f4c3126219f765fe670ff1fb88c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-11 07:02:49 +0800

    SQL LearnSpace -- Where and Having

---
+++ SQL/PostgreSQL/Fundamentals/53-having-vs-where.sql
@@ -0,0 +1,10 @@
+---------------------------------------- //
+--
+-- Having vs Where -- SQL StudySpace
+--
+---------------------------------------- //
+
+/*
+It's fairly common for developers to get confused about the difference between the HAVING and the WHERE clauses - they're pretty similar after all.
+
+*/
