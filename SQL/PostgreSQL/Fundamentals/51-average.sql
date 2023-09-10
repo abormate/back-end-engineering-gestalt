5eccd6b96d1171791f1a9be0cb417b0fd010d316
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-10 18:05:32 +0800

    SQL LearnSpace -- Average

--- SQL/PostgreSQL/Fundamentals/51-average.sql
+++ SQL/PostgreSQL/Fundamentals/51-average.sql
@@ -3,3 +3,11 @@
 -- Average
 --
 ---------------------------------------- //
+
+/*
+Just like we may want to find the minimum or maximum values within a dataset, sometimes we need to know the average!
+
+SQL offers us the AVG() function. Similar to MAX(), AVG() calculates the average of all non-NULL values.
+
+*/
+
