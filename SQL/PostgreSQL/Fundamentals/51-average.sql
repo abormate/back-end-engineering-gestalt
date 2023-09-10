4091b90967e04a7d4f7a762368172e3008a33126
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-10 18:12:43 +0800

    SQL LearnSpace -- Average

--- SQL/PostgreSQL/Fundamentals/51-average.sql
+++ SQL/PostgreSQL/Fundamentals/51-average.sql
@@ -21,3 +21,11 @@ from songs
 -- Assignment -- Practice
 ------------------------------ //
 
+/*
+Our marketing team is trying to determine the best marketing channels to advertise through but they need more information about our current users. 
+They wish to know the average age of users in the United States.
+
+Return a single value representing the average age of all users where the country_code is US.
+
+*/
+
