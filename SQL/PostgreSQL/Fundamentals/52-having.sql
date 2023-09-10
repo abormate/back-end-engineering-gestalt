cfa5920b157501a92ac398b5f88fbbc23b0590b8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-11 06:37:06 +0800

    SQL LearnSpace -- Having

--- SQL/PostgreSQL/Fundamentals/52-having.sql
+++ SQL/PostgreSQL/Fundamentals/52-having.sql
@@ -4,3 +4,9 @@
 --
 ---------------------------------------- //
 
+/*
+When we need to filter the results of a GROUP BY query even further, we can use the HAVING clause. The HAVING clause specifies a search condition for a group.
+
+The HAVING clause is similar to the WHERE clause, but it operates on groups after they've been grouped, rather than rows before they've been grouped.
+
+*/
