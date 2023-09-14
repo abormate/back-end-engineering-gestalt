ca6b44ff21a4ca4b73721a8c7f79c6a2dc8c07d6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-14 19:16:23 +0800

    SQL LearnSpace -- Round

--- SQL/PostgreSQL/Fundamentals/54-round.sql
+++ SQL/PostgreSQL/Fundamentals/54-round.sql
@@ -3,3 +3,8 @@
 -- Round -- SQL LearnSpace
 --
 --------------------------------------- //
+
+/*
+Sometimes we need to round some numbers, particularly when working with the results of an aggregation. We can use the ROUND() function to get the job done.
+
+*/
