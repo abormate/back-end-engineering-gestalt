85f39eaf9c094558a87c160a17eff6a9538872d6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-14 19:27:18 +0800

    SQL LearnSpace -- Subqueries

---
+++ SQL/PostgreSQL/Fundamentals/55-sub-query.sql
@@ -0,0 +1,14 @@
+--------------------------------------- //
+--
+-- Sub Queries -- SQL LearnSpace
+--
+--------------------------------------- //
+
+/*
+Sometimes a single query is not enough to retrieve the specific records we need.
+
+It is possible to run a query on the result set of another query - a query within a query! This is called "query-ception"... erm... I mean a "subquery".
+
+Subqueries can be very useful in a number of situations when trying to retrieve specific data that wouldn't be accessible by simply querying a single table.
+
+*/
