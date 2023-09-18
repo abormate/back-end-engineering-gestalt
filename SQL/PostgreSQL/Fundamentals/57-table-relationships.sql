8f300da4ec72077385e0556d1cf95086662b5ff4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-19 04:37:04 +0800

    SQL LearnSpace -- Table Relationships

--- SQL/PostgreSQL/Fundamentals/57-table-relationships.sql
+++ SQL/PostgreSQL/Fundamentals/57-table-relationships.sql
@@ -3,3 +3,9 @@
 -- Table Relationships -- SQL LearnSpace
 --
 ---------------------------------------- //
+
+/*
+Relational databases are powerful because of the relationships between the tables. These relationships help us to keep our databases clean and efficient. 
+A relationship between tables assumes that one of these tables has a foreign key that references the primary key of another table.
+
+*/
