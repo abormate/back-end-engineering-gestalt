a9fd2c456c4fed4162430a7373d5acdcd0e79a46
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-19 04:20:27 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/56-no-tables.sql
+++ SQL/PostgreSQL/Fundamentals/56-no-tables.sql
@@ -3,3 +3,11 @@
 -- No Tables
 --
 ---------------------------------------- //
+
+/*
+When working on a back-end application, this doesn't come up often, but it's important to remember that SQL is a full programming language. 
+We usually use it to interact with data stored in tables, but it's quite flexible and powerful.
+
+For example, you can SELECT information that's simply calculated, with no tables necessary.
+
+*/
