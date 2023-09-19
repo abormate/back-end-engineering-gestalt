c249c1b956f6465be92650173bd5bee9f259d23a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-20 07:44:00 +0800

    SQL LearnSpace -- One to Many

--- SQL/PostgreSQL/Fundamentals/58-one-to-many.sql
+++ SQL/PostgreSQL/Fundamentals/58-one-to-many.sql
@@ -4,3 +4,12 @@
 --
 ---------------------------------------- //
 
+/*
+When talking about the relationships between tables, a one-to-many relationship is probably the most commonly used relationship.
+
+A one-to-many relationship occurs when a single record in one table is related to potentially many records in another table.
+
+Note that the one->many relation only goes one way, a record in the second table can not be related to multiple records in the first table!
+
+*/
+
