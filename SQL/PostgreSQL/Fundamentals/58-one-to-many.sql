6abbf55e22271427c8f6c481de7712b198d81ed5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-20 08:34:00 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/58-one-to-many.sql
+++ SQL/PostgreSQL/Fundamentals/58-one-to-many.sql
@@ -43,3 +43,27 @@ Instead of a single users table where each user has a single country_code, do th
 ---- user_id: an integer foreign key to the users table's id field
 
 */
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  country_code TEXT NOT NULL,
+  username TEXT UNIQUE,
+  password TEXT NOT NULL,
+  is_admin BOOLEAN
+);
+
+-- Don't touch below this line --
+
+INSERT INTO users(name, age, username, password, is_admin)
+VALUES ('David', 34, 'david.lang', 'secure1234', false);
+
+INSERT INTO users(name, age, username, password, is_admin)
+VALUES ('Sam', 12, 'sam-show', 'nasjds134', false);
+
+INSERT INTO users(name, age, username, password, is_admin)
+VALUES ('Lane', 19, 'wagslane', '2jk3bAkm', false);
+
+INSERT INTO users(name, age, username, password, is_admin)
+VALUES ('Allan', 27, 'allan.jules', '243nldn', false);
