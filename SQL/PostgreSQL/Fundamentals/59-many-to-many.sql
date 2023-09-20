12858b55c5881cf9330c0fa48dcd9ad3aff4f559
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-21 06:38:56 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/59-many-to-many.sql
+++ SQL/PostgreSQL/Fundamentals/59-many-to-many.sql
@@ -13,3 +13,8 @@ A many-to-many relationship occurs when multiple records in one table can be rel
 -- Examples of Many to Many relationships
 ---------------------------- //
 
+/*
+-- A products table and a suppliers table - Products may have 0 to many suppliers, and suppliers can supply 0 to many products.
+-- A classes table and a students table - Students can take potentially many classes and classes can have many students enrolled.
+
+*/
