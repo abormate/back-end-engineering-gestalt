924b4674a7fd88892c8199f26d130372346d6c1c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-23 09:53:15 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/61-normal-forms.sql
+++ SQL/PostgreSQL/Fundamentals/61-normal-forms.sql
@@ -3,3 +3,14 @@
 -- Normal Forms -- StudySpace
 --
 ---------------------------------------- //
+
+/*
+The creator of "database normalization", Edgar F. Codd described different "normal forms" a database can adhere to. We'll talk about the most common ones.
+
+First normal form (1NF)
+Second normal form (2NF)
+Third normal form (3NF)
+Boyce-Codd normal form (BCNF)
+
+
+*/
