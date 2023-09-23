d74b343d919807cb15a68f79a1a5932f18743356
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-23 10:40:45 +0800

    SQL LearnSpace -- 1st Normal Form

---
+++ SQL/PostgreSQL/Fundamentals/62-1st-normal-form-1NF.sql
@@ -0,0 +1,13 @@
+---------------------------------------- //
+--
+-- 1st Normal Form (1NF) -- SQL LearnSpace
+--
+---------------------------------------- //
+
+/*
+To be compliant with first normal form, a database table simply needs to follow 2 rules:
+
+-- It must have a unique primary key.
+-- A cell can't have a nested table as its value (depending on the database you're using, this may not even be possible)
+
+*/
