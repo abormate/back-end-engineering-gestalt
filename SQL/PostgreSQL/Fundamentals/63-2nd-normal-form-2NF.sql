fd951e5350aeec8b7cef9a073f77e9096259fbd4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-26 05:41:47 +0800

    SQL LearnSpace -- 2NF

--- SQL/PostgreSQL/Fundamentals/63-2nd-normal-form-2NF.sql
+++ SQL/PostgreSQL/Fundamentals/63-2nd-normal-form-2NF.sql
@@ -4,3 +4,10 @@
 --
 ---------------------------------------- //
 
+/*
+A table in second normal form follows all the rules of 1st normal form, and one additional rule:
+
+All columns that are not part of the primary key are dependent on the entire primary key, and not just one of the columns in the primary key.
+
+*/
+
