3ccb451ae646e992a91f3ac4a7d3d2dc6ffca387
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-28 12:45:40 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/64-3rd-normal-form-3NF.sql
+++ SQL/PostgreSQL/Fundamentals/64-3rd-normal-form-3NF.sql
@@ -3,3 +3,10 @@
 -- 3rd Normal Form -- SQL LearnSpace
 --
 ---------------------------------------- //
+
+/*
+A table in 3rd normal form follows all the rules of 2nd normal form, and one additional rule:
+
+All columns that aren't part of the primary are dependent solely on the primary key.
+
+*/
