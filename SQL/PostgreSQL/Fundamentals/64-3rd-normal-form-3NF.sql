28a95e6c99a8d2da18b48d0d190c4ff5bf554ae7
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-28 12:57:19 +0800

    SQL LearnSpace --

--- SQL/PostgreSQL/Fundamentals/64-3rd-normal-form-3NF.sql
+++ SQL/PostgreSQL/Fundamentals/64-3rd-normal-form-3NF.sql
@@ -9,4 +9,7 @@ A table in 3rd normal form follows all the rules of 2nd normal form, and one add
 
 All columns that aren't part of the primary are dependent solely on the primary key.
 
+Notice that this is only slightly different from second normal form. In second normal form we can't have a column completely dependent on a part of the 
+primary key, and in third normal form we can't have a column that is entirely dependent on anything that isn't the entire primary key.
+
 */
