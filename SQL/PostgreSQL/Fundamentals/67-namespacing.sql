f0dbdf87e824fa5b3e48d5c6baf6c6e502743a76
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-30 13:02:23 +0800

    Backend SQL LearnSpace -- NameSpacing

---
+++ SQL/PostgreSQL/Fundamentals/67-namespacing.sql
@@ -0,0 +1,12 @@
+---------------------------------------- //
+--
+-- NameSpacing -- SQL LearnSpace
+--
+---------------------------------------- //
+
+/*
+When working with multiple tables, you can specify which table a field exists on using a dot (.) For example:
+
+*/
+
+table_name.column_name
