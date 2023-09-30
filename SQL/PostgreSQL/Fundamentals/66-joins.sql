b0c5d76d216fdb2bca08c524f5827ade027258da
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-09-30 12:13:33 +0800

    Backend SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/66-joins.sql
+++ SQL/PostgreSQL/Fundamentals/66-joins.sql
@@ -4,3 +4,9 @@
 --
 ---------------------------------------- //
 
+/*
+Joins are one of the most important features that SQL offers. Joins allow us to make use of the relationships we have set up between our tables. 
+In short, joins allow us to query multiple tables at the same time.
+
+*/
+
