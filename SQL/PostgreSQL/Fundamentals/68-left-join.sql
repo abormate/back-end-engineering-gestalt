44cf39063eebaf487e8b86e19029ce0a7ff6b5a4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-02 12:10:27 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/68-left-join.sql
+++ SQL/PostgreSQL/Fundamentals/68-left-join.sql
@@ -4,3 +4,10 @@
 --
 ---------------------------------------- //
 
+/*
+A LEFT JOIN will return every record from table_a regardless of whether or not any of those records have a match in table_b. A left join will also return any 
+matching records from table_b. Here is a Venn diagram to help visualize the effect of a LEFT JOIN.
+
+*/
+
+-- diagram image --> 
