8e3db93b59ecdb7cb488b4ae0092dea57a63582f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-02 11:56:13 +0800

    Backend SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/67-namespacing.sql
+++ SQL/PostgreSQL/Fundamentals/67-namespacing.sql
@@ -14,3 +14,5 @@ table_name.column_name
 SELECT students.name, classes.name
 FROM students
 INNER JOIN classes on classes.class_id = students.class_id;
+
+-- The above query returns the name field from the students table and the name field from the classes table.
