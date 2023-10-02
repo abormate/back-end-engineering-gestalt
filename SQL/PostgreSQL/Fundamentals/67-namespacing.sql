7bebe587a7c2c851bd54f693cd90079731d57a88
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-02 11:48:56 +0800

    Backend SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/67-namespacing.sql
+++ SQL/PostgreSQL/Fundamentals/67-namespacing.sql
@@ -10,3 +10,7 @@ When working with multiple tables, you can specify which table a field exists on
 */
 
 table_name.column_name
+
+SELECT students.name, classes.name
+FROM students
+INNER JOIN classes on classes.class_id = students.class_id;
