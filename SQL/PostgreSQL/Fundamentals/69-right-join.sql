86e58bfcc98780e828d68f3763466533f0e0eb17
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-03 15:45:11 +0800

    SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/69-right-join.sql
+++ SQL/PostgreSQL/Fundamentals/69-right-join.sql
@@ -3,3 +3,9 @@
 -- Right Join -- SQL LearnSpace
 --
 ---------------------------------------- //
+
+/*
+A RIGHT JOIN is, as you may expect, the opposite of a LEFT JOIN. It returns all records from table_b regardless of matches, and all matching records 
+between the two tables.
+
+*/
