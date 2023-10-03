189feb9efe3c677b733e7f58cf5cc8a0a0aef6e9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-03 16:30:43 +0800

    Backend SQL LearnSpace -- RIght Joins

--- SQL/PostgreSQL/Fundamentals/69-right-join.sql
+++ SQL/PostgreSQL/Fundamentals/69-right-join.sql
@@ -12,3 +12,9 @@ between the two tables.
 
 -- image diagram  --> https://prnt.sc/rfVa6rxSFcRX
 
+/*
+SQLite does not support right joins, but many dialects of SQL do! If you think about it, a RIGHT JOIN is just a LEFT JOIN with the order of the tables 
+switched, so it's not a big deal that SQLite doesn't support the syntax.
+
+*/
+
