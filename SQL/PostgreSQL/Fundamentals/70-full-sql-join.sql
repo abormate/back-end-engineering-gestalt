b9bc45db8c298f1c7f82e9630c69d0ecbc032249
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-04 18:35:25 +0800

    Backend SQL LearnSpace

--- SQL/PostgreSQL/Fundamentals/70-full-sql-join.sql
+++ SQL/PostgreSQL/Fundamentals/70-full-sql-join.sql
@@ -4,3 +4,9 @@
 --
 ---------------------------------------- //
 
+/*
+A FULL JOIN combines the result set of the LEFT JOIN and RIGHT JOIN commands. It returns all records from both from table_a and table_b regardless of 
+whether or not they have matches.
+
+*/
+
