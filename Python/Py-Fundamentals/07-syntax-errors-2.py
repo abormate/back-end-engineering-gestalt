f0bc0143ff88dc33ad8ef90ccf6d8ee7eb3676ef
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-05 20:41:36 +0800

    Python StudySpace -- Syntax Rules

--- Python/Py-Fundamentals/07-syntax-errors-2.py
+++ Python/Py-Fundamentals/07-syntax-errors-2.py
@@ -4,3 +4,9 @@
 #
 # -------------------------------------- #
 
+"""
+Syntax in programming are the rules that define what properly structured expressions and statements look like in that language. For example, in Python, 
+the following would be considered correct syntax:
+
+"""
+
