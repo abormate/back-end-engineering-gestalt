0c3523cfe626f7157f4351647f49e5d020397280
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-06 07:49:43 +0800

    Python StudySpace

--- Python/Py-Fundamentals/08-startup-bug.py
+++ Python/Py-Fundamentals/08-startup-bug.py
@@ -13,3 +13,9 @@ A new startup has a bug in its server code. The code is supposed to print messag
 # Old -- Buggy Code --
 # ---------------------------- #
 
+"""
+print("Starting up server...')
+prnt("local server is listening on port 8080")
+
+"""
+
