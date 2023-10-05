804588c6fc4fd2ec7f3dd1640304a13d0a73d1cd
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-06 07:48:11 +0800

    Python StudySpace -- Startup Bug

--- Python/Py-Fundamentals/08-startup-bug.py
+++ Python/Py-Fundamentals/08-startup-bug.py
@@ -4,3 +4,8 @@
 #
 # --------------------------------------- #
 
+"""
+A new startup has a bug in its server code. The code is supposed to print messages indicating the server has started successfully.
+
+"""
+
