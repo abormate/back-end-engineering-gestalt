cdc2fdf5656657fa8d47eba7d565ff440fb06290
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-05 19:37:01 +0800

    Python StudySpace -- Syntax Errors

--- Python/Py-Fundamentals/06-syntax-errors.py
+++ Python/Py-Fundamentals/06-syntax-errors.py
@@ -1,6 +1,33 @@
 # -------------------------------------- #
 #
-# Syntax Errors -- Python StudySpace --
+# -- Syntax Errors -- Python StudySpace --
 #
 # -------------------------------------- #
 
+'''
+Syntax is jargon for "valid code that the computer can understand". For example, the following code is invalid syntax:
+
+'''
+
+prnt("hello world")  #renders a syntax error - misspelling
+
+
+"""
+prnt() is not a valid function because "print" is spelled incorrectly. As a result, an error will be thrown and the code won't execute.
+
+"""
+
+# ------------------------------------ #
+#
+# -- Assignment -- Practice --
+#
+# ------------------------------------ #
+
+"""
+There's a bug in our game! Our players are getting mad because they pay us $20/month to have access to "Fantasy Quest".
+
+Find the syntax error in the code and correct it.
+
+"""
+
+print("Welcome to Fantasy Quest!")
