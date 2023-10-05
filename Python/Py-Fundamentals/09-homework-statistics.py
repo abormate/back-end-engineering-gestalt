16ba4ce00118e36c2bf7c24c84d3a4244542bfe2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-06 07:53:22 +0800

    Python StudySpace

--- Python/Py-Fundamentals/09-homework-statistics.py
+++ Python/Py-Fundamentals/09-homework-statistics.py
@@ -4,3 +4,16 @@
 #
 # --------------------------------------- #
 
+"""
+A local high school student was asked to do some statistics work in Python. She's trying to calculate the average number of students at all of the 
+local high schools.
+
+CHALLENGE
+Debug the code so that it correctly prints the solution to the math problem.
+
+"""
+
+# -------------------------- #
+# -- Old Code -- Buggy Code --
+# -------------------------- #
+
