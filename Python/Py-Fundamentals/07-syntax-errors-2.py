c17b2f493e05cc3217e26357890727038339b10c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-05 20:49:00 +0800

    Python StudySpace

--- Python/Py-Fundamentals/07-syntax-errors-2.py
+++ Python/Py-Fundamentals/07-syntax-errors-2.py
@@ -19,3 +19,15 @@ While in a different programming language, like Go, the correct syntax would be:
 """
 
 fmt.Println("hello world")
+
+
+"""
+Code can have many different problems that prevent it from working as intended. Some examples include:
+
+--> A bug in the logic. For example, a program that should add numbers multiplies them instead
+--> A problem with speed. A program that calculates how to play the perfect game of chess might never be able to finish because it requires too many calculations.
+
+--> A problem with syntax. This is the most common problem for new developers. Luckily the Python interpreter will try to give you a descriptive error message 
+in the console to help you find the problem.
+
+"""
