01b5b041fb55d1767ce2f514ceec75f5e021026a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-07 14:56:18 +0800

    Python StudySpace -- Variables Vary

--- Python/Py-Fundamentals/12-variables-vary.py
+++ Python/Py-Fundamentals/12-variables-vary.py
@@ -4,3 +4,10 @@
 #
 # --------------------------------------- #
 
+"""
+Variables are called "variables" because they can hold any value and that value can change (it varies).
+
+For example, the following will print 20:
+
+"""
+
