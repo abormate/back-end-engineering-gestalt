c3ae1887f66413b4f1dbc96310fe35dc6d306ce1
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-07 14:46:22 +0800

    Python StudySpace -- Variables

--- Python/Py-Fundamentals/11-variables.py
+++ Python/Py-Fundamentals/11-variables.py
@@ -4,3 +4,13 @@
 #
 # -------------------------------------- #
 
+"""
+Variables are how we store data in our program. So far we've been directly printing data by passing it directly into the print() function.
+
+Now we are going to learn to save the data in variables so we can use and change it before we need to print it.
+
+A variable is a name that we define that will point to some data. For example, I could define a new variable called my_height and set its value to 100. 
+I could also define a variable called my_name and set it equal to "Lane".
+
+"""
+
