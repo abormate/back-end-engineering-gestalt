7b4f6d1df08608a8d841b31b70e06cc0d7221ac3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-10 09:34:52 +0800

    Python StudySpace -- Comments

---
+++ Python/Py-Fundamentals/15-comments.py
@@ -0,0 +1,12 @@
+# -------------------------------------- #
+#
+# -- Comments -- Python StudySpace --
+#
+# -------------------------------------- #
+
+"""
+Comments don't run like code, they are ignored by the computer. Comments are useful for adding reminders or 
+explaining what a piece of code does in plain English.
+
+"""
+
