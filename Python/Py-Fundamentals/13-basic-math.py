59ddee6b5934b889a943f864f5b7a4f4d54bb526
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-10 09:21:42 +0800

    Python StudySpace

--- Python/Py-Fundamentals/13-basic-math.py
+++ Python/Py-Fundamentals/13-basic-math.py
@@ -10,3 +10,9 @@ Now that we know how to store and change the value of variables let's do some ma
 Here are some examples of common mathematical operators in Python syntax.
 
 """
+
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+
