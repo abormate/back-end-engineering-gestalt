5ca1e05b6b4587045251fbb8a46562e31b57fbb4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-10 09:15:42 +0800

    Python StudySpace -- Variables

--- Python/Py-Fundamentals/12-variables-vary.py
+++ Python/Py-Fundamentals/12-variables-vary.py
@@ -11,3 +11,44 @@ For example, the following will print 20:
 
 """
 
+acceleration = 10
+acceleration = 20
+print(acceleration)
+
+"""
+The line acceleration = 20 reassigns the value of acceleration to 20. It overwrites whatever was being held in the acceleration variable before.
+
+"""
+
+# ------------------------ #
+# -- Assignment -- Practice --
+# ------------------------ #
+
+"""
+We need to reduce our hero's health as they take damage in the game.
+
+Before each print() function in the provided code, change the value of player_health to 100 less than it was before.
+
+"""
+
+player_health = 1000
+
+# reduce by 100 here
+
+print(player_health)
+
+# and here
+
+print(player_health)
+
+# and here
+
+print(player_health)
+
+# and here
+
+print(player_health)
+
+# and here
+
+print(player_health)
