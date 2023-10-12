da8276761dcbe6ba6161468fcad54c8f5cb8d370
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-12 09:17:56 +0800

    Python StudySpace

--- Python/Py-Fundamentals/16-variable-names.py
+++ Python/Py-Fundamentals/16-variable-names.py
@@ -4,3 +4,11 @@
 #
 # -------------------------------------- #
 
+"""
+Variable names can't have spaces, they're continuous strings of characters.
+
+In Python you should use "snake_case" when creating variable names - it's become the "rule of thumb" for the language. By way of comparison, 
+"camel case" is where the beginning of each new word except the first is capitalized.
+
+"""
+
