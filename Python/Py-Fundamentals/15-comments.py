3a80aaa718fbf7c8da49eed9133d6dc6d12628cd
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-12 09:12:54 +0800

    Python StudySpace

--- Python/Py-Fundamentals/15-comments.py
+++ Python/Py-Fundamentals/15-comments.py
@@ -37,3 +37,7 @@ This is useful if you don't want to add the # to the start of each line when wri
 """
 
 
+# --------------------------- #
+# -- Assignment -- Practice --
+# --------------------------- #
+
