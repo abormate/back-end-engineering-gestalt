0fbe690882f27a42b9a3cbeebd63470a6c705a34
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-12 09:11:25 +0800

    Python StudySpace -- Comments

--- Python/Py-Fundamentals/15-comments.py
+++ Python/Py-Fundamentals/15-comments.py
@@ -17,7 +17,10 @@ explaining what a piece of code does in plain English.
 # speed is a variable describing how fast your player moves
 speed = 2
 
+
 # --------------------------- #
 # -- Multi-Line Comments or DocStrings --
 # --------------------------- #
+
+# You can use triple quotes to start and end multi-line comments as well:
 
