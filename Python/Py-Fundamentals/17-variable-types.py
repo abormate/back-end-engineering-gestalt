bd354983a72347f4b613f645ba5b818900997051
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-13 15:44:07 +0800

    Python StudySpace -- Var Types

--- Python/Py-Fundamentals/17-variable-types.py
+++ Python/Py-Fundamentals/17-variable-types.py
@@ -10,3 +10,9 @@
 # -- String Type --
 # -------------------------- #
 
+"""
+"Strings" are raw text in coding speak. They are called "strings" because they are a list of characters strung together. Strings are declared in Python by 
+using single quotes or double quotes. That said, for consistency's sake, we prefer double quotes.
+
+"""
+
