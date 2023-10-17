c7f33d6bbe9516ebda429876aa549153ff83c19e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-17 10:42:13 +0800

    Python StudySpace -- Var Types

--- Python/Py-Fundamentals/17-variable-types.py
+++ Python/Py-Fundamentals/17-variable-types.py
@@ -58,9 +58,25 @@ Fix the bugs in the code to move on. player_health should be an integer and play
 
 """
 
+# -------------------------- #
+# -- Old buggy code --
+# -------------------------- #
+
 player_health = "100"
 
 player_has_magic = "True"
+
+# don't touch below this line
+print(f"player_health is a/an {type(player_health)}")
+print(f"player_has_magic is a/an {type(player_has_magic)}")
+
+# -------------------------- #
+# -- Fixed new code --
+# -------------------------- #
+
+player_health = 100
+
+player_has_magic = True
 
 # don't touch below this line
 print(f"player_health is a/an {type(player_health)}")
