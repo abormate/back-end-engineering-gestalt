234b910108b279c264c39a95a7c65e35bf6f4798
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-17 10:37:59 +0800

    Python StudySpace -- Var Types

--- Python/Py-Fundamentals/17-variable-types.py
+++ Python/Py-Fundamentals/17-variable-types.py
@@ -53,3 +53,15 @@ is_tall = True
 # -- Assignment -- Practice --
 # -------------------------- #
 
+"""
+Fix the bugs in the code to move on. player_health should be an integer and player_has_magic should be a boolean.
+
+"""
+
+player_health = "100"
+
+player_has_magic = "True"
+
+# don't touch below this line
+print(f"player_health is a/an {type(player_health)}")
+print(f"player_has_magic is a/an {type(player_has_magic)}")
