017de272e8f6f0cab1cd25c395f98d4ebf1e16f2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-17 10:44:24 +0800

    Python StudySpace -- None Type Var

---
+++ Python/Py-Fundamentals/18-nonetype-variables.py
@@ -0,0 +1,14 @@
+# -------------------------------------- #
+#
+# -- Nonetype Variables -- Python StudySpace --
+#
+# -------------------------------------- #
+
+"""
+Not all variables have a value. We can declare an "empty" variable by setting it to None.
+
+"""
+
+empty = None
+
+
