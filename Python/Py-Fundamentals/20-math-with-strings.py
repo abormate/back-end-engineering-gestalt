ca7872fbd11fddebbc3d6e5cee5d9776b42c99a2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-18 19:45:42 +0800

    Python StudySpace

--- Python/Py-Fundamentals/20-math-with-strings.py
+++ Python/Py-Fundamentals/20-math-with-strings.py
@@ -4,3 +4,11 @@
 #
 # -------------------------------------- #
 
+"""
+Most of the math operators we went over earlier don't work with strings, aside from the + addition operator. When working with strings the + operator 
+performs a "concatenation".
+
+"Concatenation" is a fancy word that means the joining of two strings.
+
+"""
+
