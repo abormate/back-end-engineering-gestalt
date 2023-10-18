6fb68c306f4653119941c18ca197a4e64c3d71ff
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-18 13:51:47 +0800

    Python StudySpace -- Dynamic Typing of Variables

--- Python/Py-Fundamentals/19-dynamic-typing.py
+++ Python/Py-Fundamentals/19-dynamic-typing.py
@@ -3,3 +3,15 @@
 # -- Dynamic Typing --
 #
 # -------------------------------------- #
+
+"""
+Python is dynamically typed. All this means is that a variable can store any type, and that type can change.
+
+For example, if I make a number variable, I can later change that variable to a string:
+
+This is valid:
+
+"""
+
+speed = 5
+speed = "five"
