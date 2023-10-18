21e2946e15b9226a8526689ed9f5cc703120b455
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-18 19:47:31 +0800

    Python StudySpace

--- Python/Py-Fundamentals/20-math-with-strings.py
+++ Python/Py-Fundamentals/20-math-with-strings.py
@@ -12,3 +12,14 @@ performs a "concatenation".
 
 """
 
+first_name = "Lane "
+last_name = "Lane"
+full_name = first_name + last_name
+
+""""
+full_name now holds the value "Lane Lane".
+
+Notice the extra space at the end of "Lane " in the first_name variable. That extra space is there to separate the words in the final result: "Lane Wagner".
+
+"""
+
