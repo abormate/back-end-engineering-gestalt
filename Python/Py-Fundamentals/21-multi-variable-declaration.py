be83517161edd63dfd9f29b663df2b61d3dde54e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-19 17:26:31 +0800

    Python StudySpace

--- Python/Py-Fundamentals/21-multi-variable-declaration.py
+++ Python/Py-Fundamentals/21-multi-variable-declaration.py
@@ -13,3 +13,10 @@ sword_name, sword_damage, sword_length = "Excalibur", 10, 200
 sword_name = "Excalibur"
 sword_damage = 10
 sword_length = 200
+
+"""
+Any number of variables can be declared on the same line, and variables declared on the same line should be related to one another in some way so that the code remains easy to understand.
+
+We call code that's easy to understand "clean code".
+
+"""
