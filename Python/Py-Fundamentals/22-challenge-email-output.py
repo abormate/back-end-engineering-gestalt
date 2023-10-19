39ee28a690c58a268cf61ebface235b6bbd7bb5d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-19 17:36:22 +0800

    Python StudySpace -- Email Output Exercise

--- Python/Py-Fundamentals/22-challenge-email-output.py
+++ Python/Py-Fundamentals/22-challenge-email-output.py
@@ -29,6 +29,10 @@ Please consider filling out our customer survey.
 
 """
 
+# --------------------------- #
+# -- Set up Code --
+# --------------------------- #
+
 email_start = "Thank you for shopping with us!"
 email_middle = "Your total today was: "
 email_end = "Please consider filling out our customer survey."
@@ -37,3 +41,18 @@ total_price = "20.25"
 
 # don't touch above this line
 
+# --------------------------- #
+# -- Final code --
+# --------------------------- #
+
+email_start = "Thank you for shopping with us!"
+email_middle = "Your total today was: "
+email_end = "Please consider filling out our customer survey."
+dollar_sign = "$"
+total_price = "20.25"
+
+# don't touch above this line
+
+print(email_start)
+print(email_middle + dollar_sign + total_price)
+print(email_end)
