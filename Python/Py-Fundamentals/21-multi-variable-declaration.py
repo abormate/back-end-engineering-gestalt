6778c34b899669629261d01bc09bdd2fe798534b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-19 17:03:42 +0800

    Python StudySpace

--- Python/Py-Fundamentals/21-multi-variable-declaration.py
+++ Python/Py-Fundamentals/21-multi-variable-declaration.py
@@ -5,3 +5,5 @@
 # -------------------------------------- #
 
 # We can save space when creating many new variables by declaring them on the same line:
+
+sword_name, sword_damage, sword_length = "Excalibur", 10, 200
