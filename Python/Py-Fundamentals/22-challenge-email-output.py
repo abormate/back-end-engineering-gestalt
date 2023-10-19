ca183c35a5a212c613799a67c89f2e2a5a6fe9d0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-19 17:31:25 +0800

    Python StudySpace -- Challenge Exercise

--- Python/Py-Fundamentals/22-challenge-email-output.py
+++ Python/Py-Fundamentals/22-challenge-email-output.py
@@ -4,3 +4,8 @@
 #
 # --------------------------------------- #
 
+"""
+An e-commerce store has configured an automated receipt, but can't figure out how to print the email message in the correct order.
+
+"""
+
