9aac252a0c5157418bfcc2c15bb23efec5cce6aa
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-20 12:13:42 +0800

    Python StudySpace -- Playoffs

--- Python/Py-Fundamentals/23-playoff-challenge.py
+++ Python/Py-Fundamentals/23-playoff-challenge.py
@@ -10,3 +10,22 @@ through the 7 games.
 
 """
 
+# --------------------------- #
+# -- Challenge -- Buggy Code --
+# --------------------------- #
+
+game_one_score = 97
+game_two_score = 91
+game_three_score = 106
+game_four_score = 105
+game_five_score = 96
+game_six_score = 93
+game_seven_score = 104
+
+# Don't touch above this line
+
+average_score = # ?
+
+# Don't touch below this line
+
+print(round(average_score))
