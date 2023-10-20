b743ebccf54e8eda1f51d8e5ac146196e13b230b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-20 12:12:42 +0800

    Python StudySpace -- Playoffs

--- Python/Py-Fundamentals/23-playoff-challenge.py
+++ Python/Py-Fundamentals/23-playoff-challenge.py
@@ -4,3 +4,9 @@
 #
 # -------------------------------------- #
 
+"""
+Allan's favorite NBA Basketball team just played a 7 game series in the Playoffs. He needs help writing a program that will calculate the average points scored by his team 
+through the 7 games.
+
+"""
+
