a5abcfba2b7fe8271e169e4ef866b67078b07df4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-20 12:23:30 +0800

    Python StudySpace -- Poem Challenge

--- Python/Py-Fundamentals/24-poem-challenge.py
+++ Python/Py-Fundamentals/24-poem-challenge.py
@@ -9,3 +9,15 @@ Tim is working on a program that writes a poem to the console. However, Tim is h
 
 """
 
+# ------------------------- #
+# -- Challenge -- Debug Code
+# ------------------------- #
+
+"""
+Use string concatenation to assign values to the line_one and line_two variables correctly.
+
+line_one should include part 1 and 2
+line_two should include part 3 and 4
+
+"""
+
