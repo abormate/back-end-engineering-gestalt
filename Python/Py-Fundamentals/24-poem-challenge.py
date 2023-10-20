89cd99c5d8be1218950c4151dbdcccb2c5ade2ee
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-20 12:21:27 +0800

    Python StudySpace -- Poem Challenge

--- Python/Py-Fundamentals/24-poem-challenge.py
+++ Python/Py-Fundamentals/24-poem-challenge.py
@@ -4,3 +4,8 @@
 #
 # -------------------------------------- #
 
+"""
+Tim is working on a program that writes a poem to the console. However, Tim is having trouble getting the poem to print on only two lines.
+
+"""
+
