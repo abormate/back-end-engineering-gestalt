ffb70dd34822f7a8f16c068476c5f94c7668cd5d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-23 20:14:37 +0800

    Python StudySpace -- Functions and Arguments

--- Python/Py-Fundamentals/25-functions.py
+++ Python/Py-Fundamentals/25-functions.py
@@ -95,3 +95,18 @@ print(f"Player 2 has an attack area of {player2_area} square meters")
 # -- Fixed Code -- 
 # --------------------------- #
 
+def area_of_circle(r):
+    return 3.14 * r * r
+
+
+# don't touch above this line
+
+player1_area = area_of_circle(1)
+def area2(r2):
+    return area_of_circle(r2)
+player2_area = area2(0.5)
+
+# don't touch below this line
+
+print(f"Player 1 has an attack area of {player1_area} square meters")
+print(f"Player 2 has an attack area of {player2_area} square meters")
