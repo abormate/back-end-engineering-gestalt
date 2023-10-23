40f75bc8902ae4a5da7901bd703feae0f1e820c0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-23 19:54:59 +0800

    Python StudySpace -- Functions

--- Python/Py-Fundamentals/25-functions.py
+++ Python/Py-Fundamentals/25-functions.py
@@ -4,3 +4,8 @@
 #
 # -------------------------------------- #
 
+"""
+Functions allow us to reuse and organize code. For example, let's pretend we need to calculate the area of a circle. We can use the formula area = pi * r^2, or in code:
+
+"""
+
