c7c46410502554649c6bce5b923573e846ec2f6d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-25 19:33:00 +0800

    Python StudySpace

--- Python/Py-Fundamentals/26-multiple-parameters.py
+++ Python/Py-Fundamentals/26-multiple-parameters.py
@@ -4,3 +4,7 @@
 #
 # ----------------------------------------- #
 
+"""
+Functions can have multiple parameters (which is just a fancy name for "inputs"). For example, this subtract function accepts 2 values as parameters, a and b:
+
+"""
