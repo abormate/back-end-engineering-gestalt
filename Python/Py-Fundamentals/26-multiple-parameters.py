213f61d48cc923c942fe7c9846bb0a4a6688bf55
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-25 19:38:40 +0800

    Python StudySpace -- Parameters

--- Python/Py-Fundamentals/26-multiple-parameters.py
+++ Python/Py-Fundamentals/26-multiple-parameters.py
@@ -21,3 +21,40 @@ print(result)
 # -- Assignment -- Practice --
 # ------------------------- #
 
+"""
+We need to be able to calculate the total damage from an attack involving 3 different enemies. Complete the calculate_damage function that takes three numbers as its 
+parameters and returns the result of adding them all together.
+
+"""
+
+# ------------------------ #
+# -- Buggy Code --
+# ------------------------ #
+
+def calculate_damage(enemy_one_dmg, enemy_two_dmg, enemy_three_dmg):
+    # ?
+
+
+# Don't touch below this line
+
+print(f"You dealt {calculate_damage(2, 3, 4)} points of damage!")
+print(f"You dealt {calculate_damage(-1, 4, 3)} points of damage!")
+print(f"You dealt {calculate_damage(3, 2, 4)} points of damage!")
+print(f"You dealt {calculate_damage(1, 4, 2)} points of damage!")
+
+
+# ---------------------- #
+# -- Fixed Code --
+# ---------------------- #
+
+def calculate_damage(enemy_one_dmg, enemy_two_dmg, enemy_three_dmg):
+    return enemy_one_dmg + enemy_two_dmg + enemy_three_dmg
+
+
+# Don't touch below this line
+
+print(f"You dealt {calculate_damage(2, 3, 4)} points of damage!")
+print(f"You dealt {calculate_damage(-1, 4, 3)} points of damage!")
+print(f"You dealt {calculate_damage(3, 2, 4)} points of damage!")
+print(f"You dealt {calculate_damage(1, 4, 2)} points of damage!")
+
