d3bc51bbdb46ff33193d476772dbb506b63b6257
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-25 19:33:18 +0800

    Python StudySpace

--- Python/Py-Fundamentals/26-multiple-parameters.py
+++ Python/Py-Fundamentals/26-multiple-parameters.py
@@ -8,3 +8,11 @@
 Functions can have multiple parameters (which is just a fancy name for "inputs"). For example, this subtract function accepts 2 values as parameters, a and b:
 
 """
+
+def subtract(a, b):
+    return a - b
+
+
+result = subtract(5, 3)
+print(result)
+# 2
