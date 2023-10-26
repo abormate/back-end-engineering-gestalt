a41aa7102e49ad6a433186b4d0919a07d47ec6f8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-26 19:56:32 +0800

    Python StudySpace

--- Python/Py-Fundamentals/27-where-declare-functions.py
+++ Python/Py-Fundamentals/27-where-declare-functions.py
@@ -3,3 +3,10 @@
 # -- Where to Declare Functions -- Python StudySpace --
 #
 # -------------------------------------- #
+
+"""
+You've probably noticed that a variable needs to be declared before it's used. For example, the following doesn't work
+
+
+
+"""
