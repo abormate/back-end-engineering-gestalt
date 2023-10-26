ce1928670609e4e8300074b9953acf45e43afd20
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-26 19:59:32 +0800

    Python StudySpace

--- Python/Py-Fundamentals/27-where-declare-functions.py
+++ Python/Py-Fundamentals/27-where-declare-functions.py
@@ -17,3 +17,13 @@ my_name = 'Lane'
 my_name = 'Lane'
 print(my_name)
 
+
+"""
+Lines of code execute in order from top to bottom, so a variable needs to be created before it can be used. That means that if you define a function, you can't call that 
+function until after the definition.
+
+The main() function is a convention used in many programming languages to specify the entrypoint of an application. By defining a single main function, and only calling main() 
+at the end of the entire program we ensure that all of our functions are defined before they're called.
+
+"""
+
