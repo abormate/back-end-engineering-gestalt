acd25c6ec8065d2a567acc91f90f2353b82db21c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-27 09:26:31 +0800

    Python StudySpace

--- Python/Py-Fundamentals/28-order-of-functions.py
+++ Python/Py-Fundamentals/28-order-of-functions.py
@@ -5,3 +5,9 @@
 # -------------------------------------- #
 
 # All functions must be defined before they're used.
+
+"""
+You might think this would make structuring Python code difficult because the order in which the functions are declared can quickly become so dependent on each other that 
+writing anything becomes impossible.
+
+"""
