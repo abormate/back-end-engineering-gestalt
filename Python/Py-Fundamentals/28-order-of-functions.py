9ac4d65f4d8880fb443d3bd698eb879583c5f199
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-27 09:26:58 +0800

    Python StudySpace

--- Python/Py-Fundamentals/28-order-of-functions.py
+++ Python/Py-Fundamentals/28-order-of-functions.py
@@ -10,4 +10,7 @@
 You might think this would make structuring Python code difficult because the order in which the functions are declared can quickly become so dependent on each other that 
 writing anything becomes impossible.
 
+As it turns out, most Python developers solve this problem by simply defining all the functions in their program first, then finally calling the entrypoint function last. 
+If you do that, then the order of the function definitions doesn't matter. The entrypoint function is conventionally called "main".
+
 """
