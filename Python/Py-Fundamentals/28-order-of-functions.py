8e883dec745e7980cea8c4837b384acc37a27080
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-27 09:31:23 +0800

    Python StudySpace

--- Python/Py-Fundamentals/28-order-of-functions.py
+++ Python/Py-Fundamentals/28-order-of-functions.py
@@ -32,3 +32,27 @@ main()
 # ------------------------- #
 
 # Write a function called to_celsius that converts a temperature in Fahrenheit to Celsius
+
+# ------------------------ #
+# -- Set up Code --
+# ------------------------ #
+
+# def to_celsius(f):
+    # ?
+
+
+## Don't touch below this line
+
+
+def test(f):
+    c = round(to_celsius(f), 2)
+    print(f"{f} degrees fahrenheit is {c} degrees celsius")
+
+
+test(100)
+test(88)
+test(104)
+test(112)
+
+
+
