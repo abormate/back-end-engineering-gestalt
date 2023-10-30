01689b4b4cf4a0ce547b54fbc7a102daa31c48c0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-30 14:24:11 +0800

    Python StudySpace

---
+++ Python/Py-Fundamentals/31-multiple-return.py
@@ -0,0 +1,12 @@
+# -------------------------------------- #
+#
+# -- Multiple Return Values --
+# 
+# -------------------------------------- #
+
+"""
+In Python, we can return more than one value from a function. All we need to do is separate each value we are returning by a comma, and when we call the function, 
+we need to capture all the returned values in individual variables.
+
+"""
+
