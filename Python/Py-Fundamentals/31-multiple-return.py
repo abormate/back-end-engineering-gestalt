630a6cc147b3f8027c6fbebc6684206253090a84
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-10-31 18:23:42 +0800

    Python StudySpace -- Multi Returns

--- Python/Py-Fundamentals/31-multiple-return.py
+++ Python/Py-Fundamentals/31-multiple-return.py
@@ -40,3 +40,23 @@ Second, the power input after adding 1 to it.
 For example:
 
 """
+
+def become_warrior(first_name, last_name, power):
+    return first_name + " " + last_name + " the warrior", power+1
+
+
+# Don't edit below this line
+
+
+def main():
+    test("Frodo", "Baggins", 5)
+    test("Bilbo", "Baggins", 10)
+    test("Gandalf", "The Grey", 9000)
+
+
+def test(first_name, last_name, power_level):
+    title_string, power = become_warrior(first_name, last_name, power_level)
+    print(f"{title_string} has a power level of: {power}")
+
+
+main()
