d6375bfa4af20f3345c63337690737676e32cad9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-01 13:08:08 +0800

    Python StudySpace

--- Python/Py-Fundamentals/32-parameters-arguments.py
+++ Python/Py-Fundamentals/32-parameters-arguments.py
@@ -4,3 +4,14 @@
 #
 # --------------------------------------- #
 
+"""
+Parameters are the names used for inputs when defining a function. Arguments are the names of the inputs supplied when a function is called.
+
+To reiterate, arguments are the actual values that go into the function, say 42.0, "the dark knight", or True. Parameters are the names we use in the function definition to refer 
+to those values, which at the time of writing the function, could be anything.
+
+That said, it is important to understand that this is all semantics, and frankly developers are really lazy with these definitions. You'll often hear the words arguments and 
+parameters used interchangeably.
+
+"""
+
