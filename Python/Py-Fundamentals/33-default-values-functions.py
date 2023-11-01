bf4361a10f28d84bd8233da74b055ac5f8ee48c3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-01 14:10:01 +0800

    Python StudySpace -- Default Values

--- Python/Py-Fundamentals/33-default-values-functions.py
+++ Python/Py-Fundamentals/33-default-values-functions.py
@@ -4,3 +4,11 @@
 #
 # --------------------------------------- #
 
+"""
+Python has a way to specify a default value for function arguments. This can be convenient if a function has arguments that are essentially "optional", and you as the 
+function creator want to use a specific default value in case the caller doesn't provide one
+
+A default value is created by using the assignment (=) operator in the function signature.
+
+"""
+
