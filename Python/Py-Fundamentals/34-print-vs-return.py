6d8b9ffdde0bc33d5638a45d2755e68ec4930bb6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-02 18:24:07 +0800

    Python StudySpace -- Return VS Print

--- Python/Py-Fundamentals/34-print-vs-return.py
+++ Python/Py-Fundamentals/34-print-vs-return.py
@@ -69,3 +69,29 @@ test("Frodo", "Baggins", "warrior")
 test("Bilbo", "Baggins", "thief")
 test("Gandalf", "The Grey", "wizard")
 test("Aragon", "Son of Arathorn", "ranger")
+
+# ----------------------------- #
+# -- Fixed Code --
+# ----------------------------- #
+
+def get_title(first_name, last_name, job):
+    title = first_name + " " + last_name + " the " + job
+    return title
+
+
+# Don't touch below this line
+
+
+def test(first_name, last_name, job):
+    title = get_title(first_name, last_name, job)
+    print(f"First name: {first_name}")
+    print(f"Last name: {last_name}")
+    print(f"Job: {job}")
+    print(f"Title: {title}")
+    print("=====================================")
+
+
+test("Frodo", "Baggins", "warrior")
+test("Bilbo", "Baggins", "thief")
+test("Gandalf", "The Grey", "wizard")
+test("Aragon", "Son of Arathorn", "ranger")
