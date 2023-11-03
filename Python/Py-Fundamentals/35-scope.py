6a2dd921235d27f64a8052dc16cd3dfc05c60d31
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-03 20:51:38 +0800

    Python StudySpace -- Scope

--- Python/Py-Fundamentals/35-scope.py
+++ Python/Py-Fundamentals/35-scope.py
@@ -4,3 +4,12 @@
 #
 # --------------------------------------- #
 
+"""
+Scope refers to where a variable or function name is available to be used. For example, 
+when we create variables in a function (by giving names to our parameters for example), 
+that data is not available outside of that function.
+
+For example:
+
+"""
+
