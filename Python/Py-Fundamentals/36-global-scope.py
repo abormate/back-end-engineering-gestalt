093c6bbdb50382e546dd6835250182370a153737
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-04 10:27:38 +0800

    Python StudySpace -- Scope of Variables (Global Scope)

--- Python/Py-Fundamentals/36-global-scope.py
+++ Python/Py-Fundamentals/36-global-scope.py
@@ -38,3 +38,47 @@ Declare the variable player_level at the top of the global scope and set it to 4
 
 """
 
+# ---------------------------- #
+# -- Buggy Code --
+# ---------------------------- #
+
+# ?
+
+
+def calculate_health(modifier):
+#    return player_level * modifier
+
+
+# def calculate_primary_stats(armor_bonus, modifier):
+#    return armor_bonus + modifier + player_level
+
+
+# Don't touch below this line
+
+print(f"Character has {calculate_health(10)} max health.")
+
+print(f"Character has {calculate_primary_stats(3, 8)} primary stats.")
+
+
+# ------------------------- #
+# -- Fixed Code --
+# ------------------------- #
+
+player_level = 4
+
+
+def calculate_health(modifier):
+    return player_level * modifier
+
+
+def calculate_primary_stats(armor_bonus, modifier):
+    return armor_bonus + modifier + player_level
+
+
+# Don't touch below this line
+
+print(f"Character has {calculate_health(10)} max health.")
+
+print(f"Character has {calculate_primary_stats(3, 8)} primary stats.")
+
+
