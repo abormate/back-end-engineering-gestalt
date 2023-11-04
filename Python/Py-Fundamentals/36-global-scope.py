c6337f3306018ed54a13e001056f6c0bc6593ac7
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-04 10:22:08 +0800

    Python StudySpace -- Global Scope

--- Python/Py-Fundamentals/36-global-scope.py
+++ Python/Py-Fundamentals/36-global-scope.py
@@ -4,3 +4,12 @@
 #
 # ---------------------------------------- #
 
+"""
+So far we've been working in the global scope. That means that when we define a 
+variable or a function, that name is accessible in every other place in our program,
+even within other functions.
+
+For example:
+
+"""
+
