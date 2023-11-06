e1a5aa6dbeb597b98a320f09ac8c5698d91bdf2f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-06 12:07:24 +0800

    Python StudySpace -- Unit Tests

--- Python/Py-Fundamentals/37-unit-tests.py
+++ Python/Py-Fundamentals/37-unit-tests.py
@@ -13,3 +13,27 @@ we provide) to print something like:
 
 Armor: 2
 Health: 18
+
+# If your code prints that exact output, you pass. If it doesn't, you fail.
+
+
+# ------------------------------- #
+# -- A new type of Lesson --
+# ------------------------------- #
+
+"""
+Going forward, you'll encounter a new type of lesson: unit tests. A unit test is just an automated 
+program that tests a small "unit" of code. Usually just a function or two.
+
+These new unit-test-style lessons will test your code's functionality rather than its output. Our tests 
+will call functions in your code with different arguments, and expect certain return values. If your 
+code returns the correct values, you pass. If it doesn't, you fail.
+
+There are two reasons for this change:
+
+
+-- It's more realistic. In the real world, you'll be writing unit tests and running them against your code to make sure it works as expected.
+
+-- You can run and debug your code with print statements, and leave those print statements in when you submit. Unlike the output-based lessons, you won't have to remove your print statements to pass.
+
+"""
