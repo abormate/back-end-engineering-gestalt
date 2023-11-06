ee73835e18aaf00ab7d5c336bb07564290c1356d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-06 12:05:24 +0800

    Python StudySpace -- Unit Tests

--- Python/Py-Fundamentals/37-unit-tests.py
+++ Python/Py-Fundamentals/37-unit-tests.py
@@ -4,3 +4,12 @@
 #
 # ---------------------------------------------- #
 
+"""
+Up until this point, all the coding lessons you've completed have been testing you based on console 
+output (what's printed). For example, a lesson might expect your code (in conjunction with the code 
+we provide) to print something like:
+
+"""
+
+Armor: 2
+Health: 18
