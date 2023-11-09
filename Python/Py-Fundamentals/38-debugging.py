b26d7272ceca03671707c2f8f904e4f9fc31dc7e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-09 09:17:44 +0800

    Python StudySpace

--- Python/Py-Fundamentals/38-debugging.py
+++ Python/Py-Fundamentals/38-debugging.py
@@ -4,3 +4,12 @@
 #
 # --------------------------------------------- #
 
+"""
+When you're working as a professional developer, you'll typically write code on your computer and test 
+it by yourself before it's deployed to users.
+
+That first part of the process is called debugging. You write some code, run it, and if it doesn't work,
+ you fix the bugs. You repeat this process until you're confident that your code works as expected.
+
+"""
+
