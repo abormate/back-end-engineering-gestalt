649a189f5f803cea770f67c206ba71b6b1f04bb3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-10 16:44:53 +0800

    Python StudySpace

--- Python/Py-Fundamentals/39-learn-effective.py
+++ Python/Py-Fundamentals/39-learn-effective.py
@@ -4,3 +4,11 @@
 #
 # ---------------------------------------------- #
 
+"""
+This course is about to get a bit harder. There's no way around it, if programming 
+were a walk in the park everyone would be earning 6 figures as a software engineer. 
+
+But it's not, and to succeed without getting stuck and frustrated you need to learn 
+how to learn.
+
+"""
