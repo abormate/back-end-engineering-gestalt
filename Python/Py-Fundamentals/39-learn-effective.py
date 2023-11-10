6334687a0a1d3cc8272b997b0e761fa9b5db70d6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-10 16:48:14 +0800

    Python StudySpace -- Learn How to Learn

--- Python/Py-Fundamentals/39-learn-effective.py
+++ Python/Py-Fundamentals/39-learn-effective.py
@@ -17,3 +17,26 @@ how to learn.
 # -- Process for Solving Hard Coding Problems
 # -------------------------------- #
 
+"""
+
+1. Read the lesson first! Figure out the examples before writing your own code.
+
+2. Read the assignment. Understand the goal of the assignment before you start 
+writing code. Start writing code.
+
+3. Add print() statements. Don't wait until you've written a lot of code to start 
+testing. Add print() statements and use the Run button to see if your code is 
+doing what you expect at each step. It's easier to find issues in small bits of 
+code than in large blocks of code.
+
+Keep running, printing, and fixing until you're confident your code is working.
+
+4. Submit your code. If the assignment you're working on has unit tests, no need 
+to remove your debugging print() statements. If the assignment you're working on 
+is testing console output, be sure to remove your print() statements before 
+submitting.
+
+5. Compare your code to the instructor's. You will not be penalized for looking 
+at the solution after you have successfully completed the assignment.
+
+"""
