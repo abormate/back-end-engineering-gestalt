308273375e69245def43e7a871d0b08ca8facd37
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-11 14:18:00 +0800

    Python StudySpace -- Arithmetic -- Floor Division

---
+++ Python/Py-Fundamentals/41-floor-division.py
@@ -0,0 +1,21 @@
+# ------------------------------------------ #
+#
+# -- Floored Division -- Python StudySpace --
+#
+# ------------------------------------------ #
+
+"""
+Python has great out-of-the-box support for mathematical operations. This, among 
+other reasons, is why it has had such success in artificial intelligence, machine 
+learning, and data science applications.
+
+Floor division is like normal division except the result is floored afterward, 
+which means the remainder is removed. As you would expect, this means the result 
+is an integer instead of a float.
+
+"""
+
+7 // 3
+# 2 (an integer)
+
+
