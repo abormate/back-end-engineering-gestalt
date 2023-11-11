b6a1e9af953beee7fd492b559378ab43dd14db2e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-11 13:54:00 +0800

    Python StudySpace --

--- Python/Py-Fundamentals/40-python-numbers.py
+++ Python/Py-Fundamentals/40-python-numbers.py
@@ -4,3 +4,20 @@
 #
 # -------------------------------------- #
 
+"""
+In Python, numbers without a decimal part are called Integers - just like they are in mathematics.
+
+Integers are simply whole numbers, positive or negative. For example, 3 and -3 are both examples of integers.
+
+Arithmetic can be performed as you might expect:
+
+"""
+
+# Addition
+2 + 1
+# 3
+
+# Subtraction
+2 - 1
+# 1
+
