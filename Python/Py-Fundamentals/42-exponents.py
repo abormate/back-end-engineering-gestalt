fd268d09df88b59999125de0b2a0beb3aa1c1873
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-12 08:55:57 +0800

    Python StudySpace -- Exponents

--- Python/Py-Fundamentals/42-exponents.py
+++ Python/Py-Fundamentals/42-exponents.py
@@ -8,3 +8,19 @@
 Python has built-in support for exponents - something most languages require a math library for.
 
 """
+
+# reads as "three squared" or
+# "three raised to the second power"
+3 ** 2
+# 9
+
+# ------------------------ #
+# -- Assignment -- Practice 
+# ------------------------ #
+
+"""
+What is 2 ** 3?
+
+"""
+
+# 8
