b98b0c7cc594b80d08ed3cc76ff90065c80bef55
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-13 12:24:33 +0800

    Python StudySpace -- Variable Updates

--- Python/Py-Fundamentals/43-change-in-place.py
+++ Python/Py-Fundamentals/43-change-in-place.py
@@ -13,3 +13,12 @@ player_score = player_score + 1
 player_score = 4
 player_score = player_score - 1
 # player_score now equals 3
+
+"""
+Don't let the fact that the expression player_score = player_score - 1 is not a 
+valid mathematical expression be confusing. It doesn't matter, it is valid code. 
+
+It's valid because the way the expression should be read in English is:
+
+"""
+
