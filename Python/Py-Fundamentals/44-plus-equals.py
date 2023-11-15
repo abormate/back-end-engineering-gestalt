2fdd292be1b7606adca7e3181f36546a97cc27ed
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-15 14:00:08 +0800

    Python StudySpace

---
+++ Python/Py-Fundamentals/44-plus-equals.py
@@ -0,0 +1,36 @@
+# ----------------------------------- #
+#
+# -- Plus Equals -- Python StudySpace --
+#
+# ----------------------------------- #
+
+"""
+Python makes reassignment easy when doing math. In JavaScript or Go you might be 
+familiar with the ++ syntax for incrementing a number variable. In Python, we use 
+the += operator instead.
+
+"""
+
+star_rating = 4
+star_rating += 1
+# star_rating is now 5
+
+
+# -------------------------- #
+# -- Other Operators --
+# -------------------------- #
+
+# The other operators work similarly:
+
+star_rating = 4
+star_rating -= 1
+# star_rating is now 3
+
+star_rating = 4
+star_rating *= 2
+# star_rating is now 8
+
+star_rating = 4
+star_rating /= 2
+# star_rating is now 2
+
