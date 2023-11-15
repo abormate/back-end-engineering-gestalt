74676fadadbce62bb56d76d995d910d3d5d03f9c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-15 14:02:48 +0800

    Python StudySpace

--- Python/Py-Fundamentals/45-plus-equals.py
+++ Python/Py-Fundamentals/45-plus-equals.py
@@ -4,3 +4,10 @@
 #
 # --------------------------------------- #
 
+"""
+Python makes reassignment easy when doing math. In JavaScript or Go you might be 
+familiar with the ++ syntax for incrementing a number variable. In Python, we use 
+the += operator instead.
+
+"""
+
