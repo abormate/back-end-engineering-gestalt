820638e7c8264e4ebf3c6ff0ac3d4fcdda83f945
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-16 11:58:25 +0800

    Python StudySpace

--- Python/Py-Fundamentals/46-scientific-notation.py
+++ Python/Py-Fundamentals/46-scientific-notation.py
@@ -4,3 +4,12 @@
 #
 # -------------------------------------------- #
 
+"""
+As we covered earlier, a float is a positive or negative number with a fractional 
+part.
+
+You can add the letter e or E followed by a positive or negative integer to 
+specify that you're using scientific notation.
+
+"""
+
