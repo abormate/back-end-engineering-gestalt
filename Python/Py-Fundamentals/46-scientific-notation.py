ff872c8909389d0a443e145f7c14b72bb9214b5f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-16 12:09:03 +0800

    Python StudySpace -- Scientific Notation

--- Python/Py-Fundamentals/46-scientific-notation.py
+++ Python/Py-Fundamentals/46-scientific-notation.py
@@ -59,7 +59,62 @@ Complete the max_players_on_server function. It should return 3 values:
 The max players on a "small" server: 1,024,000,000,000,000,000 (1.024e18)
 The max players on a "medium" server: 10,240,000,000,000,000,000
 The max players on a "large" server: 102,400,000,000,000,000,000
+
+
 Use scientific notation to represent these numbers.
 
 """
 
+# ---------------------------- #
+# Buggy Code -- Not Done
+# ---------------------------- #
+
+def max_players_on_server():
+    pass
+
+# ---------------------------- #
+# Unit Tester
+# ---------------------------- #
+
+from main import *
+
+run_cases = [
+    (1.024e18, 1.024e19, 1.024e20),
+]
+
+submit_cases = run_cases
+
+
+def test(expected1, expected2, expected3):
+    print("---------------------------------")
+    print(f"Expecting: {(expected1, expected2, expected3)}")
+    result = max_players_on_server()
+    print(f"Actual: {result}")
+    if result == (expected1, expected2, expected3):
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in run_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
