418a2014a6219f0cb9b198934b36bfefa814f8dc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-17 08:08:55 +0800

    Python StudySpace -- Logic

--- Python/Py-Fundamentals/47-logical-operators.py
+++ Python/Py-Fundamentals/47-logical-operators.py
@@ -4,3 +4,16 @@
 #
 # --------------------------------------------- #
 
+"""
+You're probably familiar with the logical operators AND and OR.
+
+Logical operators deal with boolean values, True and False.
+
+The logical AND operator requires that both inputs are True to return True. 
+The logical OR operator only requires that at least one input is True to return 
+True.
+
+For example:
+
+"""
+
