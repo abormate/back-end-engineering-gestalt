f542e499d22e564c487764b31b70ec8f2d9359dc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-18 10:55:53 +0800

    Python StudySpace -- Bitwise operations

---
+++ Python/Py-Fundamentals/49-bitwise-ampersand-operator.py
@@ -0,0 +1,20 @@
+# ---------------------------------------- #
+#
+# -- Bitwise and Ampersand (&) --
+#
+# ---------------------------------------- #
+
+"""
+Bitwise operators are similar to logical operators, but instead of operating on 
+boolean values, they apply the same logic to all the bits in a value. For example, 
+say you had the numbers 5 and 7 represented in binary. You could perform a 
+bitwise AND operation and the result would be 5.
+
+
+0101 = 5
+&
+0111 = 7
+=
+0101 = 5
+
+"""
