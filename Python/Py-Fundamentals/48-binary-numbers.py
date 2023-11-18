6135eaf01d0cea5bda809fcf50159af074e96e84
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-18 10:36:03 +0800

    Python StudySpace

--- Python/Py-Fundamentals/48-binary-numbers.py
+++ Python/Py-Fundamentals/48-binary-numbers.py
@@ -4,3 +4,10 @@
 #
 # --------------------------------------- #
 
+"""
+Binary numbers are just "base 2" numbers. They work the same way as "normal" 
+base 10 numbers, but with 2 symbols instead of 10.
+
+
+
+"""
