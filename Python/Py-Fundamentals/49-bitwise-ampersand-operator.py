2757f77477b5d600b8ea7ecfe8c12dbce47d0f15
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-18 10:57:04 +0800

    Python StudySpace -- Logical Operations (Boolean) from Bitwise

--- Python/Py-Fundamentals/49-bitwise-ampersand-operator.py
+++ Python/Py-Fundamentals/49-bitwise-ampersand-operator.py
@@ -10,11 +10,26 @@ boolean values, they apply the same logic to all the bits in a value. For exampl
 say you had the numbers 5 and 7 represented in binary. You could perform a 
 bitwise AND operation and the result would be 5.
 
-
+Example --
 0101 = 5
 &
 0111 = 7
 =
 0101 = 5
+
+
+A 1 in binary is the same as True, while 0 is False. So really a bitwise operation 
+is just a bunch of logical operations that are completed in tandem.
+
+& is the bitwise AND operator in Python. 5 & 7 = 5, while 5 & 2 = 0.
+
+
+Example
+
+0101 = 5
+&
+0010 = 2
+=
+0000 = 0
 
 """
