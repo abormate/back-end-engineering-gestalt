2583f6553cd3c50829a9f19ce6445ee8d62f4300
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-25 06:35:30 +0800

    Python StudySpace

--- Python/Py-Fundamentals/52-number-syntax.py
+++ Python/Py-Fundamentals/52-number-syntax.py
@@ -21,3 +21,28 @@ The numbers should be:
 10 million and 4.5 million
 
 """
+
+# -------------------------- #
+# -- Buggy Code --
+# -------------------------- #
+
+def main():
+    test(8,000,000, 3,000,000)
+    test(10,000,000, 4,500,000)
+
+
+# Don't edit below this line
+
+
+def calculate_male_ratio(num_fruit_bats, num_male_bats):
+    return num_male_bats / num_fruit_bats
+
+
+def test(num_fruit_bats, num_male_bats):
+    ratio = calculate_male_ratio(num_fruit_bats, num_male_bats)
+    print(f"{ratio * 100}% of fruit bats are male")
+    print("=====================================")
+
+
+main()
+
