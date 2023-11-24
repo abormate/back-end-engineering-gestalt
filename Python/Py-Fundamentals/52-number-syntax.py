b77fc7d0a8036f389388a68d55a85348d9b96a39
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-25 06:34:32 +0800

    Python StudySpace

--- Python/Py-Fundamentals/52-number-syntax.py
+++ Python/Py-Fundamentals/52-number-syntax.py
@@ -4,3 +4,20 @@
 #
 # ------------------------------------- #
 
+"""
+Challenge Exercise
+
+You've been working with a biologist who is researching the mating behaviors of 
+fruit bats. They've added some calls to the main function that contain invalid 
+syntax.
+
+Fix the biologist's syntax error. Use the proper delimiter so that the numbers 
+are still easy to parse visually.
+
+
+The numbers should be:
+
+8 million and 3 million
+10 million and 4.5 million
+
+"""
