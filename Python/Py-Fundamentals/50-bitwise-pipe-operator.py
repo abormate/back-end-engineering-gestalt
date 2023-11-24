9b65a4c71e2b45b4300ea475453da54c70f5f557
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-24 18:14:58 +0800

    Python StudySpace

--- Python/Py-Fundamentals/50-bitwise-pipe-operator.py
+++ Python/Py-Fundamentals/50-bitwise-pipe-operator.py
@@ -9,5 +9,8 @@ As you may have guessed, the bitwise "or" operator is similar to the bitwise "an
 operator in that it works on binary rather than boolean values. However, the 
 bitwise "or" operator "ORs" the bits together. Here's an example:
 
+0101 is 5
+0111 is 7
+
 """
 
