a75a77d0338abca8961b8e289fc2fc71c6057f7a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-25 06:40:08 +0800

    Python StudySpace -- Number Syntax

--- Python/Py-Fundamentals/52-number-syntax.py
+++ Python/Py-Fundamentals/52-number-syntax.py
@@ -46,3 +46,27 @@ def test(num_fruit_bats, num_male_bats):
 
 main()
 
+# -------------------------- #
+# -- Repaired Code --
+# -------------------------- #
+
+def main():
+    test(8_000_000, 3_000_000)
+    test(10_000_000, 4_500_000)
+
+
+# Don't edit below this line
+
+
+def calculate_male_ratio(num_fruit_bats, num_male_bats):
+    return num_male_bats / num_fruit_bats
+
+
+def test(num_fruit_bats, num_male_bats):
+    ratio = calculate_male_ratio(num_fruit_bats, num_male_bats)
+    print(f"{ratio * 100}% of fruit bats are male")
+    print("=====================================")
+
+
+main()
+
