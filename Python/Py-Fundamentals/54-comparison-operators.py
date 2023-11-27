cfa743cb10e125e51bee98ece6570e813d67a184
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-27 16:19:54 +0800

    Python StudySpace -- Comparison

--- Python/Py-Fundamentals/54-comparison-operators.py
+++ Python/Py-Fundamentals/54-comparison-operators.py
@@ -15,4 +15,19 @@ name for these kinds of comparison operations that always result in True or Fals
 == "equal to"
 != "not equal to"
 
+5 < 7 # evaluates to True
+5 > 7 # evaluates to False
+5 >= 7 # evaluates to False
+5 <= 7 # evaluates to True
+5 == 7 # evaluates to False
+5 != 7 # evaluates to True
+
 """
+
+# -------------------------- #
+# -- Assignment -- Practice
+# -------------------------- #
+
+def player_1_wins(player_1_score, player_2_score):
+    pass
+
