7f1cf338d89c545bb6af60e819e615e6dc0d361f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-27 15:46:13 +0800

    Python StudySpace

--- Python/Py-Fundamentals/53-turning-decimal-to-binary.py
+++ Python/Py-Fundamentals/53-turning-decimal-to-binary.py
@@ -14,3 +14,11 @@ has built-in functionality to parse strings of 1's and 0's as binary numbers.
 # -- Challenge --
 # ---------------------- #
 
+"""
+Complete the body_parts function. It takes binary strings as input and returns 
+them as integers. Each integer is the numerical value of the string when 
+interpreted as binary.
+
+For example:
+
+"""
