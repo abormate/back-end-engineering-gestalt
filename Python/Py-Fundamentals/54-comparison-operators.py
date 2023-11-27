60db241b1d880510f41bac07c9fa6bdab75eeb84
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-27 16:17:54 +0800

    Python StudySpace

--- Python/Py-Fundamentals/54-comparison-operators.py
+++ Python/Py-Fundamentals/54-comparison-operators.py
@@ -8,4 +8,11 @@
 When coding it's necessary to be able to compare two values. Boolean logic is the 
 name for these kinds of comparison operations that always result in True or False.
 
+< "less than"
+> "greater than"
+<= "less than or equal to"
+>= "greater than or equal to"
+== "equal to"
+!= "not equal to"
+
 """
