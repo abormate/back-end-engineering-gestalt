adf65c523e28751d28682be034876d641ab049f7
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-27 15:43:06 +0800

    Python StudySpace

--- Python/Py-Fundamentals/53-turning-decimal-to-binary.py
+++ Python/Py-Fundamentals/53-turning-decimal-to-binary.py
@@ -4,3 +4,8 @@
 #
 # ------------------------------------- #
 
+"""
+You've been assigned to write some tests for the Python interpreter itself! Python 
+has built-in functionality to parse strings of 1's and 0's as binary numbers.
+
+"""
