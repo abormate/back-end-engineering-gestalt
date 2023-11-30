05678b9e12e3e3d78d818a86e6eb5e30afb444f0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-30 19:46:30 +0800

    Python StudySpace -- Comparison Practice

--- Python/Py-Fundamentals/56-comparison-practice.py
+++ Python/Py-Fundamentals/56-comparison-practice.py
@@ -87,3 +87,8 @@ main()
 # -- Repaired Code --
 # --------------------------------- #
 
+def can_withstand_blow(hero_armor, enemy_damage):
+    if hero_armor >= enemy_damage:
+        return True
+    else:
+        return False
