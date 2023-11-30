6e5bbb780b16de28dabac0a54e63eb0fda139f11
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-30 20:32:23 +0800

    Python StudySpace

--- Python/Py-Fundamentals/57-if-statements.py
+++ Python/Py-Fundamentals/57-if-statements.py
@@ -6,7 +6,17 @@
 
 # It's often useful to only execute code if a certain condition is met:
 
+"""
 if CONDITION:
 	# do some stuff here
 
 # code after the if block will run regardless
+
+# For example, in this code
+
+if bob_score > bill_score:
+	print("Bob Wins!")
+
+print("Game Complete")
+
+"""
