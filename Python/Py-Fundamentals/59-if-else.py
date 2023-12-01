b56da518e38818bc9d5ac14d793332411de56803
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-02 07:43:33 +0800

    Python StudySpace

--- Python/Py-Fundamentals/59-if-else.py
+++ Python/Py-Fundamentals/59-if-else.py
@@ -37,3 +37,20 @@ the only body executed.
 # -- Assignment -- Practice --
 # -------------------------- #
 
+"""
+Complete the player_status function. If the player's health less than or equal to 0, 
+return the string:
+
+dead
+
+Otherwise, if it's less than or equal to 5 return the string:
+
+injured
+
+Otherwise, return the string:
+
+healthy
+
+
+
+"""
