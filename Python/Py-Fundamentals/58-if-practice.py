f3be0934e0581c16082114ef2a7c71093495c6ce
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-01 09:41:43 +0800

    Python StudySpace

--- Python/Py-Fundamentals/58-if-practice.py
+++ Python/Py-Fundamentals/58-if-practice.py
@@ -4,3 +4,20 @@
 #
 # -------------------------------------- #
 
+# Remember, you can use the == operator to check if two values are equal. 
+# For example:
+
+is_equal = 5 == 5
+# is_equal is True
+
+# ----------------------------- #
+# -- Assignment -- Practice
+# ----------------------------- #
+
+"""
+Complete the check_swords_for_army function. If the number of swords and the 
+number of soldiers match, return the string:
+
+"""
+
+# correct amount
