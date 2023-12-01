bd7800d6d43784bcc6e76f3cecc811bd44dac496
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-02 07:36:16 +0800

    Python StudySpace

--- Python/Py-Fundamentals/59-if-else.py
+++ Python/Py-Fundamentals/59-if-else.py
@@ -6,7 +6,18 @@
 
 """
 An if statement can be followed by zero or more elif (which stands for "else if") 
-statements, which can be followed by zero or one else statement. For example:
+statements, which can be followed by zero or one else statement.
+
+For example:
+
+if score > high_score:
+    print('High score beat!')
+elif score > second_highest_score:
+    print('You got second place!')
+elif score > third_highest_score:
+    print('You got third place!')
+else:
+    print('Better luck next time')
+
 
 """
-
