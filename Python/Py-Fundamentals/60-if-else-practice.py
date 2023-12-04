13ba670043957211fb84a136d20ba810d5ff485d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-04 11:50:34 +0800

    Python StudySpace -- If Else Practice

--- Python/Py-Fundamentals/60-if-else-practice.py
+++ Python/Py-Fundamentals/60-if-else-practice.py
@@ -97,3 +97,12 @@ if "__RUN__" in globals():
 
 main()
 
+# ------------------------------- #
+# -- Repaired Code --
+# ------------------------------- #
+
+def check_high_score(current_player_name, high_scoring_player_name):
+    if current_player_name == high_scoring_player_name:
+        return "You are the highest scoring player!"
+    else:
+        return "You are not the highest scoring player!"
