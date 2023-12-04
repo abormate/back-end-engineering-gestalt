3e9ff1ab4b42ece586f07c8045631d747584c627
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-04 11:43:32 +0800

    Python StudySpace

--- Python/Py-Fundamentals/60-if-else-practice.py
+++ Python/Py-Fundamentals/60-if-else-practice.py
@@ -4,3 +4,13 @@
 #
 # ------------------------------------------ #
 
+"""
+Here are some basic rules with if/else blocks.
+
+You can't have an elif or an else without an if
+You can have an else without an elif
+
+Remember, to check if two values are the same use the == operator.
+
+"""
+
