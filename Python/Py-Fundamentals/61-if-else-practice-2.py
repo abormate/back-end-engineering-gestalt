fb6584f112f865442b0535c0e8d74b507f502993
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-07 13:01:02 +0800

    Python StudySpace

--- Python/Py-Fundamentals/61-if-else-practice-2.py
+++ Python/Py-Fundamentals/61-if-else-practice-2.py
@@ -7,7 +7,7 @@
 """
 Here are some basic rules with if/else blocks.
 
-You can't have an elif or an else without an if
-You can have an else without an elif
+-- You can't have an elif or an else without an if
+-- You can have an else without an elif
 
 """
