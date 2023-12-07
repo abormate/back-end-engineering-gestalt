3d7f176550c0f1306623fdff14c1bfe333d1a751
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-07 13:09:13 +0800

    Python StudySpace

--- Python/Py-Fundamentals/61-if-else-practice-2.py
+++ Python/Py-Fundamentals/61-if-else-practice-2.py
@@ -37,3 +37,14 @@ Otherwise, return the string:
 # -- Buggy Code --
 # --------------------------- #
 
+# --------------------------- #
+# -- Repaired Code --
+# --------------------------- #
+
+def check_high_score(player_name, high_scoring_player_name, low_scoring_player_name):
+    if player_name == high_scoring_player_name:
+        return "high"
+    elif player_name == low_scoring_player_name:
+        return "low"
+    else:
+        return "neither"
