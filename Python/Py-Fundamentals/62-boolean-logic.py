e55fef2bd9ac684b0c9a2a47945c4ae1592aba37
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-09 07:56:10 +0800

    Python StudySpace

--- Python/Py-Fundamentals/62-boolean-logic.py
+++ Python/Py-Fundamentals/62-boolean-logic.py
@@ -4,3 +4,12 @@
 #
 # ------------------------------------- #
 
+"""
+Boolean logic refers to logic dealing with boolean (True or False) values. For 
+example,
+
+Dogs must have four legs and weigh less than 100 kilograms. (Both conditions must 
+be true)
+
+"""
+
