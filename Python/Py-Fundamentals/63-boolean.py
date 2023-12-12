c8c9893f8f1cbeb5684f19e35629232524b96bed
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-12 11:04:04 +0800

    Python StudySpace

--- Python/Py-Fundamentals/63-boolean.py
+++ Python/Py-Fundamentals/63-boolean.py
@@ -4,3 +4,12 @@
 #
 # ------------------------------------- #
 
+"""
+In Fantasy Quest, players can go to a town's local pub. Drinking virtual beer 
+refills their stamina!
+
+Complete the function that determines if a bartender should serve drinks to a 
+customer. The function should return True only if all the following conditions 
+are met:
+
+"""
