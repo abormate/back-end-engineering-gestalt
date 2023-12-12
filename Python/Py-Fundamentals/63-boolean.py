a61de54f7ed3871092870cb0b8d30e3efa9fb7f4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-12 11:04:34 +0800

    Python StudySpace

--- Python/Py-Fundamentals/63-boolean.py
+++ Python/Py-Fundamentals/63-boolean.py
@@ -12,4 +12,11 @@ Complete the function that determines if a bartender should serve drinks to a
 customer. The function should return True only if all the following conditions 
 are met:
 
+The time is between 5 and 10 both inclusive
+The bartender is not on break
+The customer's age is 21 or older.
+
+
+If any of these conditions are not met the function should return False.
+
 """
