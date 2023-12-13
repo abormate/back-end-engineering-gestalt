f46834ead013ba144aabcf2d9a35da31a8270353
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-13 16:02:30 +0800

    Python StudySpace -- Parking Meter Practice

--- Python/Py-Fundamentals/64-practice-parking-meter.py
+++ Python/Py-Fundamentals/64-practice-parking-meter.py
@@ -27,3 +27,8 @@ If time_parked meets or exceeds time_purchased return the string "overtime charg
 Otherwise, return the string "no charges yet"
 
 """
+
+def check_parking_meter(time_parked, time_purchased):
+    if time_parked >= time_purchased:
+        return "overtime charged"
+    return "no charges yet"
