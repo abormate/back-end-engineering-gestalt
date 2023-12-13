6ca37cdbec6f491edcaad2bc5c66f0607cfc11be
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-13 15:41:03 +0800

    Python StudySpace

--- Python/Py-Fundamentals/64-practice-parking-meter.py
+++ Python/Py-Fundamentals/64-practice-parking-meter.py
@@ -4,3 +4,9 @@
 #
 # --------------------------------------- #
 
+"""
+A parking enforcement officer was assigned to check on some faulty parking meters 
+on Main Street. He determined that the meters were giving tickets to cars whose 
+parking duration had not yet expired.
+
+"""
