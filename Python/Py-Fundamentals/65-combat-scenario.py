ed1cfd8504be29e69fe95db6012eba571687c4dc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-13 16:07:53 +0800

    Python StudySpace

--- Python/Py-Fundamentals/65-combat-scenario.py
+++ Python/Py-Fundamentals/65-combat-scenario.py
@@ -4,3 +4,15 @@
 #
 # ---------------------------------------- #
 
+"""
+A new text-based RPG you are building doesn't have any way for players to know if 
+they are strong enough to fight certain enemies.
+
+If a player's power level is greater than the enemies' defense that player has an 
+advantage
+
+If the player's power and enemies' defense are equal they are evenly matched
+Otherwise, that player has a disadvantage.
+
+"""
+
