1e4100f29c2a5443f144b11ac842c970d31432a7
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-13 15:50:26 +0800

    Python StudySpace

--- Python/Py-Fundamentals/64-practice-parking-meter.py
+++ Python/Py-Fundamentals/64-practice-parking-meter.py
@@ -14,3 +14,10 @@ parking duration had not yet expired.
 # --------------------------- #
 # -- Challenge Practice --
 # --------------------------- #
+
+"""
+The parking agency has tasked you with fixing the code for the meters.
+
+Create a check_parking_meter function. It takes two inputs:
+
+"""
