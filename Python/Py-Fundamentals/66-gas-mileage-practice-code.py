11e9469c4ac0eef60ce8bcb2e4ac6e3d2149d223
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-14 10:59:42 +0800

    Python StudySpace -- Gasoline Mileage Practice

--- Python/Py-Fundamentals/66-gas-mileage-practice-code.py
+++ Python/Py-Fundamentals/66-gas-mileage-practice-code.py
@@ -10,3 +10,19 @@ figure out if his car has enough gas to get him to work and back home.
 
 """
 
+# ----------------------- #
+# -- Practice --
+# ----------------------- #
+
+"""
+Complete the has_enough_gas function.
+
+Do some Pythonic math to determine how many gallons are needed for Tyler to get to 
+work AND make it back home after he gets off work. Assign the result to a 
+gallons_needed variable.
+
+Return True if there are at least enough gallons in the tank based on the 
+gallons_needed variable, and False otherwise.
+
+"""
+
