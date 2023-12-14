7a81e22628bbe0802e19f1cbca3f98eb915e33b5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-14 10:57:51 +0800

    Python StudySpace

--- Python/Py-Fundamentals/66-gas-mileage-practice-code.py
+++ Python/Py-Fundamentals/66-gas-mileage-practice-code.py
@@ -4,3 +4,9 @@
 #
 # ------------------------------------------- #
 
+"""
+There isn't a gas station between Tyler's house and his work. He is trying to 
+figure out if his car has enough gas to get him to work and back home.
+
+"""
+
