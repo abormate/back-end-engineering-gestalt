f34952d1725a044af0447e9b8901bd2640fa2075
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-14 11:09:19 +0800

    Python StudySpace -- Loops

---
+++ Python/Py-Fundamentals/67-loops.py
@@ -0,0 +1,11 @@
+# -------------------------------------- #
+#
+# -- Loops -- Python StudySpace --
+# 
+# -------------------------------------- #
+
+"""
+Loops are a programmer's best friend. Loops allow us to do the same operation 
+multiple times without having to write it explicitly each time.
+
+"""
