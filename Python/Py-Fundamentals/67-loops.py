e91cbb347153f199bdd14278e50bc213c6e17025
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-15 12:02:28 +0800

    Python StudySpace

--- Python/Py-Fundamentals/67-loops.py
+++ Python/Py-Fundamentals/67-loops.py
@@ -33,3 +33,15 @@ A "for loop" in Python is written like this:
 
 for i in range(0, 10):
     print(i)
+
+"""
+In English, the code says:
+
+1.) Start with i equals 0. (i in range(0)
+2.) If i is not less than 10 (range(0, 10)), exit the loop.
+3.) Print i to the console. (print(i))
+4.) Add 1 to i. (range defaults to incrementing by 1)
+5.) Go back to step 2
+
+"""
+
