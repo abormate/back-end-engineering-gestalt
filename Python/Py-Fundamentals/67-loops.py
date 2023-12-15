ecb59840a6cd51c58584d37eccca3aea201640c3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-15 11:58:56 +0800

    Python StudySpace

--- Python/Py-Fundamentals/67-loops.py
+++ Python/Py-Fundamentals/67-loops.py
@@ -9,3 +9,14 @@ Loops are a programmer's best friend. Loops allow us to do the same operation
 multiple times without having to write it explicitly each time.
 
 """
+
+print(0)
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
