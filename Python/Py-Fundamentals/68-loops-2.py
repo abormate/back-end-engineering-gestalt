59734bb9225a1aae78d682d997a56296281e25bc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-16 15:32:54 +0800

    Python StudySpace -- Loops Practice

--- Python/Py-Fundamentals/68-loops-2.py
+++ Python/Py-Fundamentals/68-loops-2.py
@@ -59,3 +59,26 @@ def main():
 
 main()
 
+# --------------------------- #
+# -- Repaired Code --
+# --------------------------- #
+
+def print_numbers():
+    for i in range(0, 200):
+        print(i)
+
+
+# Don't edit below this line
+
+
+def test():
+    print("Printing numbers from 0 to 199:")
+    print_numbers()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
