d852c8993e1a76de2529374572dc8c954a050268
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-16 15:30:49 +0800

    Python StudySpace

--- Python/Py-Fundamentals/68-loops-2.py
+++ Python/Py-Fundamentals/68-loops-2.py
@@ -36,3 +36,26 @@ numbers 0-199 to the console.
 
 """
 
+# ------------------------- #
+# -- Buggy Code --
+# ------------------------- #
+
+def print_numbers():
+    # ?
+
+
+# Don't edit below this line
+
+
+def test():
+    print("Printing numbers from 0 to 199:")
+    print_numbers()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
+
