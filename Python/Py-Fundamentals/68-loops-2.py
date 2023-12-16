7acc5250b83f04a6f1dfe5e4f01e351618c9aba8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-16 15:29:16 +0800

    Python StudySpace

--- Python/Py-Fundamentals/68-loops-2.py
+++ Python/Py-Fundamentals/68-loops-2.py
@@ -9,4 +9,19 @@
 for i in range(0, 10):
     print(i)
 
-    
+"""
+In English, the code says:
+
+Start with i equals 0. (i in range(0)
+If i is not less than 10 (range(0, 10)), exit the loop.
+Print i to the console. (print(i))
+Add 1 to i. (range defaults to incrementing by 1)
+Go back to step 2
+
+
+The result is that the numbers 0-9 are logged to the console in order.
+
+WHITESPACE MATTERS IN PYTHON!
+The body of a for-loop must be indented; otherwise, you'll get a syntax error.
+
+"""
