b565c597fb3362fd6c9f039b948ada2b20359aa1
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-18 11:34:28 +0800

    Python StudySpace -- Loops Practice

--- Python/Py-Fundamentals/69-loops-practice.py
+++ Python/Py-Fundamentals/69-loops-practice.py
@@ -9,3 +9,27 @@
 for i in range(0, 10):
     print(i)
 
+"""
+In English, the code says:
+
+1. Start with i equals 0. (i in range(0)
+2. If i is greater than or equal to 10 (range(0, 10)), exit the loop.
+3. Print i to the console. (print(i))
+4. Add 1 to i. (range defaults to incrementing by 1)
+5. Go back to step 2
+
+
+The result is that the numbers 0-9 are logged to the console in order.
+
+"""
+
+# ----------------------------- #
+# -- Assignment -- Practice --
+# ----------------------------- #
+
+"""
+In the print_numbers_from_five_to function, edit the for-loop to print every 
+number from 5 up to but not including the given end number.
+
+"""
+
