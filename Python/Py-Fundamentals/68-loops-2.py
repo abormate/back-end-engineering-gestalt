5eb5c6e2abad926a119f103e6c6073912b8281cd
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-18 11:29:34 +0800

    Python StudySpace -- Loops

--- Python/Py-Fundamentals/68-loops-2.py
+++ Python/Py-Fundamentals/68-loops-2.py
@@ -47,7 +47,7 @@ def print_numbers():
 # Don't edit below this line
 
 
-def test():
+# def test():
     print("Printing numbers from 0 to 199:")
     print_numbers()
     print("=====================================")
