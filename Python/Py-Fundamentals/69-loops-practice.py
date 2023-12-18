5bf88d3ecbfffec51ef89f1957d4ce807b5db7d2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-18 11:31:27 +0800

    Python StudySpace -- Loops Practice

---
+++ Python/Py-Fundamentals/69-loops-practice.py
@@ -0,0 +1,11 @@
+# ----------------------------------- #
+#
+# -- Loops Practice -- Python StudySpace --
+#
+# ----------------------------------- #
+
+# As a reminder, a "for loop" in Python is written like this:
+
+for i in range(0, 10):
+    print(i)
+
