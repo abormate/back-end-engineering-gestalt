e57cf12ad8a2599dfc9fe575ccb4036e0cb39591
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-21 21:35:07 +0800

    Python StudySpace

--- Python/Py-Fundamentals/71-f-strings.py
+++ Python/Py-Fundamentals/71-f-strings.py
@@ -4,3 +4,9 @@
 #
 # ------------------------------------ #
 
+"""
+You can create a string with dynamic values by using f-strings in Python. It's a 
+beautiful syntax that I wish more programming languages used.
+
+"""
+
