dcc6ddd29c1152dc0520f17e846d6371c77f4167
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-21 21:36:27 +0800

    Python StudySpace

--- Python/Py-Fundamentals/71-f-strings.py
+++ Python/Py-Fundamentals/71-f-strings.py
@@ -13,3 +13,9 @@ beautiful syntax that I wish more programming languages used.
 num_bananas = 10
 print(f"You have {num_bananas} bananas")
 # You have 10 bananas
+
+"""
+The opening quotes need to be preceded by an f, then any variables within curly 
+brackets have their values "interpolated" (injected) into the string.
+
+"""
