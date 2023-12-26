8357bbb8ccb8c045df90055a1c3d9dbc50b87a1f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-26 12:15:59 +0800

    Python StudySpace -- Sum Practice

--- Python/Py-Fundamentals/72-sum-game.py
+++ Python/Py-Fundamentals/72-sum-game.py
@@ -23,3 +23,17 @@ number_of_apples -= 2
 # -- Assignment -- Practice --
 # ----------------------------- #
 
+"""
+Fix the bug in the sum_of_numbers function. Instead of adding 1 to total at 
+each iteration of the loop, it should add i. For example, instead of:
+
+1 + 1 + 1 + 1 + 1...
+
+we want:
+
+0 + 1 + 2 + 3 + 4...
+
+The desired output is a single number after the loop has finished executing.
+
+"""
+
