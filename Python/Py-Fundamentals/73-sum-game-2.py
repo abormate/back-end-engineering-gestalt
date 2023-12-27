9c14b83c96f4a481d973b608603de36aaa348847
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-27 18:32:44 +0800

    Python StudySpace

--- Python/Py-Fundamentals/73-sum-game-2.py
+++ Python/Py-Fundamentals/73-sum-game-2.py
@@ -11,8 +11,13 @@ return the result.
 
 """
 
+# ---------------------------- #
+# -- Erroneous Code -- Buggy --
+# ---------------------------- #
+
 def sum_of_odd_numbers(end):
     total = 0
     for i in range(0, end):
         total += i
     return total
+
