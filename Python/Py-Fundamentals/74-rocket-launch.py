2de135a51cbed2eff0916beb078c30aeef565f63
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-27 18:44:17 +0800

    Python StudySpace

--- Python/Py-Fundamentals/74-rocket-launch.py
+++ Python/Py-Fundamentals/74-rocket-launch.py
@@ -4,3 +4,9 @@
 #
 # ------------------------------------- #
 
+"""
+A team of scientists is getting ready to launch the rockets they have been 
+working on. You've been asked to write a program that will countdown to the 
+rockets' launch.
+
+"""
