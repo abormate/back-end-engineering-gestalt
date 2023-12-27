12f783fe6b2738c0ad822b7dafea3d3a48d50656
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-27 18:56:43 +0800

    Python StudySpace -- Rocket Launch Practice

--- Python/Py-Fundamentals/74-rocket-launch.py
+++ Python/Py-Fundamentals/74-rocket-launch.py
@@ -32,6 +32,7 @@ NUM... Blastoff!
 # -- Broken Code -- Buggy 
 # ------------------------- #
 
+"""
 def countdown_to_blastoff():
     # ?
 
@@ -50,10 +51,35 @@ def main():
 
 
 main()
+"""
 
 # ------------------------ #
 # -- Repaired Code --
 # ------------------------ #
+
+def countdown_to_blastoff():
+    for i in range(10, 0, -1):
+        if i == 1:
+            print(f"{i}... Blastoff!")
+        else:
+            print(f"{i}...")
+        
+
+
+# Don't edit below this line
+
+
+def test():
+    print("Counting down to blastoff:")
+    countdown_to_blastoff()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
 
 
 
