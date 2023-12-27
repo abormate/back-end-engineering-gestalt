7da3d73abf0957f107b00b5eeafe25344a63b3ad
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-27 18:45:52 +0800

    Python StudySpace

--- Python/Py-Fundamentals/74-rocket-launch.py
+++ Python/Py-Fundamentals/74-rocket-launch.py
@@ -28,3 +28,26 @@ NUM... Blastoff!
 
 """
 
+# ------------------------- #
+# -- Broken Code -- Buggy 
+# ------------------------- #
+
+def countdown_to_blastoff():
+    # ?
+
+
+# Don't edit below this line
+
+
+def test():
+    print("Counting down to blastoff:")
+    countdown_to_blastoff()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
+
