29ea14fee8ebff53679904b042ff18bdbb4970b1
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-27 18:25:16 +0800

    Python StudySpace

--- Python/Py-Fundamentals/73-sum-game-2.py
+++ Python/Py-Fundamentals/73-sum-game-2.py
@@ -4,3 +4,9 @@
 #
 # ----------------------------------- #
 
+"""
+Complete the sum_of_odd_numbers function. It should calculate the sum of all the 
+odd numbers starting at 1 up to (but not including) the given end number and 
+return the result.
+
+"""
