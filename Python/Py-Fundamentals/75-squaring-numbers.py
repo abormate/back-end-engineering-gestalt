af86d8d30e2cc5d6408b6aff5b7b2bc7762cbe1c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-28 12:18:25 +0800

    Python StudySpace

--- Python/Py-Fundamentals/75-squaring-numbers.py
+++ Python/Py-Fundamentals/75-squaring-numbers.py
@@ -4,3 +4,12 @@
 #
 # ---------------------------------------- # 
 
+"""
+John is finishing some math homework and needs to calculate the square of some 
+numbers. However, he's getting tired of entering each of them into a calculator 
+individually.
+
+The square of a number is just the number multiplied by itself.
+
+"""
+
