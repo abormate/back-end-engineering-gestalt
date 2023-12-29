82e7a3cc84c6c56e9b68fb2ca56461d583d1158e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-29 21:15:42 +0800

    Python StudySpace

--- Python/Py-Fundamentals/76-experience-points.py
+++ Python/Py-Fundamentals/76-experience-points.py
@@ -4,3 +4,17 @@
 #
 # ------------------------------------------ #
 
+"""
+You've been hired by a game studio to work on their latest role-playing game, in 
+this game adventurers need experience points (XP) to level up and become 
+stronger. 
+
+You've been tasked with creating a function to calculate the total amount of XP 
+needed for a player to reach a specific level.
+
+Each character starts with 0 XP at level 1. To reach the next level, they need 5 
+more XP than the last level required. So level 2 requires just 5 XP, while level 
+3 requires 10.
+
+"""
+
