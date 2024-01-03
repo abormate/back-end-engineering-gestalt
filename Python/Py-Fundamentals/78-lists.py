85cb27250785092b786c093362044c9fcb88e67d
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-03 12:08:23 +0800

    Python StudySpace -- Lists

---
+++ Python/Py-Fundamentals/78-lists.py
@@ -0,0 +1,12 @@
+# -------------------------------------- #
+#
+# -- Lists -- Python StudySpace --
+#
+# -------------------------------------- #
+
+"""
+A natural way to organize and store data is in a List. Some languages call them 
+"arrays", but in Python we just call them lists. Think of all the apps you use 
+and how many of the items in the app are organized into lists.
+
+"""
