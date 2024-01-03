4b8eacfe7187ee9aafc49a0d23df3d9ae53757af
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-03 11:31:38 +0800

    Python StudySpace

--- Python/Py-Fundamentals/77-prime-numbers.py
+++ Python/Py-Fundamentals/77-prime-numbers.py
@@ -14,3 +14,14 @@ or not a number is prime. She asked you to write a program to help her.
 # -- Assignment -- Practice --
 # -------------------------- #
 
+"""
+Write a function that takes a single number as input and returns True if it is a 
+prime number or False if it is not.
+
+WHAT IS A PRIME NUMBER?
+A prime number is a positive integer, greater than 1, that is only divisible by 
+itself and 1. For example, 2, 3, 5, and 7 are all prime numbers, but 1, 4, 6, 8, 
+and 9 are not.
+
+"""
+
