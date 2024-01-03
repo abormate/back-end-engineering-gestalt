081709405fe3a75e15e8d062ec761136c1e75ea4
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-03 11:21:57 +0800

    Python StudySpace

--- Python/Py-Fundamentals/77-prime-numbers.py
+++ Python/Py-Fundamentals/77-prime-numbers.py
@@ -4,3 +4,8 @@
 #
 # ------------------------------------- #
 
+"""
+A math student is working on a project and needs an easy way to determine whether 
+or not a number is prime. She asked you to write a program to help her.
+
+"""
