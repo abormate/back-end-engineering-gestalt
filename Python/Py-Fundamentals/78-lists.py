2863067cc71732df443d4c48b3a12704dc1ee962
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-04 17:24:48 +0800

    Python StudySpace -- Lists

--- Python/Py-Fundamentals/78-lists.py
+++ Python/Py-Fundamentals/78-lists.py
@@ -17,3 +17,7 @@ The state of a chess game is a list of moves
 This list is a list of things that are lists
 
 """
+
+# Lists in Python are declared using square brackets, with commas separating each item:
+
+inventory = ["Iron Breastplate", "Healing Potion", "Leather Scraps"]
