5c91695379736b511052bca5b71d8054d537a35f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-04 17:24:11 +0800

    Python StudySpace -- Lists

--- Python/Py-Fundamentals/78-lists.py
+++ Python/Py-Fundamentals/78-lists.py
@@ -9,4 +9,11 @@ A natural way to organize and store data is in a List. Some languages call them
 "arrays", but in Python we just call them lists. Think of all the apps you use 
 and how many of the items in the app are organized into lists.
 
+For example:
+
+A twitter feed is a list of posts
+An online store is a list of products
+The state of a chess game is a list of moves
+This list is a list of things that are lists
+
 """
