5f120a87809b836b6758d95911e3af508f4d1a04
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-05 19:34:27 +0800

    Python StudySpace -- Lists Continued

--- Python/Py-Fundamentals/79-lists-continued.py
+++ Python/Py-Fundamentals/79-lists-continued.py
@@ -4,3 +4,9 @@
 #
 # ------------------------------------ #
 
+"""
+Sometimes when we're manually creating lists it can be hard to read if all the 
+items are on the same line of code. We can declare the list using multiple lines 
+if we want to:
+
+"""
