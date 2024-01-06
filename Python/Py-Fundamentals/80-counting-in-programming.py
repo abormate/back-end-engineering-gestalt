9b3b49627eae06bd40cc3695a76448e109d717df
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-06 14:07:04 +0800

    Python StudySpace

--- Python/Py-Fundamentals/80-counting-in-programming.py
+++ Python/Py-Fundamentals/80-counting-in-programming.py
@@ -15,3 +15,10 @@ We don't start counting at 1, we start at 0 instead.
 # -- Indexes --
 # -------------------------- #
 
+"""
+Each item in a list has an index that refers to its spot in the list.
+
+Take the following list as an example:
+
+"""
+
