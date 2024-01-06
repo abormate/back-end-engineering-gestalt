508adab8aa982ea82e854cc451d1db7e6b05e105
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-06 14:15:39 +0800

    Python StudySpace -- Index into Lists

--- Python/Py-Fundamentals/81-index-into-lists.py
+++ Python/Py-Fundamentals/81-index-into-lists.py
@@ -4,3 +4,95 @@
 #
 # --------------------------------------- #
 
+"""
+Now that we know how to create new lists, we need to know how to access specific 
+items in the list.
+
+We access items in a list directly by using their index. Indexes start at 0 
+(the first item) and increment by one with each successive item. The syntax is 
+as follows:
+
+"""
+
+best_languages = ["JavaScript", "Go", "Rust", "Python", "C"]
+print(best_languages[1])
+# prints "Go", because index 1 was provided
+
+
+# ------------------------ #
+# -- Assignment --
+# ------------------------ #
+
+"""
+We need to allow our players to access items in their inventories!
+
+Fix our get_leather_straps function by changing the value of item_index to the 
+index in inventory that holds the value "Leather Scraps".
+
+"""
+
+# ------------------------ #
+# -- Buggy Code --
+# ------------------------ #
+
+def get_leather_straps():
+    inventory = [
+        "Healing Potion",
+        "Leather Scraps",
+        "Iron Helmet",
+        "Bread",
+        "Shortsword",
+    ]
+
+    item_index = 0
+
+    return inventory[item_index]
+
+# ----------------------- #
+# -- Unit Tester --
+# ----------------------- #
+
+from main import *
+
+run_cases = [
+    ("Leather Scraps",),
+]
+
+submit_cases = run_cases + [
+    ("Leather Scraps",),
+]
+
+
+def test(expected_output):
+    print("---------------------------------")
+    print(f"Expecting: {expected_output}")
+    result = get_leather_straps()
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
