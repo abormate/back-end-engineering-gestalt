6b67aa1db77d130e424f9ede8a9957ecb26f46d9
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-08 08:35:58 +0800

    Python StudySpace -- Index into Lists

--- Python/Py-Fundamentals/81-index-into-lists.py
+++ Python/Py-Fundamentals/81-index-into-lists.py
@@ -101,3 +101,15 @@ main()
 # -- Repaired Code --
 # ------------------------ #
 
+def get_leather_straps():
+    inventory = [
+        "Healing Potion",
+        "Leather Scraps",
+        "Iron Helmet",
+        "Bread",
+        "Shortsword",
+    ]
+
+    item_index = 1
+
+    return inventory[item_index]
