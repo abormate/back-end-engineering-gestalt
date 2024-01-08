26f4d02e127c8efea9273bbd5815beed5d22e34b
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-08 08:38:26 +0800

    Python StudySpace

--- Python/Py-Fundamentals/82-list-length.py
+++ Python/Py-Fundamentals/82-list-length.py
@@ -9,3 +9,10 @@
 fruits = ["apple", "banana", "pear"]
 length = len(fruits)
 # Prints: 3
+
+"""
+The length of the list is equal to the number of items present. Don't be fooled 
+by the fact that the length is not equal to the index of the last element, in 
+fact, it will always be one greater.
+
+"""
