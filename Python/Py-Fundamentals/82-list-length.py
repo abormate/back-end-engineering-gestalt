56b1305037fdd9adffccb44277f59586b4897ab1
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-08 08:41:29 +0800

    Python StudySpace

--- Python/Py-Fundamentals/82-list-length.py
+++ Python/Py-Fundamentals/82-list-length.py
@@ -21,3 +21,20 @@ fact, it will always be one greater.
 # -- Assignment -- Practice --
 # ------------------------ #
 
+"""
+Some of our player's inventories are huge, so looking through the entire list is 
+cumbersome. Let's find an easy way for us to get the index of the last item in 
+their inventory.
+
+Complete the get_last_index function so that it returns the length of the 
+inventory list minus 1.
+
+"""
+
+# ------------------------ #
+# -- Buggy Code --
+# ------------------------ #
+
+def get_last_index(lst):
+    pass
+
