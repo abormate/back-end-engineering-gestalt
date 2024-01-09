bc388279f3f41a4082d03115705b82293e04cc21
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-09 12:42:09 +0800

    Python StudySpace

--- Python/Py-Fundamentals/83-list-updates.py
+++ Python/Py-Fundamentals/83-list-updates.py
@@ -19,3 +19,11 @@ inventory[0] = "Leather Armor"
 # -- Assignment -- 
 # --------------------------- #
 
+"""
+We need to update the items in our players' inventory whenever they smelt Iron 
+Ore into an Iron Bar!
+
+On line 7, update the Iron Ore element in the list to be an Iron Bar.
+
+"""
+
