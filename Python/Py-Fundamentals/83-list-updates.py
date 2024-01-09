c4a137c5f336403ecbc5fd7d683ad64178dd729c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-09 12:40:55 +0800

    Python StudySpace

--- Python/Py-Fundamentals/83-list-updates.py
+++ Python/Py-Fundamentals/83-list-updates.py
@@ -4,3 +4,14 @@
 #
 # --------------------------------------- #
 
+"""
+We can also change the item that exists at a given index. For example, we can 
+change Leather to Leather Armor in the inventory list in the following way:
+
+"""
+
+inventory = ["Leather", "Healing Potion", "Iron Ore"]
+inventory[0] = "Leather Armor"
+# inventory: ['Leather Armor', 'Healing Potion', 'Iron Ore']
+
+
