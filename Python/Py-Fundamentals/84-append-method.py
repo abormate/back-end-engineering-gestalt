bd401ae3948438671c9a99fa069ecb236a03d528
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-09 12:47:38 +0800

    Python StudySpace

--- Python/Py-Fundamentals/84-append-method.py
+++ Python/Py-Fundamentals/84-append-method.py
@@ -4,3 +4,9 @@
 # 
 # --------------------------------------- #
 
+"""
+It's common to create an empty list then fill it with values using a loop. We can 
+add values to the end of a list using the .append() method:
+
+"""
+
