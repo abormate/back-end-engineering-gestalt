87469f497504f47d3b5bb34daf87417ade390ff5
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-10 17:04:23 +0800

    Python StudySpace -- Pop Removal

--- Python/Py-Fundamentals/85-pop-removal-values.py
+++ Python/Py-Fundamentals/85-pop-removal-values.py
@@ -15,3 +15,53 @@ last_vegetable = vegetables.pop()
 # vegetables = ['broccoli', 'cabbage', 'kale']
 # last_vegetable = 'tomato'
 
+
+# ---------------------------- #
+# -- Assignment -- Practice --
+# ---------------------------- #
+
+"""
+Our player is selling the items in their inventory to the shopkeep!
+
+Pop the last element from the inventory list until there is nothing left. Pop the 
+elements into an item variable so that each prints in turn on line 19.
+
+"""
+
+# ---------------------------- #
+# -- Buggy Code --
+# ---------------------------- #
+
+def clear_inventory():
+    inventory = [
+        "Healing Potion",
+        "Iron Bar",
+        "Kite Shield",
+        "Shortsword",
+        "Leather Scraps",
+        "Tattered Cloth",
+    ]
+
+    print(f"inventory: {inventory}")
+
+    # don't touch above this line
+
+    for i in range(0, len(inventory)):
+        # ?
+
+        # don't touch below this line
+        print(f"Selling: {item}")
+        print(f"inventory: {inventory}")
+
+
+def test():
+    clear_inventory()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
+
