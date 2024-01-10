efa5be99d9444f6bf83860f555a97902768e4cd7
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-11 06:39:06 +0800

    Python StudySpace -- Pop Removal Values

--- Python/Py-Fundamentals/85-pop-removal-values.py
+++ Python/Py-Fundamentals/85-pop-removal-values.py
@@ -69,3 +69,35 @@ main()
 # -- Functional Repaired Code --
 # ----------------------------- #
 
+def clear_inventory():
+    inventory = [
+        "Healing Potion",
+        "Iron Bar",
+        "Kite Shield",
+        "Shortsword",
+        "Leather Scraps",
+        "Tattered Cloth",
+    ]
+
+    print(f"inventory: {inventory}")
+
+    # don't touch above this line
+
+    for i in range(0, len(inventory)):
+        # ?
+
+        # don't touch below this line
+        print(f"Selling: {item}")
+        print(f"inventory: {inventory}")
+
+
+def test():
+    clear_inventory()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
