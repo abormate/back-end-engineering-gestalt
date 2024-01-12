824351456105d00dc6ab33aa03703a4d69578deb
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-12 13:48:05 +0800

    Python StudySpace

--- Python/Py-Fundamentals/86-count-items-list.py
+++ Python/Py-Fundamentals/86-count-items-list.py
@@ -13,3 +13,8 @@ For example, the following code will print each item in the sports list.
 # for i in range(0, len(sports)):
 #    print(sports[i])
 
+
+# ---------------------------- #
+# -- Assignment -- Practice --
+# ---------------------------- #
+
