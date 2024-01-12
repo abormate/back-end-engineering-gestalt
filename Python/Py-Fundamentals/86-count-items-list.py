61ce660f45d0d4846d12fdcbab59255b6e3a1226
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-12 13:37:46 +0800

    Python StudySpace

--- Python/Py-Fundamentals/86-count-items-list.py
+++ Python/Py-Fundamentals/86-count-items-list.py
@@ -4,3 +4,9 @@
 #
 # ------------------------------------------- #
 
+"""
+Remember that we can iterate (count) over all the elements in a list using a loop. 
+For example, the following code will print each item in the sports list.
+
+"""
+
