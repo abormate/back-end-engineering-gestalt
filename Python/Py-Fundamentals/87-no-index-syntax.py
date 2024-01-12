2812a1707c766e7cdc0682bbffb5069ccd3c8377
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-12 14:00:41 +0800

    Python StudySpace -- No Index Syntax

---
+++ Python/Py-Fundamentals/87-no-index-syntax.py
@@ -0,0 +1,20 @@
+# --------------------------------------- #
+#
+# -- No Index Syntax -- Python StudySpace --
+#
+# --------------------------------------- #
+
+"""
+In my opinion, Python has the most elegant syntax for iterating directly over the 
+items in a list without worrying about index numbers. If you don't need the index 
+number you can use the following syntax:
+
+"""
+
+trees = ['oak', 'pine', 'maple']
+for tree in trees:
+    print(tree)
+# Prints:
+# oak
+# pine
+# maple
