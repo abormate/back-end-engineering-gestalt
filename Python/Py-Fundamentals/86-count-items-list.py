eae0fadb0315cf142275819f7cae5b91861d88ea
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-12 13:48:43 +0800

    Python StudySpace

--- Python/Py-Fundamentals/86-count-items-list.py
+++ Python/Py-Fundamentals/86-count-items-list.py
@@ -18,3 +18,13 @@ For example, the following code will print each item in the sports list.
 # -- Assignment -- Practice --
 # ---------------------------- #
 
+"""
+Our players need a way to see how many copies of a specific item they have within 
+their inventory!
+
+Let's finish the get_item_counts function. Within the loop, count how many times 
+Potion, Bread, and Shortsword appear by incrementing the potion_count, 
+bread_count and shortsword_count variables respectively.
+
+"""
+
