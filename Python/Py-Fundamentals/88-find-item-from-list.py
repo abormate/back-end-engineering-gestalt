1bafb4b574a297c67dfec4a7e944b0503e78aa19
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-15 12:15:33 +0800

    Python StudySpace -- Find Item in List

--- Python/Py-Fundamentals/88-find-item-from-list.py
+++ Python/Py-Fundamentals/88-find-item-from-list.py
@@ -15,3 +15,11 @@ for fruit in fruits:
 # -- Assignment -- Practice --
 # --------------------------- #
     
+"""
+We need to check if a player has a specific item in their inventory. In the 
+contains_leather_scraps function, use the no-index syntax to iterate over each 
+item in items. If you find an item called Leather Scraps, set the found variable 
+to True.
+
+"""
+
