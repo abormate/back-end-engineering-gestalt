83199ef81661b9b248746d072a65369bab4eee95
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-15 20:06:28 +0800

    Python StudySpace --

--- Python/Py-Fundamentals/88-find-item-from-list.py
+++ Python/Py-Fundamentals/88-find-item-from-list.py
@@ -27,4 +27,15 @@ to True.
 # -- Buggy Code --
 # --------------------------- #
 
+def contains_leather_scraps(items):
+    found = False
+
+    # don't touch above this line
+
+    # ?
+
+    # don't touch below this line
+
+    return found
+
 
