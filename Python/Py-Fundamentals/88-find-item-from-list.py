ff7fbf9ec332dd16fc330a82c3678c76dad79286
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-16 09:43:02 +0800

    Python StudySpace -- Find Item from List

--- Python/Py-Fundamentals/88-find-item-from-list.py
+++ Python/Py-Fundamentals/88-find-item-from-list.py
@@ -95,6 +95,18 @@ if "__RUN__" in globals():
 main()
 
 # --------------------------- #
-# -- Repaired Code --
+# -- Work Code --
 # --------------------------- #
 
+def contains_leather_scraps(items):
+    found = False
+
+    # don't touch above this line
+
+    for item in items:
+        if item == "Leather Scraps":
+            found = True
+
+    # don't touch below this line
+
+    return found
