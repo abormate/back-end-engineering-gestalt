6b7dc205ceb76cf5bf38efb55812147787ad5c6d
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-16 09:52:22 +0800

    Python StudySpace

--- Python/Py-Fundamentals/89-find-differences-from-list.py
+++ Python/Py-Fundamentals/89-find-differences-from-list.py
@@ -10,4 +10,10 @@ we want to know about it so we can congratulate them! Your assignment is to find
 any differences between the old_character_levels and new_character_levels lists 
 and to print the index where the values are different.
 
+The existing code in the loop you've been given loops over all of the indexes in 
+old_character_levels. Because old_character_levels and new_character_levels are 
+the same lengths, you can reuse i to index into both.
+
+
+
 """
