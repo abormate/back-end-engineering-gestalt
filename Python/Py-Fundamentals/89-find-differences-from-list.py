a7de5e02cfb70468fa84877c72227a0d11afdffc
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-16 09:46:46 +0800

    Python StudySpace -- Differences

--- Python/Py-Fundamentals/89-find-differences-from-list.py
+++ Python/Py-Fundamentals/89-find-differences-from-list.py
@@ -4,3 +4,10 @@
 #
 # ------------------------------------------- #
 
+"""
+We keep track of each character's level in a list. When someone's level changes 
+we want to know about it so we can congratulate them! Your assignment is to find 
+any differences between the old_character_levels and new_character_levels lists 
+and to print the index where the values are different.
+
+"""
