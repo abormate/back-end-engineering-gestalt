a26b0d2997c4fc09c42fdbab5da18c0fb6a5813a
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-17 12:22:57 +0800

    Python StudySpace -- Find Maximum

--- Python/Py-Fundamentals/90-find-max.py
+++ Python/Py-Fundamentals/90-find-max.py
@@ -11,3 +11,7 @@ greater than negative infinity we can use it to help us accomplish our goal of
 finding the max value. I've added it for you as a starting point.
 
 """
+
+negative_infinity = float('-inf')
+positive_infinity = float('inf')
+
