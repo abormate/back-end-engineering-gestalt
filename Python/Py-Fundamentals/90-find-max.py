3c2f2b11a1e26fb0caa1079bb4c25ce9a85d0dd0
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-17 12:21:43 +0800

    Python StudySpace

--- Python/Py-Fundamentals/90-find-max.py
+++ Python/Py-Fundamentals/90-find-max.py
@@ -4,3 +4,10 @@
 #
 # --------------------------------------- #
 
+"""
+The built-in float() function can be used to create a numeric floating point 
+value that represents the negative infinity value. Because every value will be 
+greater than negative infinity we can use it to help us accomplish our goal of 
+finding the max value. I've added it for you as a starting point.
+
+"""
