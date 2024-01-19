d933a11cc9e4c3c9bef6aa412cd872ddb95e65d8
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-19 11:31:12 +0800

    Python StudySpace

--- Python/Py-Fundamentals/92-slice-dice-lists.py
+++ Python/Py-Fundamentals/92-slice-dice-lists.py
@@ -4,3 +4,15 @@
 #
 # -------------------------------------------- #
 
+"""
+Python makes it easy to slice and dice lists to work only with the section you 
+care about. One way to do this is to use the simple slicing operator, which is 
+just a colon :.
+
+With this operator, you can specify where to start and end the slice, and how to 
+step through the original. List slicing returns a new list from the existing 
+list.
+
+The syntax is as follows:
+
+"""
