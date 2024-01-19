894fa121362df3d4de178123b69150729b0cc050
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-19 11:39:30 +0800

    Python StudySpace -- Slicing Lists

--- Python/Py-Fundamentals/92-slice-dice-lists.py
+++ Python/Py-Fundamentals/92-slice-dice-lists.py
@@ -72,3 +72,17 @@ numbers[-3:] # Gives [7, 8, 9]
 # -- Assignment -- Practice --
 # -------------------------- #
 
+"""
+Complete the given get_champion_slices function. It takes a list of champions and 
+should return three new lists based on the given champions:
+
+First, return a slice of the champions list that starts with the third champion 
+and goes to the end of the list.
+
+Next, return a slice of the champions list that starts at the beginning of the 
+list and ends with the third champion from the end (inclusive).
+
+Last, return a slice of the champions list that only includes the champions in 
+even numbered indexes.
+
+"""
