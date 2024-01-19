d59409090694a0c8dc3af3da963dbc79e037d6c1
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-19 11:52:28 +0800

    Python StudySpace -- Concatenate Lists

---
+++ Python/Py-Fundamentals/93-concatenate-list-ops.py
@@ -0,0 +1,12 @@
+# --------------------------------------------- #
+#
+# -- Concatenate -- List -- Python StudySpace --
+#
+# --------------------------------------------- #
+
+"""
+Concatenating two lists (smushing them together) is easy in Python, just use the 
++ operator.
+
+"""
+
