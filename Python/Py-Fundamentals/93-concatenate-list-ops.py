7acf0d58dd70f15e19d383d0783b89fdac2ed606
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-19 11:54:12 +0800

    Python StudySpace -- Concatenate Lists

--- Python/Py-Fundamentals/93-concatenate-list-ops.py
+++ Python/Py-Fundamentals/93-concatenate-list-ops.py
@@ -10,3 +10,30 @@ Concatenating two lists (smushing them together) is easy in Python, just use the
 
 """
 
+total = [1, 2, 3] + [4, 5, 6]
+print(total)
+# Prints: [1, 2, 3, 4, 5, 6]
+
+# ------------------------------ #
+# -- Assignment -- Practice --
+# ------------------------------ #
+
+"""
+Fantasy Quest allows users to keep lists of their favorite items. Your job is to 
+finish the concatenate_favorites function. It takes three different parameters - 
+the player's favorite_weapons, favorite_armor and favorite_items.
+
+Create a new list that combines favorite_weapons, favorite_armor, and 
+favorite_items in this order.
+
+Return the list containing the combined favorites.
+
+"""
+
+# ----------------------------- #
+# -- Buggy Code --
+# ----------------------------- #
+
+def concatenate_favorites(favorite_weapons, favorite_armor, favorite_items):
+    pass
+
