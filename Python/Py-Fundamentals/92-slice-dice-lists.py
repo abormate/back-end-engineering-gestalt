37894eb5e70de7d7684db24af82c68038d7363b8
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-19 11:49:23 +0800

    Python StudySpace -- List Slicing

--- Python/Py-Fundamentals/92-slice-dice-lists.py
+++ Python/Py-Fundamentals/92-slice-dice-lists.py
@@ -171,3 +171,8 @@ main()
 # -- Worked on Code --
 # ------------------------ #
 
+def get_champion_slices(champions):
+    first = champions[2:]
+    second = champions[:-2]
+    third = champions[::2]
+    return first, second, third
