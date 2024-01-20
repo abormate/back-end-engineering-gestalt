d5811d2ec6185f1e99b1aea9b2ee340ace505c4f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-20 14:56:31 +0800

    Python StudySpace -- List Ops Contains

---
+++ Python/Py-Fundamentals/94-list-ops-contains.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------ #
+#
+# -- List Operations -- Contains -- Python StudySpace --
+#
+# ------------------------------------------------ #
+
+"""
+Checking whether a value exists in a list is also really easy in Python, just use 
+the in keyword.
+
+"""
+
+fruits = ["apple", "orange", "banana"]
+print("apple" in fruits)
+# Prints: True
