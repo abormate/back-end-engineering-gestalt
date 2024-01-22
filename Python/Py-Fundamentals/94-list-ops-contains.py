ab5b6ba21353063cc4959ed75fda157700dc8939
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-22 12:16:24 +0800

    Python StudySpace -- List Contains Built-in Method

--- Python/Py-Fundamentals/94-list-ops-contains.py
+++ Python/Py-Fundamentals/94-list-ops-contains.py
@@ -108,3 +108,20 @@ main()
 # -- Worked on Code --
 # ------------------------------------- #
 
+def is_top_weapon(weapon):
+    top_weapons = [
+        "sword of justice",
+        "sword of slashing",
+        "stabby daggy",
+        "great axe",
+        "silver bow",
+        "spellbook",
+        "spiked knuckles",
+    ]
+
+    # don't touch above this line
+
+    if weapon in top_weapons:
+        return True
+    else:
+        return False
