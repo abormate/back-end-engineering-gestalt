2e7e24cae309614421000e2b30815dfc97b0db71
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-22 11:56:32 +0800

    Python StudySpace -- Contains List

--- Python/Py-Fundamentals/94-list-ops-contains.py
+++ Python/Py-Fundamentals/94-list-ops-contains.py
@@ -18,3 +18,11 @@ print("apple" in fruits)
 # -- Assignment -- Practice --
 # ----------------------------------- #
 
+"""
+Our players have requested an in-game feature that will allow them to type in a 
+weapon name to check if it's in the list of top weapons in the realm.
+
+Complete the is_top_weapon function. It should return True if the weapon is in 
+the top_weapons list, otherwise it should return False.
+
+"""
