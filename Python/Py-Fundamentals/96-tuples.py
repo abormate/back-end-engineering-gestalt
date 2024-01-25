f9ec78dcda714deb1cf02e2b3a4312dd59108656
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-25 20:50:29 +0800

    Python StudySpace -- Tuples

--- Python/Py-Fundamentals/96-tuples.py
+++ Python/Py-Fundamentals/96-tuples.py
@@ -4,3 +4,9 @@
 #
 # ----------------------------------- #
 
+"""
+Tuples are collections of data that are ordered and unchangeable. You can think 
+of a tuple as a List with a fixed size. Tuples are created with round brackets:
+
+"""
+
