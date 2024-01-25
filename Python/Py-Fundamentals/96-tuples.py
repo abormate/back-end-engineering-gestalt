0d4264740098c71aa11ad4dd0d4586477f6e861c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-25 20:51:06 +0800

    Python StudySpace -- Tuples

--- Python/Py-Fundamentals/96-tuples.py
+++ Python/Py-Fundamentals/96-tuples.py
@@ -10,3 +10,10 @@ of a tuple as a List with a fixed size. Tuples are created with round brackets:
 
 """
 
+my_tuple = ("this is a tuple", 45, True)
+print(my_tuple[0])
+# this is a tuple
+print(my_tuple[1])
+# 45
+print(my_tuple[2])
+# True
