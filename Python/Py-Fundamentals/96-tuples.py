e3674e99ef81dd407cddc41242724574df8f289b
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-26 21:08:03 +0800

    Python StudySpace -- Tuples

--- Python/Py-Fundamentals/96-tuples.py
+++ Python/Py-Fundamentals/96-tuples.py
@@ -73,3 +73,36 @@ Use the same data for each hero, and order it in the same way.
 # -- Buggy Code --
 # --------------------------------- #
 
+def get_heroes():
+    heroes = [
+        "Glorfindel",
+        2093,
+        True,
+        "Gandalf",
+        1054,
+        False,
+        "Gimli",
+        389,
+        False,
+        "Aragorn",
+        87,
+        False,
+    ]
+
+    return heroes
+
+
+# Don't touch below this line
+
+
+def test():
+    heroes = get_heroes()
+    for hero in heroes:
+        print(f"name: {hero[0]}, age: {hero[1]}, is_elf: {hero[2]}")
+
+
+def main():
+    test()
+
+
+main()
