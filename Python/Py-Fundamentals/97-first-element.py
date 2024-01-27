01648da8dc84d0bdf68c7189b7e7fbfe106cbaa1
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-27 20:53:49 +0800

    Python StudySpace

--- Python/Py-Fundamentals/97-first-element.py
+++ Python/Py-Fundamentals/97-first-element.py
@@ -4,3 +4,9 @@
 #
 # ----------------------------------------- #
 
+"""
+Let's add another function to our inventory system. Write a function that returns 
+the first element from a list. If the list is empty then return the string ERROR 
+instead.
+
+"""
