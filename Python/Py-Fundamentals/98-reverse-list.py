f4319e474d6d66e00045c40ac005cce679776f94
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-29 13:38:01 +0800

    Python StudySpace -- Reverse

--- Python/Py-Fundamentals/98-reverse-list.py
+++ Python/Py-Fundamentals/98-reverse-list.py
@@ -4,3 +4,13 @@
 #
 # ---------------------------------------- #
 
+"""
+Some of our players would like to view their inventories in reverse order.
+
+Let's write a function that takes a list as an input and returns a new list 
+except all the items are in reverse order.
+
+For example:
+
+"""
+
