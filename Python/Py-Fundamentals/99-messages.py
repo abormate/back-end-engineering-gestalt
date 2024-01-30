126a5b84b92fa26487c54ddd8ddc4698cdf70aef
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-30 20:22:55 +0800

    Python StudySpace

--- Python/Py-Fundamentals/99-messages.py
+++ Python/Py-Fundamentals/99-messages.py
@@ -4,3 +4,13 @@
 #
 # ------------------------------------ #
 
+"""
+You are about to write a bit more code in a single function than you have before.
+
+Do not try to write it all at once. Start with the outermost loop, and work your 
+way inwards. Add extra print() statements and run your code often to make sure 
+it's doing what you expect. Just make sure to remove the extra print() 
+statements before submitting your code.
+
+
+"""
