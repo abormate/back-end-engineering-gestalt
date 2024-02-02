3c4040b87f2f756aec0b293a2f3bf9a6d9919499
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-02 22:22:52 +0800

    Python StudySpace -- Evens and Odds

--- Python/Py-Fundamentals/100-evens-and-odds.py
+++ Python/Py-Fundamentals/100-evens-and-odds.py
@@ -21,6 +21,10 @@ if each number in the list is either odd or even.
 Increment the num_evens counter if even, and the num_odds counter if it's odd. 
 Lastly, return the two values num_odds and num_evens in that order.
 
+----------
+!! TIP !!
+----------
 
+Remember, you can check if a number is even by using the modulo operator (%).
 
 """
