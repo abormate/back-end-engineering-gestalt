784c08c5793e7424b24e514b63db74e048482904
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-02 22:21:27 +0800

    Python StudySpace

--- Python/Py-Fundamentals/100-evens-and-odds.py
+++ Python/Py-Fundamentals/100-evens-and-odds.py
@@ -14,3 +14,9 @@ numbers exist in a list.
 # -- Assignment --
 # ---------------------------- #
 
+"""
+Write the get_odds_and_evens function to loop through the numbers list and check 
+if each number in the list is either odd or even.
+
+
+"""
