79947d3c1a9ed0659bab419717568cd8ec054344
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-02 22:20:58 +0800

    Python StudySpace -- Evens and Odds

--- Python/Py-Fundamentals/100-evens-and-odds.py
+++ Python/Py-Fundamentals/100-evens-and-odds.py
@@ -9,3 +9,8 @@ You've been asked to write a program that will calculate how many odd and even
 numbers exist in a list.
 
 """
+
+# ---------------------------- #
+# -- Assignment --
+# ---------------------------- #
+
