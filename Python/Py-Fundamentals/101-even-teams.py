adebcc056eedefa6521e95020d67513ab2b96ec2
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-03 22:18:50 +0800

    Python StudySpace

--- Python/Py-Fundamentals/101-even-teams.py
+++ Python/Py-Fundamentals/101-even-teams.py
@@ -4,3 +4,10 @@
 #
 # -------------------------------------- #
 
+"""
+Students at the local wizarding school have been spending too much time trying to 
+split their players up into even teams. The coach has provided you with a list of 
+the players in the class and has asked you to write a program that will split the 
+players into even teams.
+
+"""
