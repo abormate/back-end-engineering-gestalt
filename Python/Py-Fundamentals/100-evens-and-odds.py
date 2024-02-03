35856164786cd08bf4ca3b13afc8b2c0c9c91fcb
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-03 22:12:20 +0800

    Python StudySpace -- Odds and Evens

--- Python/Py-Fundamentals/100-evens-and-odds.py
+++ Python/Py-Fundamentals/100-evens-and-odds.py
@@ -107,5 +107,12 @@ def get_odds_and_evens(numbers):
 
     # Don't touch above this line
 
+    for i in numbers:
+        if i % 2 == 0:
+            num_evens += 1
+        else:
+            num_odds += 1
+    return num_odds, num_evens
+
     
 
