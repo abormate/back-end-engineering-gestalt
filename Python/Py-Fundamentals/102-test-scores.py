ebcdf3690cdd928db870935b34e65cc66c25d56f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-06 09:25:35 +0800

    Python StudySpace

--- Python/Py-Fundamentals/102-test-scores.py
+++ Python/Py-Fundamentals/102-test-scores.py
@@ -12,3 +12,15 @@ She has asked you to write a program that compares an answer key to a student's
 multiple-choice answers and calculates the percentage of questions they got right.
 
 """
+
+# -------------------------- #
+# -- Assignment --
+# -------------------------- #
+
+""" 
+Finish the get_test_scores function by looping over the answer_sheet and 
+student_answers lists. Calculate and return the student's score.
+
+For example, if these were the lists:
+
+"""
