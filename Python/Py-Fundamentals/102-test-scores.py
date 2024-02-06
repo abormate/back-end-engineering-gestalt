639420db09b1061bb0fd59ca107d857b5854d7e3
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-06 09:18:57 +0800

    Python StudySpace

--- Python/Py-Fundamentals/102-test-scores.py
+++ Python/Py-Fundamentals/102-test-scores.py
@@ -8,5 +8,7 @@
 Your teacher has been manually grading tests by hand and it has been taking up 
 all of her free time. 
 
+She has asked you to write a program that compares an answer key to a student's 
+multiple-choice answers and calculates the percentage of questions they got right.
 
 """
