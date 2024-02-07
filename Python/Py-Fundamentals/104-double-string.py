f4811da15cdca80e75f8f17dbbf3611d54e404ba
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-07 13:17:47 +0800

    Python StudySpace

--- Python/Py-Fundamentals/104-double-string.py
+++ Python/Py-Fundamentals/104-double-string.py
@@ -4,3 +4,13 @@
 #
 # -------------------------------------- #
 
+"""
+An alien spaceship has landed on Earth! In an extremely realistic, and not 
+completely fictional turn of events... the alien race speaks English, albeit a 
+slightly modified version of English.
+
+Our English: Hello there
+Their English: HHeelllloo tthheerree
+
+"""
+
