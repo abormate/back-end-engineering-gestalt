046f32b673e4952171cb9e1f7b1586d20ffc22d8
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-07 09:43:01 +0800

    Python StudySpace -- Test Scores 2

--- Python/Py-Fundamentals/103-test-scores-2.py
+++ Python/Py-Fundamentals/103-test-scores-2.py
@@ -19,4 +19,25 @@ wants a more flexible program that can grade all of her student's tests.
 Complete the get_test_score function. It should calculate a report that describes 
 the percentage of multiple-choice answers a student got right on their test.
 
+-------
+INPUTS
+-------
+
+--> answer_sheet: A list of the correct multiple-choice answers
+--> student_answers: A list where the first index is the name of the student, but 
+    the rest of the list consists of the student's multiple-choice answers.
+
+-------    
+OUTPUT
+-------
+
+The function should return 2 values:
+
+name: a string
+percentage: a float
+
+e.g.
+
+return name, percentage
+
 """
