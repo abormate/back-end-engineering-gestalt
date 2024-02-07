292b2adae10ab75aa94a40f354172d2b629c1931
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-07 13:14:52 +0800

    Python StudySpace -- Exam Test Scores 2

--- Python/Py-Fundamentals/103-test-scores-2.py
+++ Python/Py-Fundamentals/103-test-scores-2.py
@@ -389,3 +389,11 @@ main()
 # -- Worked on Code --
 # ------------------------------- #
 
+def get_test_score(answer_sheet, student_answers):
+    score = 0
+    student_name = student_answers[0]
+    for i in range(len(answer_sheet)):
+        if answer_sheet[i] == student_answers[i + 1]:
+            score += 1
+    percentage = score / len(answer_sheet) * 100
+    return student_name, percentage
