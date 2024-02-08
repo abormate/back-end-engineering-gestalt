87aab38343ce91d0e536bb113961a61dee497da0
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-08 15:40:21 +0800

    Python StudySpace

--- Python/Py-Fundamentals/105-dictionaries.py
+++ Python/Py-Fundamentals/105-dictionaries.py
@@ -9,3 +9,10 @@ Dictionaries in Python are used to store data values in key -> value pairs.
 Dictionaries are a great way to store groups of information.
 
 """
+
+car = {
+  "brand": "Tesla",
+  "model": "3",
+  "year": 2019
+}
+
