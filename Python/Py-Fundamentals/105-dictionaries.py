653d1316b014ed8674877c6e31e9480a3b8792c2
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-08 15:41:41 +0800

    Python StudySpace

--- Python/Py-Fundamentals/105-dictionaries.py
+++ Python/Py-Fundamentals/105-dictionaries.py
@@ -20,3 +20,29 @@ car = {
 # -- Assignment --
 # ---------------------------- #
 
+"""
+Complete the get_character_record function. It takes a character's name, server, 
+level, and rank as individual inputs, and returns a dictionary with the following 
+keys:
+
+name
+server
+level
+rank
+id
+
+
+Each key should map to its corresponding input, except the id key. The id key 
+maps to the name and the server inputs concatenated with a # in the middle for 
+uniqueness. We can't have two bloodwarrior123's on the same server!
+
+For example, given:
+
+name = bloodwarrior123
+server = server1
+
+
+Then the id field should be set to bloodwarrior123#server1. I recommend using an 
+f-string to create the id field.
+
+"""
