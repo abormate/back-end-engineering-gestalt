c2e663a34043c107658cf2827849fceb4a84df6d
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-09 11:55:39 +0800

    Python StudySpace

---
+++ Python/Py-Fundamentals/106-duplicate-keys.py
@@ -0,0 +1,13 @@
+# ------------------------------------------ #
+#
+# -- Duplicate Keys -- Python StudySpace --
+#
+# ------------------------------------------ #
+
+"""
+Because dictionaries rely on unique keys, you can't have two of the same key in 
+the same dictionary. If you try to use the same key twice, the first value will 
+simply be overwritten.
+
+"""
+
