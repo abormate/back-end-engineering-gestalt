3fefb743af647dcac661827f794838196c13abeb
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-09 18:16:49 +0800

    Python StudySpace -- Set Dictionary Values

--- Python/Py-Fundamentals/108-setting-dictionary-values.py
+++ Python/Py-Fundamentals/108-setting-dictionary-values.py
@@ -11,3 +11,30 @@ is the same as getting data out of a key, just use the assignment operator (=) t
 give that key a value.
 
 """
+
+names = ["jack bronson", "jill mcarty", "john denver"]
+
+names_dict = {}
+for name in names:
+    # .split() returns a list of strings
+    # where each string is a single word from the original
+    name_lst = name.split()
+
+    # here we update the dictionary
+    names_dict[name_lst[0]] = name_lst[1]
+
+print(names_dict)
+# Prints: {'jack': 'bronson', 'jill': 'mcarty', 'john': 'denver'}
+
+# ------------------------------- #
+# -- Practice -- Assignment --
+# ------------------------------- #
+
+"""
+Which is the correct syntax to create a new key/value pair in an existing 
+dictionary?
+
+-- answer --
+dict[key] = value
+
+"""
