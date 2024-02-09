c08d0d073cd954a4a89164fbb254fe4e35c4cc26
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-09 18:12:28 +0800

    Python StudySpace -- Set Values for Dictionary

---
+++ Python/Py-Fundamentals/108-setting-dictionary-values.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------- #
+#
+# -- Set Dictionary Values -- Python StudySpace --
+#
+# ----------------------------------------------- #
+
+"""
+You don't need to create a dictionary with values already inside. It is common to 
+create a blank dictionary then populate it later using dynamic values. The syntax 
+is the same as getting data out of a key, just use the assignment operator (=) to 
+give that key a value.
+
+"""
