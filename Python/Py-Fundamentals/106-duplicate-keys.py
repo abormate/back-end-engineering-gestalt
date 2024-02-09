995ed53662260c333806068a5f42506f4469317c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-09 11:56:44 +0800

    Python StudySpace

--- Python/Py-Fundamentals/106-duplicate-keys.py
+++ Python/Py-Fundamentals/106-duplicate-keys.py
@@ -11,3 +11,26 @@ simply be overwritten.
 
 """
 
+# -------------------------------- #
+# -- Assignment -- Practice --
+# -------------------------------- #
+
+"""
+Another developer on our team has introduced a bug by specifying duplicate keys 
+in the dictionary! Fix the bug.
+
+The get_character_record function takes a character's name, server, level, and 
+rank. It should return a dictionary with the following fields:
+
+name
+server
+level
+rank
+id
+
+
+Where the id is the name and the server concatenated together with a # in the 
+middle for uniqueness. We can't have two bloodwarrior123's on the same server!
+
+
+"""
