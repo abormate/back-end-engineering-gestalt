9028e09aef7006905cc257b6dfa32c625c56982e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-10 10:00:41 +0800

    Python StudySpace

--- Python/Py-Fundamentals/109-updating-dictionary-values.py
+++ Python/Py-Fundamentals/109-updating-dictionary-values.py
@@ -4,3 +4,9 @@
 #
 # ----------------------------------------------- #
 
+"""
+If you try to set the value of a key that already exists, you'll end up just 
+updating the value of that key.
+
+"""
+
