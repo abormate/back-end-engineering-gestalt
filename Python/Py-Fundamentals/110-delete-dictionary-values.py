5fc7cbec5a44d1f42453a0d2df66bd2e4cec1c07
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-12 10:24:42 +0800

    Python StudySpace -- Delete Dictionary Values

--- Python/Py-Fundamentals/110-delete-dictionary-values.py
+++ Python/Py-Fundamentals/110-delete-dictionary-values.py
@@ -6,3 +6,13 @@
 
 # You can delete existing keys using the del keyword.
 
+names_dict = {
+    'jack': 'bronson',
+    'jill': 'mcarty',
+    'joe': 'denver'
+}
+
+del names_dict['joe']
+
+print(names_dict)
+# Prints: {'jack': 'bronson', 'jill': 'mcarty'}
