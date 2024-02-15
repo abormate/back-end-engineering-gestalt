f920615f60b080801ce10a9ecb94539e5f9b28dd
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-15 08:56:42 +0800

    Python StudySpace

--- Python/Py-Fundamentals/111-counting-practice.py
+++ Python/Py-Fundamentals/111-counting-practice.py
@@ -6,3 +6,14 @@
 
 # If you're unsure whether or not a key exists in a dictionary, use the in keyword.
 
+cars = {
+    'ford': 'f150',
+    'tesla': '3'
+}
+
+print('ford' in cars)
+# Prints: True
+
+print('gmc' in cars)
+# Prints: False
+
