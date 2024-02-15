d86c12ff720e7f2dee45ed0e4c8e422854d1a708
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-15 11:32:46 +0800

    Python StudySpace -- Iterating over dictionaries

--- Python/Py-Fundamentals/112-iterating-over-dictionary.py
+++ Python/Py-Fundamentals/112-iterating-over-dictionary.py
@@ -22,3 +22,34 @@ for name in fruit_sizes:
 # -- Assignment -- Practice --
 # ----------------------------- #
     
+"""
+We need to display on our player's screens what the most common enemy in a given 
+area of the game map is.
+
+Complete the get_most_common_enemy function by iterating over all enemies in the 
+dictionary and returning only the name of the enemy with the highest count. If 
+there are no enemies, return None.
+
+enemies_dict is a dictionary of name -> count.
+
+"""
+
+# ----------------------------- #
+# -- Negative Infinity -- Tip --
+# ----------------------------- #
+
+"""
+When you're trying to find a "max" value, it helps to keep track of the 
+"max so far" in a variable and to start that variable at the lowest possible 
+number, negative infinity.
+
+"""
+
+max_so_far = float("-inf")
+
+"""
+You'll also want to keep track of the enemy name associated with the maximum count.
+I would set the default for that variable to None.
+
+"""
+
