2c1436797936ef29e7df6be1343d0d886442a5d7
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-15 11:27:44 +0800

    Python StudySpace -- Iterating over Dictionary

--- Python/Py-Fundamentals/112-iterating-over-dictionary.py
+++ Python/Py-Fundamentals/112-iterating-over-dictionary.py
@@ -4,3 +4,16 @@
 #
 # ----------------------------------------------- #
 
+fruit_sizes = {
+    "apple": "small",
+    "banana": "large",
+    "grape": "tiny"
+}
+
+for name in fruit_sizes:
+    size = fruit_sizes[name]
+    print(f"name: {name}, size: {size}")
+
+# name: apple, size: small
+# name: banana, size: large
+# name: grape, size: tiny
