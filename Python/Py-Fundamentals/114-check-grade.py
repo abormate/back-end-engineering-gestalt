bcefaca405cf9b72871c2a83f697c6bf2740ccb0
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-17 08:18:52 +0800

    Python StudySpace -- English 1010 Check Grades

---
+++ Python/Py-Fundamentals/114-check-grade.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------- #
+#
+# -- Check Grade -- Python StudySpace -- Challenge --
+#
+# ----------------------------------------------- #
+
+"""
+A local college is having trouble with its student administration program. They 
+have asked you to help them get a student's grade from their English_1010 class.
+
+Here's the structure of a student dictionary:
+
+"""
+
