b26c2d4bb9f4c94420878aefc33c305c3fe0b4ce
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-18 12:26:26 +0800

    Python StudySpace

---
+++ Python/Py-Fundamentals/115-total-score.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------- #
+#
+# -- Total Score -- Python StudySpace --
+#
+# ----------------------------------------------- #
+
+"""
+A website that tracks basketball scores and stats is having trouble with its data. 
+The first-half score and second-half score are stored in separate dictionaries, 
+making it difficult for them to parse the overall score. 
+
+They have asked you to 
+help them write a program that merges the two dictionaries and another function 
+that calculates the total score.
+
+"""
