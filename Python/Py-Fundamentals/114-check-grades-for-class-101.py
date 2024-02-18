85f22b07b050350437013f0658819794be34e4ee
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-18 12:20:23 +0800

    Python StudySpace -- Accessing Deeply Nested Dictionaries

--- Python/Py-Fundamentals/114-check-grades-for-class-101.py
+++ Python/Py-Fundamentals/114-check-grades-for-class-101.py
@@ -188,3 +188,5 @@ main()
 # -- Worked on Code --
 # ----------------------------- #
 
+def get_english_grade(student):
+    return student["type"]["student"]["courses"]["English_1010"]["current_grade"]
