23c6941e705c206b5453c50eeee035e6ef860b97
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-18 12:29:45 +0800

    Python StudySpace

--- Python/Py-Fundamentals/115-total-score.py
+++ Python/Py-Fundamentals/115-total-score.py
@@ -26,6 +26,12 @@ The merge function accepts two score dictionaries as input and returns a single
 merged dictionary that contains all of the keys and values from the input 
 dictionaries.
 
+The total_score function should take a single score dictionary as input and 
+return the total score calculated from the values of that dictionary. Take a 
+look at the test suite near the top of the file for the names of keys to expect. 
+
+If no points were scored, the function should return 0.
+
 
 
 """
