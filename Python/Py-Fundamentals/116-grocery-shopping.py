e07c8659eaad8e3dd0d2179924ce2de59550ba31
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-21 16:52:33 +0800

    Python StudySpace

--- Python/Py-Fundamentals/116-grocery-shopping.py
+++ Python/Py-Fundamentals/116-grocery-shopping.py
@@ -17,4 +17,39 @@ help her manage her finances when she's grocery shopping.
 """
 Complete the calculate_total function.
 
+--------
+Inputs 
+--------
+
+-- items_purchased: A list of the names of items purchased on this shopping trip. 
+This is a list of strings.
+
+-- grocery_list: A list of the names of items Emma wanted to purchase. This is 
+also a list of strings.
+
+
+
+-------
+Outputs
+-------
+
+The function should return 3 values in this order:
+
+-- unpurchased_items: A list of all the item names in grocery_list that weren't 
+found in items_purchased.
+
+-- receipt: A dictionary containing all the items Emma purchased, even stuff not 
+on her list. The keys are the item names and the values are their respective 
+prices from the item_prices dictionary.
+
+-- total: The total cost of all the items that were purchased.
+
+
+Return each value separately, not in a list. For example:
+
+-- return item1, item2, item3
+
 """
+
+# NOTE: The .remove() list method may be helpful.
+
