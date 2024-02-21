240d157f7be6a8b5ef2274fa789a6a9fd69b4023
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-21 16:46:51 +0800

    Python StudySpace

--- Python/Py-Fundamentals/116-grocery-shopping.py
+++ Python/Py-Fundamentals/116-grocery-shopping.py
@@ -4,3 +4,8 @@
 #
 # ------------------------------------------ #
 
+"""
+Emma has been overspending recently and wants you to write a script that will 
+help her manage her finances when she's grocery shopping.
+
+"""
