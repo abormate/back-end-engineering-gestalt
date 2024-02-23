ba17d8bd3a17555b9befef718f360d4152f56b58
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-23 08:08:13 +0800

    Python StudySpace

--- Python/Py-Fundamentals/117-unordered-sets.py
+++ Python/Py-Fundamentals/117-unordered-sets.py
@@ -4,3 +4,8 @@
 #
 # ----------------------------------------------- #
 
+"""
+Sets are like Lists, but they are unordered and they guarantee uniqueness. Only 
+ONE of each value can be in a set.
+
+"""
