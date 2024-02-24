a338d4a6f6ad473545081c6a190d2461b2365fac
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-24 09:51:44 +0800

    Python StudySpace

--- Python/Py-Fundamentals/117-unordered-sets.py
+++ Python/Py-Fundamentals/117-unordered-sets.py
@@ -9,3 +9,10 @@ Sets are like Lists, but they are unordered and they guarantee uniqueness. Only
 ONE of each value can be in a set.
 
 """
+
+fruits = {'apple', 'banana', 'grape'}
+print(type(fruits))
+# Prints: <class 'set'>
+
+print(fruits)
+# Prints: {'banana', 'grape', 'apple'}
