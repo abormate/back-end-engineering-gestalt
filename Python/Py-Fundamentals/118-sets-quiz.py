a996bbb86cd2f66c2905c7fd810383fde8faa039
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-26 16:21:02 +0800

    Python StudySpace -- Sets Quiz

---
+++ Python/Py-Fundamentals/118-sets-quiz.py
@@ -0,0 +1,18 @@
+# ---------------------------------------- #
+#
+# -- Sets Quiz -- Python StudySpace --
+#
+# ---------------------------------------- #
+
+"""
+Sets are like Lists, but they are unordered and they guarantee uniqueness. Only 
+ONE of each value can be in a set.
+
+"""
+
+fruits = {'apple', 'banana', 'grape'}
+print(type(fruits))
+# Prints: <class 'set'>
+
+print(fruits)
+# Prints: {'banana', 'grape', 'apple'}
