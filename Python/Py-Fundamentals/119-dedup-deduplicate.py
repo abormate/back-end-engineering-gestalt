115e4886a81f08b952a901882eb3b0088b9b0c2f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-02 21:49:38 +0800

    Python StudySpace

--- Python/Py-Fundamentals/119-dedup-deduplicate.py
+++ Python/Py-Fundamentals/119-dedup-deduplicate.py
@@ -4,3 +4,25 @@
 #
 # ------------------------------------------------ #
 
+"""
+----------
+Scenario: 
+----------
+
+John is a data analyst, but he's bad with code. He has asked you for a script 
+that can remove duplicates from some of his data sets.
+
+"""
+
+# -------------------------------------- #
+# -- Assignment -- Practice --
+# -------------------------------------- #
+
+"""
+Complete the remove_duplicates function. It accepts a list of strings and removes 
+any duplicate values. It should utilize and return a set, not a list!
+
+Bonus points if you can write the body of the function in a single line of code.
+
+"""
+
