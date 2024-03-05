b1c515ed1d80f248be3607b328ac6966781480cb
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-05 13:11:53 +0800

    Python StudySpace -- Vowel Counting Activity

--- Python/Py-Fundamentals/120-vowel-counter.py
+++ Python/Py-Fundamentals/120-vowel-counter.py
@@ -4,3 +4,21 @@
 #
 # ---------------------------------------- #
 
+"""
+Context necessary --
+
+You've been hired by a blogging company and they are asking you to analyze one 
+of their recent posts to determine the number of vowels present in their 
+paragraphs.
+
+
+CHALLENGE Activity
+Complete the count_vowels function. It should take a string and return a count 
+of the number of vowels within the given string, and a set of its unique vowels.
+
+TIP
+For this challenge, we are only interested in the 5 vowels: a, e, i, o, u, and 
+their capitalized versions. In addition, treat uppercase and lowercase vowels as 
+separate. For example, "A" and "a" are not the same.
+
+"""
