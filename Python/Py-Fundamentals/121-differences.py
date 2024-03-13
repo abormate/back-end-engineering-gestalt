3d69d939632735680d71e6ddc31e73982a3dda21
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-13 17:24:27 +0800

    Python StudySpace

--- Python/Py-Fundamentals/121-differences.py
+++ Python/Py-Fundamentals/121-differences.py
@@ -4,3 +4,13 @@
 #
 # ------------------------------------------- #
 
+"""
+Your local gym recently bought out another gym, and they're trying to merge their 
+customer data. You've been given 2 lists of customer IDs (numbers) and are trying 
+to figure out which customers from the first gym are not also members of the 
+second gym. Luckily, the IDs from the 2 gyms match up because they used the same 
+back-end servers.
+
+
+
+"""
