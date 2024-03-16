65fc93cb4271e96dc4bc71383349a9932342c98d
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-16 08:25:58 +0800

    Python StudySpace -- Errors and Exceptions

---
+++ Python/Py-Fundamentals/122-errors-exceptions.py
@@ -0,0 +1,28 @@
+# -------------------------------------------------- #
+#
+# -- Python StudySpace -- Errors and Exceptions --
+#
+# -------------------------------------------------- #
+
+"""
+You've probably encountered some errors in your code from time to time if you've 
+gotten this far in the course. In Python, there are two main kinds of 
+distinguishable errors.
+
+-- syntax errors
+-- exceptions
+
+"""
+
+# ------------------------ #
+# -- Syntax Errors --
+# ------------------------ #
+
+"""
+You probably know what these are by now. A syntax error is just the Python 
+interpreter telling you that your code isn't adhering to proper Python syntax.
+
+"""
+
+# this is not valid code, so it will error
+
