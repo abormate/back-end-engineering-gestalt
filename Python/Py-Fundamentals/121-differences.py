0871b00f12c1441501d11191181ce34c45df98ba
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-16 08:23:09 +0800

    Python StudySpace -- Differences

--- Python/Py-Fundamentals/121-differences.py
+++ Python/Py-Fundamentals/121-differences.py
@@ -103,4 +103,11 @@ if "__RUN__" in globals():
 
 main()
 
+# ---------------------------------- #
+# -- Code Fix --
+# ---------------------------------- #
 
+def find_missing_ids(first_ids, second_ids):
+    s1 = set(first_ids)
+    s2 = set(second_ids)
+    return list(s1 - s2)
