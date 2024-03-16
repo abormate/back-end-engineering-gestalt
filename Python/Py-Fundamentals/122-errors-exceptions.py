8ac7cbc5e9a7d5cbfda13b2bbd10e17636ea5619
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-16 08:28:03 +0800

    Python StudySpace -- Errors and Exceptions

--- Python/Py-Fundamentals/122-errors-exceptions.py
+++ Python/Py-Fundamentals/122-errors-exceptions.py
@@ -26,3 +26,25 @@ interpreter telling you that your code isn't adhering to proper Python syntax.
 
 # this is not valid code, so it will error
 
+# ----------------------- #
+# -- Exceptions --
+# ----------------------- #
+
+"""
+Even if your code has the right syntax, however, it may still cause an error 
+when an attempt is made to execute it. Errors detected during execution are 
+called "exceptions" and can be handled gracefully by your code. You can even 
+raise your own exceptions when bad things happen in your code.
+
+Python uses a try-except pattern for handling errors.
+
+"""
+
+try:
+  10 / 0
+except Exception as e:
+  print(e)
+
+# prints "division by zero"
+  
+  
