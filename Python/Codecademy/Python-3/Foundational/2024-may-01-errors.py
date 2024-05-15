bec7cf6b288529c6e6fb2bd8d65dd63b77da2a36
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-15 10:18:28 +0800

    Python LearnSpace -- 01 -- Errors

--- Python/Codecademy/Python-3/Foundational/2024-may-01-errors.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-01-errors.py
@@ -7,7 +7,10 @@
 # Codecademy -- Python 3 -- Learning Course
 
 """
-
+Humans are prone to making mistakes. Humans are also typically in 
+charge of creating computer programs. To compensate, programming 
+languages attempt to understand and explain mistakes made in their 
+programs.
 
 
 """
