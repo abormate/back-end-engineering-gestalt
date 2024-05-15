a55a78847e6a1ffcc736e6e4cf58992c44eb7973
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-15 10:46:38 +0800

    Python LearnSpace -- 01 -- Errors and bugs

--- Python/Codecademy/Python-3/Foundational/2024-may-01-errors.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-01-errors.py
@@ -12,5 +12,11 @@ charge of creating computer programs. To compensate, programming
 languages attempt to understand and explain mistakes made in their 
 programs.
 
+Python refers to these mistakes as errors and will point to the 
+location where an error occurred with a ^ character. When programs 
+throw errors that we didn’t expect to encounter we call those errors 
+bugs. Programmers call the process of updating the program so that 
+it no longer produces unexpected errors debugging.
+
 
 """
