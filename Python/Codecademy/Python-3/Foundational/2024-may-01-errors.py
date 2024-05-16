24616054f2171001a05b913f91a76fa2632e9356
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-16 10:18:51 +0800

    Python LearnSpace -- 01 -- Errors

--- Python/Codecademy/Python-3/Foundational/2024-may-01-errors.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-01-errors.py
@@ -25,5 +25,9 @@ SyntaxError means there is something wrong with the way your program is
 written — punctuation that does not belong, a command where it is not 
 expected, or a missing parenthesis can all trigger a SyntaxError.
 
+A NameError occurs when the Python interpreter sees a word it does not 
+recognize. Code that contains something that looks like a variable but 
+was never defined will throw a NameError.
+
 
 """
