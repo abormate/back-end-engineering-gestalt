23c642d5faa4f7d30b0c3fe4b5ead7ed07f5759c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-17 07:48:53 +0800

    Python LearnSpace -- Codecademy (Python 03 - 02) -- Define Number

--- Python/Codecademy/Python-3/Foundational/2024-may-02-numbers.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-02-numbers.py
@@ -33,7 +33,13 @@ a_float = 2.1
 print(an_int + 3)
 # Output: 5
 
+Above we defined an integer and a float as the variables an_int and 
+a_float. We printed out the sum of the variable an_int with the number 3. 
+We call the number 3 here a literal, meaning it’s actually the number 3 
+and not a variable with the number 3 assigned to it.
 
+Floating-point numbers can behave in some unexpected ways due to how 
+computers store them.
 
 
 """
