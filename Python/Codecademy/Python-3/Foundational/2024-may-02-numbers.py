4829b235566fe394c9038575b1ff1e8290ed9db9
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-17 07:45:42 +0800

    Python LearnSpace -- Num

--- Python/Codecademy/Python-3/Foundational/2024-may-02-numbers.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-02-numbers.py
@@ -12,5 +12,11 @@ has a few numeric data types. It has multiple ways of storing numbers.
 Which one you use depends on your intended purpose for the number you 
 are saving.
 
+An integer, or int, is a whole number. It has no decimal point and 
+contains all counting numbers (1, 2, 3, …) as well as their negative 
+counterparts and the number 0. If you were counting the number of people 
+in a room, the number of jellybeans in a jar, or the number of keys on a 
+keyboard you would likely use an integer.
+
 
 """
