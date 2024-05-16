abd6d011a6a55b954db4d9f7ab923e018911b014
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-17 07:43:57 +0800

    Python LearnSpace

--- Python/Codecademy/Python-3/Foundational/2024-may-02-numbers.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-02-numbers.py
@@ -4,3 +4,13 @@
 #
 # ------------------------------------------------ #
 
+# -- Context -- #
+
+"""
+Computers can understand much more than just strings of text. Python 
+has a few numeric data types. It has multiple ways of storing numbers. 
+Which one you use depends on your intended purpose for the number you 
+are saving.
+
+
+"""
