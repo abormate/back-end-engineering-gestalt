96eeaaaf4bf44bd4078178e8d5bcc595ea7f26ce
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-20 11:35:31 +0800

    Python LearnSpace -- Codecademy -- Compute

--- Python/Codecademy/Python-3/Foundational/2024-may-03-calculations.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-03-calculations.py
@@ -4,3 +4,21 @@
 #
 # ------------------------------------------- #
 
+"""
+Computers absolutely excel at performing calculations. The "compute” in their 
+name comes from their historical association with providing answers to 
+mathematical questions. Python performs the arithmetic operations of addition, 
+subtraction, multiplication, and division with +, -, *, and /.
+
+# Prints "500"
+print(573 - 74 + 1)
+
+# Prints "50"
+print(25 * 2)
+
+# Prints "2.0"
+print(10 / 5)
+
+
+
+"""
