880da5dcbc6a8e4415ea667fd978f0026365abf7
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-21 18:08:10 +0800

    Python LearnSpace

--- Python/Codecademy/Python-3/Foundational/2024-may-04-updating-var.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-04-updating-var.py
@@ -4,3 +4,13 @@
 #
 # --------------------------------------------- #
 
+"""
+Variables that are assigned numeric values can be treated the same as the 
+numbers themselves. Two variables can be added together, divided by 2, and 
+multiplied by a third variable without Python distinguishing between the 
+variables and literals (like the number 2 in this example). Performing 
+arithmetic on variables does not change the variable — you can only update 
+a variable using the = sign.
+
+
+"""
