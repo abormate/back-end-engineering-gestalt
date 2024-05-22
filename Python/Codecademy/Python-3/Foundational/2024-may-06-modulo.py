dd135372eb2b6a2294f7b4ccec47515bbdc7296d
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-23 00:01:22 +0800

    Python LearnSpace -- Modulo

--- Python/Codecademy/Python-3/Foundational/2024-may-06-modulo.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-06-modulo.py
@@ -11,3 +11,30 @@ calculation. If the two numbers are divisible, then the result of the modulo
 operation will be 0.
 
 """
+
+# Prints 4 because 29 / 5 is 5 with a remainder of 4
+print(29 % 5)
+ 
+# Prints 2 because 32 / 3 is 10 with a remainder of 2
+print(32 % 3)
+ 
+# Modulo by 2 returns 0 for even numbers and 1 for odd numbers
+# Prints 0
+print(44 % 2)
+
+
+"""
+Here, we use the modulo operator to find the remainder of division operations. 
+We see that 29 % 5 equals 4, 32 % 3 equals 2, and 44 % 2 equals 0.
+
+Lets look at another example to get a better idea of how modulo is useful in 
+programming:
+
+"""
+
+print(3 % 3) # Prints 0
+print(4 % 3) # Prints 1
+print(5 % 3) # Prints 2
+print(6 % 3) # Prints 0
+print(7 % 3) # Prints 1
+
