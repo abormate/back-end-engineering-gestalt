e0314ef9166ae19fb6d4ffcb866cfeffd7c6e754
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-22 23:59:52 +0800

    Python LearnSpace -- Modulo

--- Python/Codecademy/Python-3/Foundational/2024-may-06-modulo.py
+++ Python/Codecademy/Python-3/Foundational/2024-may-06-modulo.py
@@ -4,3 +4,10 @@
 #
 # -------------------------------------- #
 
+"""
+Python offers a companion to the division operator called the modulo operator. 
+The modulo operator is indicated by % and gives the remainder of a division 
+calculation. If the two numbers are divisible, then the result of the modulo 
+operation will be 0.
+
+"""
