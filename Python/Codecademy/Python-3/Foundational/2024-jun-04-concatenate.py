05ee69b1c20e0218751e1c4f8c2d7c3a9f086c2c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-06-04 13:12:27 +0800

    Python LearnSpace -- Concatenante

--- Python/Codecademy/Python-3/Foundational/2024-jun-04-concatenate.py
+++ Python/Codecademy/Python-3/Foundational/2024-jun-04-concatenate.py
@@ -4,3 +4,18 @@
 #
 # ------------------------------------------ #
 
+"""
+The + operator doesn’t just add two numbers, it can also “add” two 
+strings! The process of combining two strings is called string 
+concatenation. Performing string concatenation creates a brand 
+new string comprised of the first string’s contents followed by 
+the second string’s contents (without any added space in-between).
+
+"""
+
+greeting_text = "Hey there!"
+question_text = "How are you doing?"
+full_text = greeting_text + question_text
+
+# Prints "Hey there!How are you doing?"
+print(full_text)
