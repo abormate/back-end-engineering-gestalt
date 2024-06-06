67619197ddf1e35cff5a0bf5e60b1e5f5dcdb4bc
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-06-06 18:42:36 +0800

    Python LearnSpace

--- Python/Codecademy/Python-3/Foundational/2024-jun-06-plus-equals.py
+++ Python/Codecademy/Python-3/Foundational/2024-jun-06-plus-equals.py
@@ -4,3 +4,10 @@
 #
 # ---------------------------------------- #
 
+"""
+Python offers a shorthand for updating variables. When you have a 
+number saved in a variable and want to add to the current value of 
+the variable, you can use the += (plus-equals) operator.
+
+"""
+
