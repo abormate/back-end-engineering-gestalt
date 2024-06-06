3cc9a899f4a3143b933b09e61f84db0314018db6
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-06-06 18:42:53 +0800

    Python LearnSpace

--- Python/Codecademy/Python-3/Foundational/2024-jun-06-plus-equals.py
+++ Python/Codecademy/Python-3/Foundational/2024-jun-06-plus-equals.py
@@ -11,3 +11,16 @@ the variable, you can use the += (plus-equals) operator.
 
 """
 
+# First we have a variable with a number saved
+number_of_miles_hiked = 12
+
+# Then we need to update that variable
+# Let's say we hike another two miles today
+number_of_miles_hiked += 2
+
+# The new value is the old value
+# Plus the number after the plus-equals
+print(number_of_miles_hiked)
+# Prints 14
+
+
