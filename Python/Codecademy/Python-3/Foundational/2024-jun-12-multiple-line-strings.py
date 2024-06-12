f30c541a7bf4795a50f7bcdf887498cabf2960bb
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-06-12 22:08:34 +0800

    Python LearnSpace -- Codecademy -- Multiple line Strings

--- Python/Codecademy/Python-3/Foundational/2024-jun-12-multiple-line-strings.py
+++ Python/Codecademy/Python-3/Foundational/2024-jun-12-multiple-line-strings.py
@@ -4,3 +4,16 @@
 #
 # --------------------------------------------- #
 
+"""
+Python strings are very flexible, but if we try to create a string 
+that occupies multiple lines we find ourselves face-to-face with a 
+SyntaxError. 
+
+Python offers a solution: multi-line strings. By using 
+three quote-marks instead of one, we tell the program that the 
+string doesn’t end until the next triple-quote. This method is 
+useful if the string being defined contains a lot of quotation 
+marks and we want to be sure we dont close it prematurely.
+
+"""
+
