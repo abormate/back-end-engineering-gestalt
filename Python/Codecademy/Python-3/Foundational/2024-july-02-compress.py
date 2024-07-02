d446bde6222e9a927189df4bd860e1d1b54cd741
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-07-02 14:34:56 +0800

    Python LearnSpace -- Greetings -- Name and Age

---
+++ Python/Codecademy/Python-3/Foundational/2024-july-02-compress.py
@@ -0,0 +1,7 @@
+my_age = 34
+half_my_age = 17
+name = "My name is Abraham. "
+greeting = "Hello! "
+greeting_with_name = greeting + name
+
+print(str(greeting_with_name) + "I'm " + str(my_age) + " years of age. ")
