94187a1e2e4628766ba73ceebe87b0d67796a61b
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-08-02 19:55:50 +0800

    Python LearnSpace -- Loveseats Furnitures -- Project Receipts

--- Python/Codecademy/Python-3/Foundational/2024-aug-01-project.py
+++ Python/Codecademy/Python-3/Foundational/2024-aug-01-project.py
@@ -6,8 +6,11 @@
 
 """
 
-Weve decided to pursue the dream of small-business ownership and open up a furniture store called Lovely Loveseats for Neat Suites on Fleet Street. With our newfound knowledge of Python programming, we’re going to build a system to help speed up the process of creating receipts for your customers.
-
+Weve decided to pursue the dream of small-business ownership and 
+open up a furniture store called Lovely Loveseats for Neat Suites 
+on Fleet Street. With our newfound knowledge of Python programming, 
+were going to build a system to help speed up the process of 
+creating receipts for your customers.
 
 
 """
