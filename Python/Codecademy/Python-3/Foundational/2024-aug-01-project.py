d88a9df99777e064877af4168f63442c1b4d14ac
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-08-03 21:33:28 +0800

    Python LearnSpace -- Lovely Loveseat Furniture Shop -- Project Receipts

--- Python/Codecademy/Python-3/Foundational/2024-aug-01-project.py
+++ Python/Codecademy/Python-3/Foundational/2024-aug-01-project.py
@@ -33,3 +33,4 @@ string:
 
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 
+lovely_loveseat_price = 254.00
