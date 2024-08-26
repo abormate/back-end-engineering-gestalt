1dee2a8692dfa6f2b8dfe716dfc9a33fc1cbe036
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-08-26 19:27:49 +0800

    Python LearnSpace -- Input

---
+++ Python/Codecademy/Python-3/Foundational/2024-aug-input-lesson.py
@@ -0,0 +1,17 @@
+"""
+So far, weve covered how to assign variables values directly 
+in a Python file. However, we often want a user of a program 
+to enter new information into the program.
+
+How can we do this? As it turns out, another way to assign a 
+value to a variable is through user input.
+
+While we output a variables value using print(), we assign 
+information to a variable using input(). The input() function 
+requires a prompt message, which it will print out for the 
+user before they enter the new information. 
+
+For example:
+
+
+"""
