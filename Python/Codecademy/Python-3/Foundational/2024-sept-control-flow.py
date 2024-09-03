9defd09caffab1cf1856de233007ab7172d9a1a4
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-09-03 16:15:31 +0800

    Python LearnSpace --

--- Python/Codecademy/Python-3/Foundational/2024-sept-control-flow.py
+++ Python/Codecademy/Python-3/Foundational/2024-sept-control-flow.py
@@ -15,6 +15,6 @@ catch a couple extra Zs. But alas, it is a weekday, so you
 are up and dressed and you go to look outside, “Whats the 
 weather like? Do I need an umbrella?”
 
-
+These questions and decisions control the flow of your morning, each step and result is a product of the conditions of the day and your surroundings. Your computer, just like you, goes through a similar flow every time it executes code. A program will run (wake up) and start moving through its checklists, is this condition met, is that condition met, okay lets execute this code and return that value.
 
 """
