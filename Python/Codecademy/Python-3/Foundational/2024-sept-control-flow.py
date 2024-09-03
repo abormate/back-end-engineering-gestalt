f0bf1b303f39325bc084d28b50e48d6e433a947a
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-09-03 16:13:30 +0800

    Python LearnSpace --

--- Python/Codecademy/Python-3/Foundational/2024-sept-control-flow.py
+++ Python/Codecademy/Python-3/Foundational/2024-sept-control-flow.py
@@ -9,6 +9,6 @@ Imagine waking up in the morning.
 
 You wake up and think, “Ugh. Is it a weekday?”
 
-
+If so, you have to get up and get dressed and get ready for work or school. If not, you can sleep in a bit longer and catch a couple extra Z’s. But alas, it is a weekday, so you are up and dressed and you go to look outside, “Whats the weather like? Do I need an umbrella?”
 
 """
