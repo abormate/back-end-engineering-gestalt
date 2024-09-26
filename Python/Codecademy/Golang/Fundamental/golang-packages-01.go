8b3b6b9d885b66fa162b55813171fe8596f4a31d
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-09-26 16:20:05 +0800

    Golang Programming LearnSpace

--- Python/Codecademy/Golang/Fundamental/golang-packages-01.go
+++ Python/Codecademy/Golang/Fundamental/golang-packages-01.go
@@ -4,3 +4,30 @@
 // -- Golang -- Packages -- 
 //
 // -------------------------------------- //
+
+/*
+Now that we understand how to compile and run Go programs, 
+let’s take a look at Go packages.
+
+Projects can contain many .go files, organized into packages. 
+Each package is like a directory: .go files to do with one 
+part of your program can go in one package, other .go files 
+to do with something else can go into another package. If we 
+were writing a calculator program, we could put the files for 
+the calculation in package calc and the files for input & 
+output in package io.
+
+*/
+
+package main 
+
+import "fmt" 
+
+func main () {
+  fmt.Println("Hello World") 
+}
+
+/*
+
+
+*/
