37cb92f8c6e1e422bbda4957f73332e910d68f95
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-09-30 19:55:11 +0800

    Golang -- Programming -- LearnSpace -- Multiple Packages

---
+++ Python/Codecademy/Golang/Fundamental/golang-multiple-packages.go
@@ -0,0 +1,24 @@
+// ---------------------------------------------- //
+//
+// -- Go Programming -- Import Multiple Packages --
+//
+// ---------------------------------------------- //
+
+/*
+Previously, we imported a single package, fmt. This package is 
+useful but it is only one in a list of many included with Go.
+
+The standard packages are so useful that you will often use 
+multiple packages in each .go file.
+
+*/
+
+// ------------------------------- //
+// -- Importing Multiple Packages -- 
+// ------------------------------- //
+
+/* 
+To import multiple packages, we can add multiple import 
+statements:
+
+*/
